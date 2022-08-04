498e4f73526a95f1e89c4eb3de9130f9a72f4d86
sojlabjoi <ungurkristjan@gmail.com>
2022-08-04 13:46:56 +0300

    Reduced code and added retrieval for 'clientToken' used in 'sessionId'

--- net/minecraft/auth/microsoft/MAuthenticate.java
+++ net/minecraft/auth/microsoft/MAuthenticate.java
@@ -14,14 +14,14 @@ import org.json.JSONObject;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Objects;
 
 public class MAuthenticate extends VBox {
     private final MTokens minecraftTokens = new MTokens(this);
-
-    private static final String authUrl = "https://login.live.com/oauth20_authorize.srf"
+    static final String authUrl = "https://login.live.com/oauth20_authorize.srf"
             + "?client_id=00000000402b5328"
             + "&response_type=code"
             + "&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL"
@@ -32,66 +32,61 @@ public class MAuthenticate extends VBox {
     static final String xstsAuthUrl = "https://xsts.auth.xboxlive.com/xsts/authorize";
     static final String minecraftLoginUrl = "https://api.minecraftservices.com/authentication/login_with_xbox";
     static final String minecraftProfileUrl = "https://api.minecraftservices.com/minecraft/profile";
+    static String clientTokenString;
     public final LFrame launcherFrame;
-    protected JFrame frame;
-    protected WebView webView;
+    final JFrame frame = new JFrame("Login with Microsoft");
 
     public MAuthenticate(LFrame launcherFrame) {
         this.launcherFrame = launcherFrame;
     }
 
     public void authenticate() {
-        MAuthenticate microsoftAuthenticate = this;
-
-        JFXPanel jfxPanel = new JFXPanel();
-        Platform.runLater(() -> jfxPanel.setScene(new Scene(this)));
-        frame = new JFrame("Login with Microsoft");
-        frame.add(jfxPanel);
-        frame.pack();
         frame.setVisible(true);
-        frame.setSize(485, 638);
-        frame.setLocationRelativeTo(launcherFrame);
+        frame.setPreferredSize(new Dimension(485, 638));
         frame.setResizable(false);
-        frame.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("favicon2.png"))).getImage());
+        frame.pack();
+        frame.setLocationRelativeTo(launcherFrame);
         frame.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosing(WindowEvent we) {
-               Thread thread = new Thread(() -> Platform.runLater(() -> {
-                    frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-                    frame.dispose();
-                })); thread.setDaemon(true); thread.start();
+            public void windowOpened(WindowEvent e) {
+                JFXPanel fxPanel = new JFXPanel();
+                frame.add(fxPanel);
+
+                Platform.runLater(() -> {
+                    WebView webView = new WebView();
+                    webView.getEngine().load(authUrl);
+                    webView.getEngine().setJavaScriptEnabled(true);
+                    webView.setPrefSize(485, 638);
+                    webView.getEngine().getHistory().getEntries().addListener((ListChangeListener<WebHistory.Entry>) change -> {
+                        if (change.next() && change.wasAdded()) {
+                            for (WebHistory.Entry entry : change.getAddedSubList()) {
+                                if (entry.getUrl().startsWith(redirectUrl + "?code=")) {
+                                    clientTokenString = entry.getUrl().substring(entry.getUrl().indexOf("=") + 1, entry.getUrl().indexOf("&"));
+                                    minecraftTokens.acquireAccessToken(clientTokenString);
+                                }
+                            }
+                        }
+                        if (change.wasAdded() && webView.getEngine().getLocation().contains("oauth20_desktop.srf?error=access_denied")) {
+                            frame.dispose();
+                        }
+                    });
+                    fxPanel.setScene(new Scene(webView));
+                });
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Platform.runLater(frame::dispose);
             }
         });
-
-        Thread webViewThread = new Thread(() -> Platform.runLater(() -> {
-            webView = new WebView();
-            microsoftAuthenticate.getChildren().add(webView);
-            webView.getEngine().load(authUrl);
-            webView.getEngine().setJavaScriptEnabled(true);
-            webView.setPrefSize(485, 638);
-            webView.getEngine().getHistory().getEntries().addListener(this::onChanged);
-        })); webViewThread.setDaemon(true); webViewThread.start();
-    }
-
-    private void onChanged(ListChangeListener.Change<? extends WebHistory.Entry> c) {
-        if (c.next() && c.wasAdded()) {
-            for (WebHistory.Entry entry : c.getAddedSubList()) {
-                if (entry.getUrl().startsWith(redirectUrl + "?code=")) {
-                    minecraftTokens.acquireAccessToken(entry.getUrl().substring(entry.getUrl().indexOf("=") + 1, entry.getUrl().indexOf("&")));
-                }
-            }
-        }
-        if (c.wasAdded() && webView.getEngine().getLocation().contains("oauth20_desktop.srf?error=access_denied")) {
-            frame.dispose();
-        }
+        frame.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("favicon2.png"))).getImage());
     }
 
     void acquireMCProfile(String accessToken) {
         try {
             JSONObject jsonResponse = AUtils.requestJSONGET(MAuthenticate.minecraftProfileUrl, accessToken);
             this.getLauncherFrame().playOnline(jsonResponse.getString("name"),
-                    ":mockToken:" + accessToken + jsonResponse.getString("id"));
-
+                    String.format("%s:%s:%s", clientTokenString.substring(9).replaceAll("-", ""), accessToken, jsonResponse.getString("id")));
             frame.dispose();
         } catch (JSONException e) {
             e.printStackTrace();
