6d44b142e4070f1a9bd2f48f40a72838e6a70a33
sojlabjoi <ungurkristjan@gmail.com>
2022-08-15 09:16:53 +0300

    Updated the version number.

--- net/minecraft/launcher/LauncherUpdate.java
+++ net/minecraft/launcher/LauncherUpdate.java
@@ -6,7 +6,7 @@ import org.json.JSONObject;
 import javax.swing.JOptionPane;
 
 public final class LauncherUpdate {
-    public static final String currentVersion = "0.8.1422";
+    public static final String currentVersion = "0.8.1522";
     public static final String latestVersion = isOutdated();
 
     private LauncherUpdate() {
