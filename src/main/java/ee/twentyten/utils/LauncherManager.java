8d8df756807d89410763bec0c3d3e8324ac168f0
sojlabjoi <ungurkristjan@gmail.com>
2023-01-23 14:00:03 +0200

    * Renamed "FileManager" to "LauncherManager".

--- src/main/java/ee/twentyten/utils/LauncherManager.java
+++ src/main/java/ee/twentyten/utils/LauncherManager.java
@@ -6,35 +6,34 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public final class FileManager {
+public final class LauncherManager {
 
   private static final String USER_HOME = System.getProperty("user.home", ".");
   private static final String APPDATA = System.getenv("APPDATA");
   private static Map<EPlatform, File> gameDirectory;
 
   static {
-    FileManager.gameDirectory = new HashMap<>();
+    LauncherManager.gameDirectory = new HashMap<>();
 
     File gameDirectoryForOsx = new File(
         String.format("%s/Library/Application Support", USER_HOME), "twentyten");
-    FileManager.gameDirectory.put(EPlatform.OSX, gameDirectoryForOsx);
+    LauncherManager.gameDirectory.put(EPlatform.OSX, gameDirectoryForOsx);
 
     File gameDirectoryForLinux = new File(USER_HOME, ".twentyten");
-    FileManager.gameDirectory.put(EPlatform.LINUX, gameDirectoryForLinux);
+    LauncherManager.gameDirectory.put(EPlatform.LINUX, gameDirectoryForLinux);
 
     File gameDirectoryForWindows =
         APPDATA != null ? new File(APPDATA, ".twentyten") : new File(USER_HOME, ".twentyten");
-    FileManager.gameDirectory.put(EPlatform.WINDOWS, gameDirectoryForWindows);
+    LauncherManager.gameDirectory.put(EPlatform.WINDOWS, gameDirectoryForWindows);
   }
 
-  private FileManager() {
-    throw new UnsupportedOperationException("Utility class is not instantiable");
+  private LauncherManager() {
   }
 
   public static File getGameDirectory() {
     EPlatform platformName = EPlatform.getByOSNames();
 
-    File gameDirectory = FileManager.gameDirectory.get(platformName);
+    File gameDirectory = LauncherManager.gameDirectory.get(platformName);
     Objects.requireNonNull(gameDirectory, "gameDirectory == null!");
     if (!gameDirectory.mkdirs() && !gameDirectory.exists()) {
       throw new RuntimeException("Can't create the game directory!");
