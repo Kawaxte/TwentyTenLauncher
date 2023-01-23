beff5e85ee2bf9a7c1a89111078c9df85b4e3e99
sojlabjoi <ungurkristjan@gmail.com>
2023-01-23 13:56:13 +0200

    + Custom 'Properties' class with aligned fields.

---
+++ src/main/java/ee/twentyten/core/LinkedProperties.java
@@ -0,0 +1,51 @@
+package ee.twentyten.core;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+public class LinkedProperties extends Properties {
+
+  private final LinkedHashMap<Object, Object> map = new LinkedHashMap<>();
+
+  @Override
+  public synchronized Object put(Object key, Object value) {
+    map.put(key, value);
+    return super.put(key, value);
+  }
+
+  @Override
+  public Set<Object> keySet() {
+    return map.keySet();
+  }
+
+  @Override
+  public Set<Map.Entry<Object, Object>> entrySet() {
+    return map.entrySet();
+  }
+
+  @Override
+  public void store(OutputStream out, String comments) throws IOException {
+    if (comments != null) {
+      out.write("# ".getBytes());
+      out.write(comments.getBytes());
+      out.write(System.lineSeparator().getBytes());
+    }
+
+    Set<Map.Entry<Object, Object>> entries = entrySet();
+    for (Map.Entry<Object, Object> entry : entries) {
+      String key = (String) entry.getKey();
+      out.write(key.getBytes());
+      out.write("=".getBytes());
+
+      String value = (String) entry.getValue();
+      out.write(value.getBytes());
+      out.write(System.lineSeparator().getBytes());
+    }
+  }
+}
