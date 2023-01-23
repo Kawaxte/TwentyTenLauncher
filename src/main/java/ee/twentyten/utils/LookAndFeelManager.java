a09a37996411b4be5d7299b152dc30ec39791434
sojlabjoi <ungurkristjan@gmail.com>
2023-01-23 13:56:47 +0200

    + Added "Debug" outputs to all the functions.

--- src/main/java/ee/twentyten/utils/LookAndFeelManager.java
+++ src/main/java/ee/twentyten/utils/LookAndFeelManager.java
@@ -1,18 +1,20 @@
 package ee.twentyten.utils;
 
 import ee.twentyten.core.ELookAndFeel;
+import ee.twentyten.debug.DebugSystem;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public final class LookAndFeelManager {
 
   private LookAndFeelManager() {
-    throw new UnsupportedOperationException("Utility class is not instantiable");
   }
 
   public static void setLookAndFeel() {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+      DebugSystem.println(UIManager.getSystemLookAndFeelClassName());
     } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
              IllegalAccessException e) {
       throw new RuntimeException(e);
@@ -22,6 +24,8 @@ public final class LookAndFeelManager {
   public static void setLookAndFeel(ELookAndFeel lookAndFeel) {
     try {
       UIManager.setLookAndFeel(lookAndFeel.getClassName());
+
+      DebugSystem.println(UIManager.getSystemLookAndFeelClassName());
     } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
              IllegalAccessException e) {
       throw new RuntimeException(e);
