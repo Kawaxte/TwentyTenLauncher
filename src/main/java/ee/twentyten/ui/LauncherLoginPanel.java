220749a13602b94010413a8a9488205786499273
sojlabjoi <ungurkristjan@gmail.com>
2023-01-25 15:31:15 +0200

    Removed button BG because Win7 Classic theme breaks.

--- src/main/java/ee/twentyten/ui/LauncherLoginPanel.java
+++ src/main/java/ee/twentyten/ui/LauncherLoginPanel.java
@@ -1,6 +1,5 @@
 package ee.twentyten.ui;
 
-import ee.twentyten.core.ELookAndFeel;
 import ee.twentyten.core.swing.JBorderPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -20,10 +19,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LauncherLoginPanel extends JBorderPanel {
-
-  private JPanel middlePanel1;
-  private JPanel middlePanel2;
-  private JPanel bottomPanel;
 
   private JLabel errorLabel;
   private JLabel usernameLabel;
@@ -56,57 +51,51 @@ public class LauncherLoginPanel extends JBorderPanel {
   }
 
   private void createMiddlePanel1(GridLayout gl) {
-    this.middlePanel1 = new JPanel(gl);
-    this.middlePanel1.setBackground(Color.GRAY);
+    JPanel middlePanel1 = new JPanel(gl);
+    middlePanel1.setBackground(Color.GRAY);
 
     this.usernameLabel = new JLabel("Username:", SwingConstants.RIGHT);
-    this.middlePanel1.add(this.usernameLabel, 0);
+    middlePanel1.add(this.usernameLabel, 0);
 
     this.passwordLabel = new JLabel("Password:", SwingConstants.RIGHT);
-    this.middlePanel1.add(this.passwordLabel, 1);
+    middlePanel1.add(this.passwordLabel, 1);
 
     this.optionsButton = new JButton("Options");
-    if (ELookAndFeel.getCurrentLookAndFeel() == ELookAndFeel.WINDOWS) {
-      this.optionsButton.setBackground(this.getBackground());
-    }
-    this.middlePanel1.add(this.optionsButton, 2);
+    middlePanel1.add(this.optionsButton, 2);
 
-    this.add(this.middlePanel1, BorderLayout.WEST);
+    this.add(middlePanel1, BorderLayout.WEST);
   }
 
   private void createMiddlePanel2(GridLayout gl) {
-    this.middlePanel2 = new JPanel(gl);
-    this.middlePanel2.setBackground(Color.GRAY);
+    JPanel middlePanel2 = new JPanel(gl);
+    middlePanel2.setBackground(Color.GRAY);
 
     this.usernameField = new JTextField(20);
-    this.middlePanel2.add(this.usernameField, 0);
+    middlePanel2.add(this.usernameField, 0);
 
     this.passwordField = new JPasswordField(20);
-    this.middlePanel2.add(this.passwordField, 1);
+    middlePanel2.add(this.passwordField, 1);
 
     this.rememberPasswordCheckBox = new JCheckBox("Remember Password");
     this.rememberPasswordCheckBox.setContentAreaFilled(false);
-    this.middlePanel2.add(this.rememberPasswordCheckBox, 2);
+    middlePanel2.add(this.rememberPasswordCheckBox, 2);
 
-    this.add(this.middlePanel2, BorderLayout.CENTER);
+    this.add(middlePanel2, BorderLayout.CENTER);
   }
 
   private void createBottomPanel() {
-    this.bottomPanel = new JPanel(new BorderLayout());
-    this.bottomPanel.setBackground(Color.GRAY);
+    JPanel bottomPanel = new JPanel(new BorderLayout());
+    bottomPanel.setBackground(Color.GRAY);
 
     this.linkLabel = new JLabel("<html><a href=''>You need to update the launcher!</a></html>",
         SwingConstants.LEFT);
     this.linkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
     this.linkLabel.setForeground(Color.BLUE);
-    this.bottomPanel.add(this.linkLabel, BorderLayout.WEST);
+    bottomPanel.add(this.linkLabel, BorderLayout.WEST);
 
     this.loginButton = new JButton("Login");
-    if (ELookAndFeel.getCurrentLookAndFeel() == ELookAndFeel.WINDOWS) {
-      this.loginButton.setBackground(this.getBackground());
-    }
-    this.bottomPanel.add(this.loginButton, BorderLayout.EAST);
+    bottomPanel.add(this.loginButton, BorderLayout.EAST);
 
-    this.add(this.bottomPanel, BorderLayout.SOUTH);
+    this.add(bottomPanel, BorderLayout.SOUTH);
   }
 }
