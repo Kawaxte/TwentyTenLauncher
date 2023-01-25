dae074bedcd2b69dcc3334d7f76c7c6169fb7d47
sojlabjoi <ungurkristjan@gmail.com>
2023-01-25 15:26:55 +0200

    Aded 'debug format' and cleaned up the methods.

--- src/main/java/ee/twentyten/debug/DebugSystem.java
+++ src/main/java/ee/twentyten/debug/DebugSystem.java
@@ -4,25 +4,29 @@ import java.lang.management.ManagementFactory;
 
 public final class DebugSystem {
 
-  private static final boolean debug = ManagementFactory.getRuntimeMXBean().getInputArguments()
+  private static final boolean DEBUG = ManagementFactory.getRuntimeMXBean().getInputArguments()
       .toString().contains("-agentlib:jdwp");
+  private static final String DEBUG_FORMAT = "[DEBUG] (%s::%s) %s%n";
 
   private DebugSystem() {
   }
 
   public static void printf(String format, Object... args) {
-    if (debug) {
-      String className = Thread.currentThread().getStackTrace()[2].getClassName();
-      String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      System.out.printf("[DEBUG] %s::%s %s", className, methodName, String.format(format, args));
+    if (DEBUG) {
+      StackTraceElement element = Thread.currentThread().getStackTrace()[2];
+      String className = element.getClassName();
+      String methodName = element.getMethodName();
+      System.out.printf(DEBUG_FORMAT, className, methodName,
+          String.format(format, args));
     }
   }
 
   public static void println(Object obj) {
-    if (debug) {
-      String className = Thread.currentThread().getStackTrace()[2].getClassName();
-      String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      System.out.println("[DEBUG] " + className + "::" + methodName + " " + obj);
+    if (DEBUG) {
+      StackTraceElement element = Thread.currentThread().getStackTrace()[2];
+      String className = element.getClassName();
+      String methodName = element.getMethodName();
+      System.out.printf(DEBUG_FORMAT, className, methodName, obj);
     }
   }
 }
