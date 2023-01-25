9395807ae18ea9b3255e6678ea19ff195eca846a
sojlabjoi <ungurkristjan@gmail.com>
2023-01-25 15:27:22 +0200

    Getters and Setters for look and feel settings.

--- src/main/java/ee/twentyten/core/ELookAndFeel.java
+++ src/main/java/ee/twentyten/core/ELookAndFeel.java
@@ -1,6 +1,8 @@
 package ee.twentyten.core;
 
+import ee.twentyten.debug.DebugSystem;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import lombok.Getter;
 
 @Getter
@@ -27,5 +29,27 @@ public enum ELookAndFeel {
       }
     }
     return null;
+  }
+
+  public static void setLookAndFeel() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      
+      DebugSystem.println(UIManager.getLookAndFeel().getClass().getName());
+    } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
+             IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void setLookAndFeel(ELookAndFeel lookAndFeel) {
+    try {
+      UIManager.setLookAndFeel(lookAndFeel.getClassName());
+
+      DebugSystem.println(UIManager.getLookAndFeel().getClass().getName());
+    } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
+             IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
