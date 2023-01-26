6c63daf00917978d81e535b5bb286378e609179e
sojlabjoi <ungurkristjan@gmail.com>
2023-01-26 10:37:55 +0200

    Check if the launcher is outdated.

--- src/main/java/ee/twentyten/utils/VersionManager.java
+++ src/main/java/ee/twentyten/utils/VersionManager.java
@@ -1,12 +1,22 @@
 package ee.twentyten.utils;
 
 import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
 
 public final class VersionManager {
 
-  private static final String API_GITHUB_LATEST = "https://api.github.com/repos/sojlabjoi/AlphacraftLauncher/releases/latest";
-
+  public static final String GITHUB_LATEST_URL;
+  public static final String SIGNUP_LIVE_URL;
+  private static final String API_GITHUB_LATEST_URL;
   public static String currentVersion = null;
+
+  static {
+    GITHUB_LATEST_URL = "https://github.com/sojlabjoi/AlphacraftLauncher/releases/latest";
+    SIGNUP_LIVE_URL = "https://signup.live.com/signup?cobrandid=8058f65d-ce06-4c30-9559-473c9275a65d&client_id=00000000402b5328&lic=1";
+    API_GITHUB_LATEST_URL = "https://api.github.com/repos/sojlabjoi/AlphacraftLauncher/releases/latest";
+  }
 
   private VersionManager() {
   }
@@ -25,5 +35,28 @@ public final class VersionManager {
     VersionManager.currentVersion = String.format("%d.%s.%s%s", x, formattedMonth, formattedDay,
         formattedYear);
     return VersionManager.currentVersion;
+  }
+
+  public static boolean isOutdated() {
+    SwingWorker<Boolean, Void> worker = new SwingWorker<Boolean, Void>() {
+      @Override
+      protected Boolean doInBackground() throws Exception {
+        String latestVersion = RequestManager.requestJsonGet(API_GITHUB_LATEST_URL).get("tag_name")
+            .toString();
+        return !currentVersion.equals(latestVersion);
+      }
+    };
+    worker.execute();
+
+    try {
+      return worker.get();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException e) {
+      JOptionPane.showMessageDialog(null,
+          String.format("An error occurred while checking for updates:%s%n", e.getMessage()),
+          "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    return false;
   }
 }
