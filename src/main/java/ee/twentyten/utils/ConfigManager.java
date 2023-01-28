384933999c03bd13572ae0c1a11dfb99e585fb46
sojlabjoi <ungurkristjan@gmail.com>
2023-01-28 21:10:04 +0200

    Added thrown exception messages. "FilesManager" -> "DirectoryManager".

--- src/main/java/ee/twentyten/utils/ConfigManager.java
+++ src/main/java/ee/twentyten/utils/ConfigManager.java
@@ -20,10 +20,11 @@ public final class ConfigManager {
   private static final String DEFAULT_VERSION_ID = "b1.1_02";
 
   private ConfigManager() {
+    throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
   public static String getClientToken() {
-    File configFile = new File(FilesManager.getGameDirectory(), "twentyten.properties");
+    File configFile = new File(DirectoryManager.getGameDirectory(), "twentyten.properties");
     if (!configFile.exists()) {
       return UUID.randomUUID().toString().replace("-", "");
     }
@@ -32,24 +33,28 @@ public final class ConfigManager {
     try {
       properties.load(Files.newInputStream(configFile.toPath()));
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      throw new RuntimeException("Failed to load config file!", e);
     }
 
     String clientToken = UUID.randomUUID().toString().replace("-", "");
     return properties.getProperty("client-token", clientToken);
   }
 
-  public static void initConfig() throws InstantiationException, IllegalAccessException {
-    LauncherConfig config = LauncherConfig.class.newInstance();
-    config.setClientToken(DEFAULT_CLIENT_TOKEN);
-    config.setAccessToken(DEFAULT_ACCESS_TOKEN);
-    config.setUsername(DEFAULT_USERNAME);
-    config.setPassword(DEFAULT_PASSWORD);
-    config.setRememberBox(DEFAULT_REMEMBER_PASSWORD);
-    config.setBetaBox(DEFAULT_BETA_VERSION);
-    config.setAlphaBox(DEFAULT_ALPHA_VERSION);
-    config.setInfdevBox(DEFAULT_INFDEV_VERSION);
-    config.setVersionId(DEFAULT_VERSION_ID);
-    config.save();
+  public static void initConfig() {
+    try {
+      LauncherConfig config = LauncherConfig.class.newInstance();
+      config.setClientToken(DEFAULT_CLIENT_TOKEN);
+      config.setAccessToken(DEFAULT_ACCESS_TOKEN);
+      config.setUsername(DEFAULT_USERNAME);
+      config.setPassword(DEFAULT_PASSWORD);
+      config.setRememberBox(DEFAULT_REMEMBER_PASSWORD);
+      config.setBetaBox(DEFAULT_BETA_VERSION);
+      config.setAlphaBox(DEFAULT_ALPHA_VERSION);
+      config.setInfdevBox(DEFAULT_INFDEV_VERSION);
+      config.setVersionId(DEFAULT_VERSION_ID);
+      config.save();
+    } catch (InstantiationException | IllegalAccessException e) {
+      throw new RuntimeException("Failed to initialise config", e);
+    }
   }
 }
