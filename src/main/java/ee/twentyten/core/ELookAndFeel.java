5fa6b0ac9aa3dad18b1d55e3dc15b07ab1c8f0e2
sojlabjoi <ungurkristjan@gmail.com>
2023-01-28 21:11:42 +0200

    Removed METAL, NIMBUS, MOTIF and WINDOWS_CLASSIC. Updated 'getLookAndFeel' method.

--- src/main/java/ee/twentyten/core/ELookAndFeel.java
+++ src/main/java/ee/twentyten/core/ELookAndFeel.java
@@ -6,13 +6,9 @@ import lombok.Getter;
 
 @Getter
 public enum ELookAndFeel {
-  METAL("javax.swing.plaf.metal.MetalLookAndFeel"),
-  NIMBUS("javax.swing.plaf.nimbus.NimbusLookAndFeel"),
-  MOTIF("com.sun.java.swing.plaf.motif.MotifLookAndFeel"),
   AQUA("com.apple.laf.AquaLookAndFeel"),
   GTK("com.sun.java.swing.plaf.gtk.GTKLookAndFeel"),
-  WINDOWS("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"),
-  WINDOWS_CLASSIC("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+  WINDOWS("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 
   private final String className;
 
@@ -20,10 +16,10 @@ public enum ELookAndFeel {
     this.className = className;
   }
 
-  public static ELookAndFeel getCurrentLookAndFeel() {
-    String currentLookAndFeelName = UIManager.getLookAndFeel().getClass().getName();
+  public static ELookAndFeel getLookAndFeel() {
+    String lookAndFeelName = UIManager.getLookAndFeel().getClass().getName();
     for (ELookAndFeel lookAndFeel : values()) {
-      if (lookAndFeel.className.equals(currentLookAndFeelName)) {
+      if (lookAndFeel.getClassName().equals(lookAndFeelName)) {
         return lookAndFeel;
       }
     }
@@ -33,18 +29,14 @@ public enum ELookAndFeel {
   public static void setLookAndFeel() {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
-             IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static void setLookAndFeel(ELookAndFeel lookAndFeel) {
-    try {
-      UIManager.setLookAndFeel(lookAndFeel.getClassName());
-    } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
-             IllegalAccessException e) {
-      throw new RuntimeException(e);
+    } catch (UnsupportedLookAndFeelException e) {
+      throw new RuntimeException("Failed to set look and feel", e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Failed to find look and feel class", e);
+    } catch (InstantiationException e) {
+      throw new RuntimeException("Failed to instantiate look and feel class", e);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Failed to access look and feel class", e);
     }
   }
 }
