738b74e91e99819c639862325c882fdcee0076ec
sojlabjoi <ungurkristjan@gmail.com>
2023-01-28 21:15:47 +0200

    Added 'actionPerformed' and 'mousePerformed' methods.

--- src/main/java/ee/twentyten/ui/LauncherFrame.java
+++ src/main/java/ee/twentyten/ui/LauncherFrame.java
@@ -2,56 +2,91 @@ package ee.twentyten.ui;
 
 import ee.twentyten.LauncherConfig;
 import ee.twentyten.core.ELookAndFeel;
+import ee.twentyten.core.event.CustomMouseListener;
 import ee.twentyten.utils.ConfigManager;
 import ee.twentyten.utils.ImageManager;
-import ee.twentyten.utils.VersionManager;
+import ee.twentyten.utils.LauncherManager;
+import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
+import java.net.URI;
 import javax.swing.JFrame;
-import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 
-public class LauncherFrame extends JFrame {
+public class LauncherFrame extends JFrame implements ActionListener, CustomMouseListener {
 
   private static final long serialVersionUID = 1L;
+  private static LauncherFrame frameInstance;
+  private final LauncherPanel panel;
+  private final LoginLauncherPanel loginPanel;
+  private MouseListener adapter;
 
   public LauncherFrame() {
-    super(String.format("TwentyTen Launcher %s", VersionManager.getCurrentVersion()));
-    try {
-      this.setIconImage(ImageManager.readImage(LauncherFrame.class, "icons/favicon.png"));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    super(String.format("TwentyTen Launcher %s", LauncherManager.getCurrentVersion()));
 
+    this.setIconImage(ImageManager.readImage(LauncherFrame.class, "icons/favicon.png"));
     this.setMinimumSize(new Dimension(640, 480));
 
-    LauncherPanel panel = new LauncherPanel();
-    this.getContentPane().add(panel, SwingConstants.CENTER);
+    this.panel = new LauncherPanel();
+    this.loginPanel = this.panel.getLoginPanel();
+    this.loginPanel.getOptionsButton().addActionListener(this);
+    this.loginPanel.getLinkLabel().addMouseListener(this.adapter);
+    this.setContentPane(this.panel);
 
     this.pack();
 
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
-    this.setVisible(true);
   }
 
   public static void main(String[] args) {
     ELookAndFeel.setLookAndFeel();
-    try {
-      LauncherConfig config = LauncherConfig.load();
-      if (config.getClientToken() == null) {
-        ConfigManager.initConfig();
-      }
-    } catch (IOException | InstantiationException | IllegalAccessException e) {
-      throw new RuntimeException(e);
+
+    LauncherConfig config = LauncherConfig.load();
+    if (config.getClientToken() == null) {
+      ConfigManager.initConfig();
     }
 
     SwingUtilities.invokeLater(new Runnable() {
       @Override
       public void run() {
-        new LauncherFrame();
+        LauncherFrame.frameInstance = new LauncherFrame();
+        LauncherFrame.frameInstance.setVisible(true);
       }
     });
+  }
+
+  @Override
+  public void mousePerformed(final MouseEvent me) {
+    this.adapter = new MouseAdapter() {
+      final Object source = me.getSource();
+
+      @Override
+      public void mouseClicked(MouseEvent me) {
+        if (source == loginPanel.getLinkLabel()) {
+          try {
+            Desktop.getDesktop().browse(URI.create(loginPanel.getLinkUrls()));
+          } catch (IOException e) {
+            throw new RuntimeException("Can't open browser", e);
+          }
+        }
+      }
+    };
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent ae) {
+    Object source = ae.getSource();
+    if (source == this.panel.getLoginPanel().getOptionsButton()) {
+      OptionsDialog optionsDialog = new OptionsDialog(LauncherFrame.frameInstance);
+      optionsDialog.setVisible(true);
+      optionsDialog.setResizable(false);
+    }
   }
 }
