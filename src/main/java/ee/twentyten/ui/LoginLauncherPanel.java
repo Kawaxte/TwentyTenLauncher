a343c8c6864d5c5ff4d0be045f06e7d1006c411e
sojlabjoi <ungurkristjan@gmail.com>
2023-01-28 21:18:06 +0200

    Merged into single 'createMiddlePanel' method. "VersionManager" -> "LauncherManager".

--- src/main/java/ee/twentyten/ui/LoginLauncherPanel.java
+++ src/main/java/ee/twentyten/ui/LoginLauncherPanel.java
@@ -1,17 +1,12 @@
 package ee.twentyten.ui;
 
-import ee.twentyten.core.swing.JBorderPanel;
-import ee.twentyten.utils.VersionManager;
+import ee.twentyten.core.JBorderPanel;
+import ee.twentyten.utils.LauncherManager;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.IOException;
-import java.net.URI;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -24,10 +19,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class LauncherLoginPanel extends JBorderPanel {
-
-  private String linkUrls;
-  private boolean outdated;
+public class LoginLauncherPanel extends JBorderPanel {
 
   private JLabel errorLabel;
   private JLabel usernameLabel;
@@ -37,9 +29,11 @@ public class LauncherLoginPanel extends JBorderPanel {
   private JButton optionsButton;
   private JCheckBox rememberPasswordCheckBox;
   private JLabel linkLabel;
+  private String linkUrls;
+  private boolean outdated;
   private JButton loginButton;
 
-  public LauncherLoginPanel() {
+  public LoginLauncherPanel() {
     super(new BorderLayout(0, 8), true);
 
     this.setBackground(Color.GRAY);
@@ -49,37 +43,22 @@ public class LauncherLoginPanel extends JBorderPanel {
 
   private void initComponents() {
     this.linkUrls =
-        this.outdated ? VersionManager.GITHUB_LATEST_URL : VersionManager.SIGNUP_LIVE_URL;
-    this.outdated = VersionManager.isOutdated();
+        this.outdated ? LauncherManager.GITHUB_LATEST_URL : LauncherManager.SIGNUP_LIVE_URL;
+    this.outdated = LauncherManager.isOutdated();
 
     this.errorLabel = new JLabel("\u00A0", SwingConstants.CENTER);
     this.errorLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 16));
     this.errorLabel.setForeground(Color.RED.darker());
     this.add(this.errorLabel, BorderLayout.NORTH);
 
-    GridLayout gl = new GridLayout(3, 1, 0, 2);
-    this.createMiddlePanel1(gl);
-    this.createMiddlePanel2(gl);
+    this.createMiddlePanel();
     this.createBottomPanel();
-
-    MouseAdapter ma = new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent me) {
-        Object source = me.getSource();
-        if (source == linkLabel) {
-          try {
-            Desktop.getDesktop().browse(URI.create(linkUrls));
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    };
-    this.linkLabel.addMouseListener(ma);
   }
 
-  private void createMiddlePanel1(GridLayout gl) {
-    JPanel middlePanel1 = new JPanel(gl);
+  private void createMiddlePanel() {
+    GridLayout gl = new GridLayout(3, 1, 0, 2);
+
+    JPanel middlePanel1 = new JPanel(gl, true);
     middlePanel1.setBackground(Color.GRAY);
 
     this.usernameLabel = new JLabel("Username:", SwingConstants.RIGHT);
@@ -90,12 +69,9 @@ public class LauncherLoginPanel extends JBorderPanel {
 
     this.optionsButton = new JButton("Options");
     middlePanel1.add(this.optionsButton, 2);
-
     this.add(middlePanel1, BorderLayout.WEST);
-  }
 
-  private void createMiddlePanel2(GridLayout gl) {
-    JPanel middlePanel2 = new JPanel(gl);
+    JPanel middlePanel2 = new JPanel(gl, true);
     middlePanel2.setBackground(Color.GRAY);
 
     this.usernameField = new JTextField(20);
@@ -112,14 +88,14 @@ public class LauncherLoginPanel extends JBorderPanel {
   }
 
   private void createBottomPanel() {
-    JPanel bottomPanel = new JPanel(new BorderLayout());
+    JPanel bottomPanel = new JPanel(new BorderLayout(), true);
     bottomPanel.setBackground(Color.GRAY);
 
     this.linkLabel = new JLabel(this.outdated ? String.format(
         "<html><a href='%s'>You need to update the launcher!</a></html>",
-        VersionManager.GITHUB_LATEST_URL)
+        LauncherManager.GITHUB_LATEST_URL)
         : String.format("<html><a href='%s'>Need account?</a></html>",
-            VersionManager.SIGNUP_LIVE_URL), SwingConstants.LEFT);
+            LauncherManager.SIGNUP_LIVE_URL), SwingConstants.LEFT);
     this.linkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
     this.linkLabel.setForeground(Color.BLUE);
     bottomPanel.add(this.linkLabel, BorderLayout.WEST);
