68c76432bb8c86edeafc952202dc79fd220339d3
sojlabjoi <ungurkristjan@gmail.com>
2023-01-29 13:20:56 +0200

    Added buttons for closing, opening game dir & saving options.

--- src/main/java/ee/twentyten/ui/OptionsPanel.java
+++ src/main/java/ee/twentyten/ui/OptionsPanel.java
@@ -1,19 +1,46 @@
 package ee.twentyten.ui;
 
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import lombok.Getter;
 
+@Getter
 public class OptionsPanel extends JPanel {
 
+  private final OptionsTabbedPane optionsTabbedPane;
+  private JButton closeButton;
+  private JButton openGameDirectoryButton;
+  private JButton saveOptionsButton;
+
   public OptionsPanel() {
-    super(new GridBagLayout(), true);
+    super(new BorderLayout(), true);
+
+    this.optionsTabbedPane = new OptionsTabbedPane();
+    this.add(this.optionsTabbedPane);
+
+    this.setBorder(BorderFactory.createEmptyBorder(8, 8, 0, 8));
 
     this.initComponents();
   }
 
   private void initComponents() {
+    this.createBottomPanel();
   }
 
   private void createBottomPanel() {
+    JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER), true);
+    this.closeButton = new JButton("Close");
+    this.openGameDirectoryButton = new JButton("Open Game Directory");
+    this.saveOptionsButton = new JButton("Save Options");
+    bottomPanel.add(this.closeButton);
+    bottomPanel.add(
+        Box.createHorizontalStrut(this.optionsTabbedPane.getPreferredSize().width >> 2));
+    bottomPanel.add(this.openGameDirectoryButton);
+    bottomPanel.add(this.saveOptionsButton);
+    this.add(bottomPanel, BorderLayout.SOUTH);
   }
 }
