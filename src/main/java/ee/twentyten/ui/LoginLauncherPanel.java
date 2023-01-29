0d2a6a9a408ded0a62014884e7c007205b1c7337
sojlabjoi <ungurkristjan@gmail.com>
2023-01-29 13:19:15 +0200

    Renamed variables & repositioning.

--- src/main/java/ee/twentyten/ui/LoginLauncherPanel.java
+++ src/main/java/ee/twentyten/ui/LoginLauncherPanel.java
@@ -43,7 +43,7 @@ public class LoginLauncherPanel extends JBorderPanel {
 
   private void initComponents() {
     this.linkUrls =
-        this.outdated ? LauncherManager.GITHUB_LATEST_URL : LauncherManager.SIGNUP_LIVE_URL;
+        this.outdated ? LauncherManager.LATEST_RELEASE_URL : LauncherManager.ACCOUNT_SIGNUP_URL;
     this.outdated = LauncherManager.isOutdated();
 
     this.errorLabel = new JLabel("\u00A0", SwingConstants.CENTER);
@@ -59,50 +59,40 @@ public class LoginLauncherPanel extends JBorderPanel {
     GridLayout gl = new GridLayout(3, 1, 0, 2);
 
     JPanel middlePanel1 = new JPanel(gl, true);
-    middlePanel1.setBackground(Color.GRAY);
-
     this.usernameLabel = new JLabel("Username:", SwingConstants.RIGHT);
-    middlePanel1.add(this.usernameLabel, 0);
-
     this.passwordLabel = new JLabel("Password:", SwingConstants.RIGHT);
-    middlePanel1.add(this.passwordLabel, 1);
-
     this.optionsButton = new JButton("Options");
+    middlePanel1.add(this.usernameLabel, 0);
+    middlePanel1.add(this.passwordLabel, 1);
     middlePanel1.add(this.optionsButton, 2);
+    middlePanel1.setBackground(Color.GRAY);
     this.add(middlePanel1, BorderLayout.WEST);
 
     JPanel middlePanel2 = new JPanel(gl, true);
-    middlePanel2.setBackground(Color.GRAY);
-
     this.usernameField = new JTextField(20);
-    middlePanel2.add(this.usernameField, 0);
-
     this.passwordField = new JPasswordField(20);
-    middlePanel2.add(this.passwordField, 1);
-
     this.rememberPasswordCheckBox = new JCheckBox("Remember Password");
     this.rememberPasswordCheckBox.setContentAreaFilled(false);
+    middlePanel2.add(this.usernameField, 0);
+    middlePanel2.add(this.passwordField, 1);
     middlePanel2.add(this.rememberPasswordCheckBox, 2);
-
+    middlePanel2.setBackground(Color.GRAY);
     this.add(middlePanel2, BorderLayout.CENTER);
   }
 
   private void createBottomPanel() {
     JPanel bottomPanel = new JPanel(new BorderLayout(), true);
-    bottomPanel.setBackground(Color.GRAY);
-
     this.linkLabel = new JLabel(this.outdated ? String.format(
         "<html><a href='%s'>You need to update the launcher!</a></html>",
-        LauncherManager.GITHUB_LATEST_URL)
+        LauncherManager.LATEST_RELEASE_URL)
         : String.format("<html><a href='%s'>Need account?</a></html>",
-            LauncherManager.SIGNUP_LIVE_URL), SwingConstants.LEFT);
+            LauncherManager.ACCOUNT_SIGNUP_URL), SwingConstants.LEFT);
     this.linkLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
     this.linkLabel.setForeground(Color.BLUE);
-    bottomPanel.add(this.linkLabel, BorderLayout.WEST);
-
     this.loginButton = new JButton("Login");
+    bottomPanel.add(this.linkLabel, BorderLayout.WEST);
     bottomPanel.add(this.loginButton, BorderLayout.EAST);
-
+    bottomPanel.setBackground(Color.GRAY);
     this.add(bottomPanel, BorderLayout.SOUTH);
   }
 }
