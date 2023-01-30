63a860c1925ea1dc63d5c7bc8367cd1d0f61cfe2
sojlabjoi <ungurkristjan@gmail.com>
2023-01-31 00:49:03 +0200

    Added 'createDaemonThread' and 'createWorkerThread'.

--- src/main/java/ee/twentyten/util/ThreadManager.java
+++ src/main/java/ee/twentyten/util/ThreadManager.java
@@ -1,26 +1,39 @@
 package ee.twentyten.util;
 
-import ee.twentyten.core.thread.DaemonThreadFactory;
-import ee.twentyten.core.thread.WorkerThreadFactory;
+import ee.twentyten.thread.DaemonThreadFactory;
+import ee.twentyten.thread.WorkerThreadFactory;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public final class ThreadManager {
 
-  private static final ExecutorService workerThreadPool = Executors.newFixedThreadPool(10,
-      new WorkerThreadFactory());
-  private static final ExecutorService daemonThreadPool = Executors.newSingleThreadExecutor(
-      new DaemonThreadFactory());
+  private static final DaemonThreadFactory DAEMON_THREAD_FACTORY;
+  private static final WorkerThreadFactory WORKER_THREAD_FACTORY;
+
+  static {
+    DAEMON_THREAD_FACTORY = new DaemonThreadFactory();
+    WORKER_THREAD_FACTORY = new WorkerThreadFactory();
+  }
 
   private ThreadManager() {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
-  public static void executeWorkerTask(Runnable task) {
-    workerThreadPool.execute(task);
+  public static Thread createDaemonThread(Runnable r) {
+    return DAEMON_THREAD_FACTORY.newThread(r);
   }
 
-  public static void executeDaemonTask(Runnable task) {
-    daemonThreadPool.execute(task);
+  public static Thread createWorkerThread(Runnable r) {
+    return WORKER_THREAD_FACTORY.newThread(r);
+  }
+
+  public static void startThread(Thread t) {
+    t.start();
+  }
+
+  public static void runInThreadPool(Runnable r) {
+    ExecutorService es = Executors.newSingleThreadExecutor(WORKER_THREAD_FACTORY);
+    es.submit(r);
+    es.shutdown();
   }
 }
