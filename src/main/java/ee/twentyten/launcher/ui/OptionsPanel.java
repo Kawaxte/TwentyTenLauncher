40c1a56a572ba33c977ee0d37e8e100aedaedf87
sojlabjoi <ungurkristjan@gmail.com>
2023-01-31 00:51:05 +0200

    Initialise "JTabbedPane" in 'initComponents'.

--- src/main/java/ee/twentyten/launcher/ui/OptionsPanel.java
+++ src/main/java/ee/twentyten/launcher/ui/OptionsPanel.java
@@ -1,27 +1,25 @@
-package ee.twentyten.ui.panel;
+package ee.twentyten.launcher.ui;
 
-import ee.twentyten.ui.OptionsTabbedPane;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import lombok.Getter;
 
 @Getter
 public class OptionsPanel extends JPanel {
 
-  private final OptionsTabbedPane optionsTabbedPane;
-  private JButton closeButton;
+  private JTabbedPane tabbedPane;
+  private OptionsVersionsPanel optionsVersionsPanel;
+  private JButton cancelButton;
   private JButton openGameDirectoryButton;
   private JButton saveOptionsButton;
 
   public OptionsPanel() {
     super(new BorderLayout(), true);
-
-    this.optionsTabbedPane = new OptionsTabbedPane();
-    this.add(this.optionsTabbedPane);
 
     this.setBorder(BorderFactory.createEmptyBorder(8, 8, 0, 8));
 
@@ -29,17 +27,22 @@ public class OptionsPanel extends JPanel {
   }
 
   private void initComponents() {
+    this.tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+    this.optionsVersionsPanel = new OptionsVersionsPanel();
+    this.tabbedPane.add("Versions", this.optionsVersionsPanel);
+    this.add(this.tabbedPane);
+
+    this.cancelButton = new JButton("Cancel");
+    this.openGameDirectoryButton = new JButton("Open Game Directory");
+    this.saveOptionsButton = new JButton("Save Options");
     this.createBottomPanel();
   }
 
   private void createBottomPanel() {
     JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER), true);
-    this.closeButton = new JButton("Close");
-    this.openGameDirectoryButton = new JButton("Open Game Directory");
-    this.saveOptionsButton = new JButton("Save Options");
-    bottomPanel.add(this.closeButton);
+    bottomPanel.add(this.cancelButton);
     bottomPanel.add(
-        Box.createHorizontalStrut(this.optionsTabbedPane.getPreferredSize().width >> 2));
+        Box.createHorizontalStrut(this.tabbedPane.getPreferredSize().width >> 2));
     bottomPanel.add(this.openGameDirectoryButton);
     bottomPanel.add(this.saveOptionsButton);
     this.add(bottomPanel, BorderLayout.SOUTH);
