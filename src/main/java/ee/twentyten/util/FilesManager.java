651f986aa0fd0805cde0f3cf39e8133d67fa3b54
sojlabjoi <ungurkristjan@gmail.com>
2023-02-01 02:28:18 +0200

    Added 'getFilesFromUrl'.

--- src/main/java/ee/twentyten/util/FilesManager.java
+++ src/main/java/ee/twentyten/util/FilesManager.java
@@ -9,23 +9,50 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
 import javax.swing.ImageIcon;
-import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 
 public final class FilesManager {
 
-  public static final String VERSIONS_JSON_URL;
   public static final long CACHE_EXPIRATION_TIME = 86400000L;
-
-  static {
-    VERSIONS_JSON_URL = "https://raw.githubusercontent.com/sojlabjoi/AlphacraftLauncher/master/versions.json";
-  }
 
   private FilesManager() {
     throw new UnsupportedOperationException("Can't instantiate utility class");
+  }
+
+  public static String[] getFilesFromUrl(String url) {
+    List<String> fileList = new ArrayList<>();
+
+    HttpsURLConnection connection = RequestManager.sendHttpRequest(url, "GET",
+        RequestManager.X_WWW_FORM_HEADER);
+    Objects.requireNonNull(connection, "connection == null!");
+    if (connection.getContentType().startsWith("text/html")) {
+      try {
+        File f = new File(Objects.requireNonNull(connection.getURL().getFile()));
+        String content = new String(Files.readAllBytes(f.toPath()), StandardCharsets.UTF_8);
+        String[] lines = content.split("\n");
+        for (String line : lines) {
+          if (line.contains("href=")) {
+            int startIndex = line.indexOf("href=") + 6;
+            int endIndex = line.indexOf("\"", startIndex);
+
+            String file = line.substring(startIndex, endIndex);
+            if (!file.endsWith("/")) {
+              fileList.add(file);
+            }
+          }
+        }
+      } catch (IOException ioe) {
+        DebugLoggingManager.logError(FilesManager.class,
+            String.format("Failed to get files from %s", url), ioe);
+      }
+    }
+    return fileList.toArray(new String[0]);
   }
 
   public static Image readImageFile(Class<?> clazz, String name) {
@@ -52,7 +79,7 @@ public final class FilesManager {
     try (InputStream is = Objects.requireNonNull(
             RequestManager.sendHttpRequest(url, "GET", RequestManager.X_WWW_FORM_HEADER), "is == null!")
         .getInputStream()) {
-      FileUtils.copyInputStreamToFile(is, src);
+      Files.copy(is, src.toPath());
 
       DebugLoggingManager.logInfo(FilesManager.class,
           String.format("\"%s\"", src.getAbsolutePath()));
@@ -64,10 +91,10 @@ public final class FilesManager {
 
   public static void moveFile(File src, File dest) {
     try {
-      FileUtils.moveFile(src, dest);
+      Files.move(src.toPath(), dest.toPath());
     } catch (IOException ioe1) {
       try (FileOutputStream fos = new FileOutputStream(dest)) {
-        FileUtils.copyFile(src, fos);
+        Files.copy(src.toPath(), fos);
 
         DebugLoggingManager.logInfo(FilesManager.class,
             String.format("\"%s\"", dest.getAbsolutePath()));
@@ -86,17 +113,19 @@ public final class FilesManager {
 
   public static void deleteFile(File src) {
     try {
-      FileUtils.delete(src);
+      boolean deleted = src.delete();
+      if (!deleted) {
+        throw new IOException(
+            String.format("Failed to delete file from %s", src.getAbsolutePath()));
+      }
 
       DebugLoggingManager.logInfo(FilesManager.class,
           String.format("\"%s\"", src.getAbsolutePath()));
-    } catch (IOException ioe1) {
-      try {
-        FileUtils.forceDelete(src);
-      } catch (IOException ioe2) {
-        DebugLoggingManager.logError(FilesManager.class,
-            String.format("Failed to delete file from %s", src.getAbsolutePath()), ioe2);
-      }
+    } catch (IOException ioe) {
+      src.deleteOnExit();
+
+      DebugLoggingManager.logError(FilesManager.class,
+          String.format("Failed to delete file from %s", src.getAbsolutePath()), ioe);
     }
   }
 }
