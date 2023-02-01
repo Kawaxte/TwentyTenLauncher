2d0c55b57294d485b877ef5d7d3666d3aab0ba80
sojlabjoi <ungurkristjan@gmail.com>
2023-02-01 02:24:15 +0200

    Changed 'newThread' to syncronized.

--- src/main/java/ee/twentyten/thread/WorkerThreadFactory.java
+++ src/main/java/ee/twentyten/thread/WorkerThreadFactory.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ThreadFactory;
 public class WorkerThreadFactory implements ThreadFactory {
 
   @Override
-  public Thread newThread(Runnable r) {
+  public synchronized Thread newThread(Runnable r) {
     Thread t = new Thread(r);
     t.setName(String.format("WorkerThread-%d", t.getId()));
     t.setDaemon(false);
