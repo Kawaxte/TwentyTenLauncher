8b698afa5ae69a5982fd77a03d67ea586ba5edbf
sojlabjoi <ungurkristjan@gmail.com>
2023-02-01 02:48:01 +0200

    Removed 'startThread' & 'runInThreadPool'.

--- src/main/java/ee/twentyten/util/ThreadManager.java
+++ src/main/java/ee/twentyten/util/ThreadManager.java
@@ -2,8 +2,6 @@ package ee.twentyten.util;
 
 import ee.twentyten.thread.DaemonThreadFactory;
 import ee.twentyten.thread.WorkerThreadFactory;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public final class ThreadManager {
 
@@ -19,21 +17,11 @@ public final class ThreadManager {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
-  public static Thread createDaemonThread(Runnable r) {
-    return DAEMON_THREAD_FACTORY.newThread(r);
+  public static Thread createDaemonThread(String name, Runnable r) {
+    return DAEMON_THREAD_FACTORY.newThread(name, r);
   }
 
-  public static Thread createWorkerThread(Runnable r) {
-    return WORKER_THREAD_FACTORY.newThread(r);
-  }
-
-  public static void startThread(Thread t) {
-    t.start();
-  }
-
-  public static void runInThreadPool(Runnable r) {
-    ExecutorService es = Executors.newSingleThreadExecutor(WORKER_THREAD_FACTORY);
-    es.submit(r);
-    es.shutdown();
+  public static Thread createWorkerThread(String name, Runnable r) {
+    return WORKER_THREAD_FACTORY.newThread(name, r);
   }
 }
