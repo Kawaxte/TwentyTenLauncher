82e6d2b91cab9173bc8895200203f81bc9e72dd7
sojlabjoi <ungurkristjan@gmail.com>
2023-02-03 23:20:24 +0200

    Added 'UTF-8' encoring value to bytes.

--- src/main/java/ee/twentyten/custom/CustomLinkedProperties.java
+++ src/main/java/ee/twentyten/custom/CustomLinkedProperties.java
@@ -3,6 +3,7 @@ package ee.twentyten.custom;
 import ee.twentyten.util.DebugLoggingManager;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -35,20 +36,21 @@ public class CustomLinkedProperties extends Properties {
   public void store(OutputStream out, String comments) {
     try {
       if (comments != null) {
-        out.write("# ".getBytes());
-        out.write(comments.getBytes());
-        out.write(System.lineSeparator().getBytes());
+        out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+        out.write("# ".getBytes(StandardCharsets.UTF_8));
+        out.write(comments.getBytes(StandardCharsets.UTF_8));
+        out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
       }
 
       Set<Map.Entry<Object, Object>> entries = entrySet();
       for (Map.Entry<Object, Object> entry : entries) {
         String key = (String) entry.getKey();
-        out.write(key.getBytes());
-        out.write("=".getBytes());
+        out.write(key.getBytes(StandardCharsets.UTF_8));
+        out.write("=".getBytes(StandardCharsets.UTF_8));
 
         String value = (String) entry.getValue();
-        out.write(value.getBytes());
-        out.write(System.lineSeparator().getBytes());
+        out.write(value.getBytes(StandardCharsets.UTF_8));
+        out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
       }
     } catch (IOException ioe) {
       DebugLoggingManager.logError(this.getClass(), "Failed to store properties", ioe);
