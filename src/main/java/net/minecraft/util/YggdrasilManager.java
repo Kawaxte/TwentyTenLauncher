1ab3ea4b1afadd830db9b73501180892bf6a89a3
sojlabjoi <ungurkristjan@gmail.com>
2023-02-05 03:58:21 +0200

    Replaced thrown exception with ERROR logging.

--- src/main/java/net/minecraft/util/YggdrasilManager.java
+++ src/main/java/net/minecraft/util/YggdrasilManager.java
@@ -2,9 +2,8 @@ package net.minecraft.util;
 
 import ee.twentyten.config.Config;
 import ee.twentyten.launcher.ui.LauncherFrame;
-import ee.twentyten.util.DebugLoggingManager;
+import ee.twentyten.util.LoggingManager;
 import net.minecraft.authentication.mojang.YggdrasilAuthImpl;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class YggdrasilManager {
@@ -17,26 +16,28 @@ public class YggdrasilManager {
     YGGDRASIL_AUTH = new YggdrasilAuthImpl();
   }
 
-  public static JSONObject authenticateUser(String username, String password, String clientToken,
+  public static JSONObject login(String username, String password, String clientToken,
       boolean requestUser) {
     JSONObject result = YGGDRASIL_AUTH.authenticate(username, password, clientToken, requestUser);
     if (result.has("error")) {
-      DebugLoggingManager.logError(YggdrasilManager.class, result.toString());
+      LoggingManager.logError(YggdrasilManager.class, result.toString());
       return result;
     }
 
-    DebugLoggingManager.logInfo(YggdrasilManager.class, result.toString());
+    LauncherFrame.sessionValid = true;
+
+    LoggingManager.logInfo(YggdrasilManager.class, result.toString());
     return result;
   }
 
   public static JSONObject validateAccessToken(String accessToken, String clientToken) {
     JSONObject result = YGGDRASIL_AUTH.validate(accessToken, clientToken);
     if (result.has("error")) {
-      DebugLoggingManager.logError(YggdrasilManager.class, result.toString());
+      LoggingManager.logError(YggdrasilManager.class, result.toString());
       return result;
     }
 
-    DebugLoggingManager.logInfo(YggdrasilManager.class, result.toString());
+    LoggingManager.logInfo(YggdrasilManager.class, result.toString());
     return result;
   }
 
@@ -44,11 +45,11 @@ public class YggdrasilManager {
       boolean requestUser) {
     JSONObject result = YGGDRASIL_AUTH.refresh(accessToken, clientToken, requestUser);
     if (result.has("error")) {
-      DebugLoggingManager.logError(YggdrasilManager.class, result.toString());
+      LoggingManager.logError(YggdrasilManager.class, result.toString());
       return result;
     }
 
-    DebugLoggingManager.logInfo(YggdrasilManager.class, result.toString());
+    LoggingManager.logInfo(YggdrasilManager.class, result.toString());
     return result;
   }
 
@@ -57,14 +58,16 @@ public class YggdrasilManager {
     if (validate.has("error")) {
       JSONObject refresh = YggdrasilManager.refreshAccessToken(accessToken, clientToken, true);
       if (refresh.has("error")) {
-        throw new JSONException("Failed to refresh access token");
+        LauncherFrame.sessionValid = false;
+
+        LoggingManager.logError(YggdrasilManager.class, "Failed to refresh access token");
+        return;
       }
       String newAccessToken = refresh.getString("accessToken");
       String newClientToken = refresh.getString("clientToken");
       Config.instance.setAccessToken(newAccessToken);
       Config.instance.setClientToken(newClientToken);
       Config.instance.save();
-      return;
     }
     LauncherFrame.sessionValid = true;
   }
