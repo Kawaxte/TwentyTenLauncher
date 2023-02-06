1fadbdfbf5858fdf2004c308bbb5c4811965fd52
sojlabjoi <ungurkristjan@gmail.com>
2023-02-06 18:13:15 +0200

    Moved 'arguments' list into IF statement.

--- src/main/java/ee/twentyten/Launcher.java
+++ src/main/java/ee/twentyten/Launcher.java
@@ -1,7 +1,7 @@
-package ee.twentyten.launcher;
+package ee.twentyten;
 
-import ee.twentyten.launcher.ui.LauncherFrame;
-import ee.twentyten.util.LoggingManager;
+import ee.twentyten.ui.LauncherFrame;
+import ee.twentyten.util.LogHelper;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,24 +11,27 @@ public class Launcher {
 
   private static final long MIN_MEMORY;
   private static final long MAX_MEMORY;
+  private static final Class<Launcher> CLASS_REF;
 
   static {
     MIN_MEMORY = 524288L;
     MAX_MEMORY = Runtime.getRuntime().maxMemory();
+
+    CLASS_REF = Launcher.class;
   }
 
   public static void main(String[] args) {
-    List<String> arguments = new ArrayList<>();
-    arguments.add(EPlatform.getPlatform() == EPlatform.WINDOWS ? "javaw" : "java");
-    arguments.add("-Xmx1024m");
-    arguments.add("-Dsun.java2d.d3d=false");
-    arguments.add("-Dsun.java2d.opengl=false");
-    arguments.add("-Dsun.java2d.pmoffscreen=false");
-    arguments.add("-cp");
-    arguments.add(System.getProperty("java.class.path"));
-    arguments.add(LauncherFrame.class.getName());
-
     if (MAX_MEMORY < MIN_MEMORY) {
+      List<String> arguments = new ArrayList<>();
+      arguments.add(EPlatform.getPlatform() == EPlatform.WINDOWS ? "javaw" : "java");
+      arguments.add("-Xmx1024m");
+      arguments.add("-Dsun.java2d.d3d=false");
+      arguments.add("-Dsun.java2d.opengl=false");
+      arguments.add("-Dsun.java2d.pmoffscreen=false");
+      arguments.add("-cp");
+      arguments.add(System.getProperty("java.class.path"));
+      arguments.add(CLASS_REF.getName());
+
       ProcessBuilder pb = new ProcessBuilder(arguments);
       try {
         Process process = pb.start();
@@ -40,13 +43,13 @@ public class Launcher {
             String.format("An error occurred while starting the process:%n%s", ioe.getMessage()),
             "Error", JOptionPane.ERROR_MESSAGE);
 
-        LoggingManager.logError(Launcher.class, "Failed to start the process", ioe);
+        LogHelper.logError(CLASS_REF, "Failed to start the process", ioe);
       } catch (InterruptedException ie) {
         JOptionPane.showMessageDialog(null,
             String.format("An error occurred while waiting for the process to terminate:%n%s",
                 ie.getMessage()), "Error", JOptionPane.ERROR_MESSAGE);
 
-        LoggingManager.logError(Launcher.class, "Failed to wait for the process to terminate",
+        LogHelper.logError(CLASS_REF, "Failed to wait for the process to terminate",
             ie);
       }
     } else {
