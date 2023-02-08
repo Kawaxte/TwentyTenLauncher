c9e13b741332a695bfc01e31c482f3b37517a1e6
sojlabjoi <ungurkristjan@gmail.com>
2023-02-08 20:51:19 +0200

    'getAgent' no longer part of "YggdrasilAuthentication".

--- src/main/java/com/mojang/YggdrasilAuthentication.java
+++ src/main/java/com/mojang/YggdrasilAuthentication.java
@@ -4,8 +4,6 @@ import org.json.JSONObject;
 
 public abstract class YggdrasilAuthentication implements IYggdrasilAuthenticationService {
 
-  public abstract JSONObject getAgent();
-
   public abstract JSONObject authenticate(String username, String password, String clientToken,
       boolean requestUser);
 
