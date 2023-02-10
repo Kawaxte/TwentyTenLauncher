0ab5fcff205c363c44a59148e46b4ce43a8d0a07
sojlabjoi <ungurkristjan@gmail.com>
2023-02-10 23:34:54 +0200

    Added localisation values within constants

--- src/main/java/net/minecraft/update/EState.java
+++ src/main/java/net/minecraft/update/EState.java
@@ -1,26 +1,26 @@
 package net.minecraft.update;
 
+import ee.twentyten.util.LanguageHelper;
 import lombok.Getter;
+import lombok.Setter;
 
 public enum EState {
-  INIT_STATE("Initialising loader"),
-  CACHE_STATE("Checking cache for existing files"),
-  DETERMINE_STATE("Determining packages"),
-  DOWNLOAD_STATE("Downloading packages"),
-  MOVE_STATE("Moving downloaded packages"),
-  CLASSPATH_STATE("Updating classpath"),
-  DONE_STATE("Done loading");
+  INIT_STATE(LanguageHelper.getString("es.string.initState.text")),
+  CACHE_STATE(LanguageHelper.getString("es.string.cacheState.text")),
+  DETERMINE_STATE(LanguageHelper.getString("es.string.determineState.text")),
+  RETRIEVE_STATE(LanguageHelper.getString("es.string.retrieveState.text")),
+  DOWNLOAD_STATE(LanguageHelper.getString("es.string.downloadState.text")),
+  MOVE_STATE(LanguageHelper.getString("es.string.moveState.text")),
+  CLASSPATH_STATE(LanguageHelper.getString("es.string.classpathState.text")),
+  DONE_STATE(LanguageHelper.getString("es.string.doneState.text"));
 
   @Getter
+  @Setter
   private static EState state;
   @Getter
   private final String message;
 
   EState(String message) {
     this.message = message;
-  }
-
-  public static void setState(EState state) {
-    EState.state = state;
   }
 }
