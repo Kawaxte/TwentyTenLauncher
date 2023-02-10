548b296994815cc3f0989896ffcb0b9bea2a7360
sojlabjoi <ungurkristjan@gmail.com>
2023-02-10 23:32:41 +0200

    Added Strings for localisation

--- src/main/java/ee/twentyten/ui/launcher/LauncherOfflinePanel.java
+++ src/main/java/ee/twentyten/ui/launcher/LauncherOfflinePanel.java
@@ -1,6 +1,7 @@
 package ee.twentyten.ui.launcher;
 
 import ee.twentyten.custom.CustomJPanel;
+import ee.twentyten.util.LanguageHelper;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -15,6 +16,9 @@ import lombok.Getter;
 public class LauncherOfflinePanel extends CustomJPanel {
 
   private static final long serialVersionUID = 1L;
+  private final String playOnlineLabelText;
+  private final String playOfflineButtonText;
+  private final String tryAgainButtonText;
   @Getter
   private JLabel errorLabel;
   @Getter
@@ -23,6 +27,12 @@ public class LauncherOfflinePanel extends CustomJPanel {
   private JButton playOfflineButton;
   @Getter
   private JButton tryAgainButton;
+
+  {
+    this.playOnlineLabelText = LanguageHelper.getString("lop.label.playOnlineLabel.text");
+    this.playOfflineButtonText = LanguageHelper.getString("lop.button.playOfflineButton.text");
+    this.tryAgainButtonText = LanguageHelper.getString("lop.button.tryAgainButton.text");
+  }
 
   public LauncherOfflinePanel() {
     super(new BorderLayout(0, 8), true);
@@ -38,7 +48,7 @@ public class LauncherOfflinePanel extends CustomJPanel {
     this.errorLabel.setForeground(Color.RED.darker());
     this.add(this.errorLabel, BorderLayout.NORTH);
 
-    this.playOnlineLabel = new JLabel("Play online once to enable offline", SwingConstants.LEFT);
+    this.playOnlineLabel = new JLabel(this.playOnlineLabelText, SwingConstants.LEFT);
 
     this.createBottomPanel();
   }
@@ -48,12 +58,11 @@ public class LauncherOfflinePanel extends CustomJPanel {
     bottomPanel.setBackground(Color.GRAY);
     this.add(bottomPanel, BorderLayout.SOUTH);
 
-    this.playOfflineButton = new JButton("Play offline");
+    this.playOfflineButton = new JButton(this.playOfflineButtonText);
     bottomPanel.add(this.playOfflineButton, BorderLayout.WEST);
-
     bottomPanel.add(Box.createHorizontalBox());
 
-    this.tryAgainButton = new JButton("Try again");
+    this.tryAgainButton = new JButton(this.tryAgainButtonText);
     bottomPanel.add(this.tryAgainButton, BorderLayout.EAST);
   }
 }
