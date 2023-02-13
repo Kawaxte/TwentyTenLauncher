0e339918bafc5878cc3b05f60715e69be3f9dbd2
sojlabjoi <ungurkristjan@gmail.com>
2023-02-13 19:42:22 +0200

    Add documentation, Move version method(s) -> LauncherVersionHelper

--- src/main/java/ee/twentyten/util/LauncherHelper.java
+++ src/main/java/ee/twentyten/util/LauncherHelper.java
@@ -4,125 +4,125 @@ import ee.twentyten.EPlatform;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ExecutionException;
-import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public final class LauncherHelper {
 
-  private static final String USER_HOME;
-  private static final String APPDATA;
-  private static final Map<EPlatform, File> WORKING_DIRECTORIES;
-  private static final String LATEST_RELEASE_API_URL;
+
+  static String userHome;
+  static String appData;
+  static Map<EPlatform, File> workingDirectories;
 
   static {
-    LATEST_RELEASE_API_URL = "https://api.github.com/repos/sojlabjoi/AlphacraftLauncher/releases/latest";
+    LauncherHelper.userHome = System.getProperty("user.home", ".");
+    LauncherHelper.appData = System.getenv("APPDATA");
 
-    USER_HOME = System.getProperty("user.home", ".");
-    APPDATA = System.getenv("APPDATA");
-
-    WORKING_DIRECTORIES = new HashMap<>();
+    LauncherHelper.workingDirectories = new HashMap<>();
   }
 
+  /**
+   * Prevents instantiation of this class.
+   *
+   * @throws UnsupportedOperationException if this method is called.
+   */
   private LauncherHelper() {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
-
-  public static void getCurrentVersion(int release, int month, int day, int year,
-      boolean preRelease, int preVersion) {
-    if (month < 1 || month > 12) {
-      LoggerHelper.logError("month < 1 || month > 12", false);
-      return;
-    }
-    if (day < 1 || day > 31) {
-      LoggerHelper.logError("day < 1 || day > 31", false);
-      return;
-    }
-    if (preRelease && preVersion < 0) {
-      LoggerHelper.logError("preRelease && preVersion < 0", false);
-      return;
-    }
-
-    String preReleaseString = preRelease ? String.format("_pre%d", preVersion) : "";
-    String currentVersion = String.format("%d.%01d.%02d%02d%s", release, month, day, year,
-        preReleaseString);
-    System.setProperty("ee.twentyten.version", currentVersion);
-  }
-
-  private static void getWorkingDirectoryForPlatform() {
-    File workingDirectoryForMacOsx = new File(
-        String.format("%s/Library/Application Support", USER_HOME), "twentyten");
-    WORKING_DIRECTORIES.put(EPlatform.MACOSX, workingDirectoryForMacOsx);
-
-    File workingDirectoryForLinux = new File(USER_HOME, ".twentyten");
-    WORKING_DIRECTORIES.put(EPlatform.LINUX, workingDirectoryForLinux);
-
-    File workingDirectoryForWindows =
-        APPDATA != null ? new File(APPDATA, ".twentyten") : new File(USER_HOME, ".twentyten");
-    WORKING_DIRECTORIES.put(EPlatform.WINDOWS, workingDirectoryForWindows);
-  }
-
+  /**
+   * Get the working directory for the current platform.
+   * <p>
+   * The working directory is determined by the platform being used and will be
+   * either `~/Library/Application Support/twentyten` on MacOSX, `~/.twentyten`
+   * on Linux, or `%APPDATA%/.twentyten` on Windows.
+   * <p>
+   * If the working directory does not exist, this method will attempt to create
+   * it. If it cannot be created, an error will be logged and `null` will be
+   * returned.
+   *
+   * @return The working directory for the current platform, or `null` if it
+   * cannot be determined or created.
+   */
   public static File getWorkingDirectory() {
-    LauncherHelper.getWorkingDirectoryForPlatform();
+    LauncherHelper.mapWorkingDirectoryToPlatform();
 
     EPlatform platformName = EPlatform.getPlatform();
+    File workingDirectory = LauncherHelper.workingDirectories.get(platformName);
+    if (workingDirectory == null) {
+      String notFoundError = "No working directory found";
 
-    File workingDirectory = LauncherHelper.WORKING_DIRECTORIES.get(platformName);
-    Objects.requireNonNull(workingDirectory, "workingDirectory == null!");
-    if (!workingDirectory.exists()) {
-      LoggerHelper.logInfo(String.format("\"%s\"", workingDirectory.getAbsolutePath()), true);
-
-      boolean created = workingDirectory.mkdirs();
-      if (!created) {
-        Throwable t = new Throwable("Failed to create working directory");
-
-        LoggerHelper.logError(t.getMessage(), t, true);
-        return null;
-      }
+      LoggerHelper.logError(notFoundError, true);
+      return null;
     }
+    if (!workingDirectory.exists() && !workingDirectory.mkdirs()) {
+      String creationError = "Failed to create working directory";
+
+      LoggerHelper.logError(creationError, true);
+      return null;
+    }
+
     return workingDirectory;
   }
 
+  /**
+   * This method sets the look and feel for the application. It tries to set the
+   * look and feel to the system look and feel.
+   */
   public static void setLookAndFeel() {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     } catch (UnsupportedLookAndFeelException ulafe) {
       LoggerHelper.logError(ulafe.getMessage(), ulafe, true);
     } catch (ClassNotFoundException cnfe) {
-      LoggerHelper.logError("Can't find look and feel class", cnfe, true);
+      String notFoundError = "Can't find look and feel class";
+
+      LoggerHelper.logError(notFoundError, cnfe, true);
     } catch (InstantiationException ie) {
-      LoggerHelper.logError("Can't instantiate look and feel class", ie, true);
+      String instantiationError = "Can't instantiate look and feel class";
+
+      LoggerHelper.logError(instantiationError, ie, true);
     } catch (IllegalAccessException iae) {
-      LoggerHelper.logError("Can't access look and feel class", iae, true);
+      String accessError = "Can't access look and feel class";
+
+      LoggerHelper.logError(accessError, iae, true);
     }
   }
 
-  public static boolean isOutdated() {
-    SwingWorker<Boolean, Void> worker = new SwingWorker<Boolean, Void>() {
-      @Override
-      protected Boolean doInBackground() {
-        RequestHelper.performJsonRequest(LATEST_RELEASE_API_URL, "GET", RequestHelper.jsonHeader,
-            true);
+  /**
+   * Maps the working directory of the application to the corresponding
+   * platform.
+   *
+   * <p>For MACOSX, the working directory will be located at:
+   * <code>userHome/Library/Application Support/twentyten</code>
+   *
+   * <p>For LINUX, the working directory will be located at:
+   * <code>userHome/.twentyten</code>
+   *
+   * <p>For WINDOWS, the working directory will be located at:
+   * <code>appData/.twentyten</code>
+   * If appData is null, it will be located at:
+   * <code>userHome/.twentyten</code>
+   */
+  private static void mapWorkingDirectoryToPlatform() {
+    LauncherHelper.workingDirectories.put(
+        EPlatform.MACOSX,
+        new File(String.format(
+            "%s/Library/Application Support",
+            userHome), "twentyten")
+    );
 
-        String currentVersion = System.getProperty("ee.twentyten.version");
-        String latestVersion = RequestHelper.jsonHeader.get("tag_name");
-        return !currentVersion.equals(latestVersion);
-      }
-    };
-    worker.execute();
+    LauncherHelper.workingDirectories.put(
+        EPlatform.LINUX,
+        new File(userHome, ".twentyten")
+    );
 
-    try {
-      return worker.get();
-    } catch (InterruptedException ie) {
-      Thread.currentThread().interrupt();
-
-      LoggerHelper.logError("Failed to interrupt current thread", ie, true);
-    } catch (ExecutionException ee) {
-      LoggerHelper.logError("Failed to check for updates", ee, true);
-    }
-    return false;
+    File appDataFile = new File(appData, ".twentyten");
+    LauncherHelper.workingDirectories.put(
+        EPlatform.WINDOWS,
+        appData != null
+            ? appDataFile
+            : new File(userHome, ".twentyten")
+    );
   }
 }
