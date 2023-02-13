2eb9fb948fa88952414af84c31515b7bde988cfa
sojlabjoi <ungurkristjan@gmail.com>
2023-02-13 21:09:52 +0200

    Add Finnish language, Add documentation

--- src/main/java/ee/twentyten/lang/ELanguage.java
+++ src/main/java/ee/twentyten/lang/ELanguage.java
@@ -1,33 +1,46 @@
 package ee.twentyten.lang;
 
 import lombok.Getter;
-import lombok.Setter;
 
 public enum ELanguage {
   LANGUAGE_CS("Čeština"),
   LANGUAGE_DE("Deutsch"),
   LANGUAGE_ET("Eesti"),
   LANGUAGE_EN("English"),
+  LANGUAGE_FI("Suomi"),
   LANGUAGE_FR("Français"),
   LANGUAGE_HU("Magyar"),
-  LANGUAGE_JP("日本語"),
+  LANGUAGE_JA("日本語"),
   LANGUAGE_PL("Polski");
 
-  @Setter
-  public static ELanguage language;
   @Getter
   private final String name;
 
   ELanguage(String name) {
+
+    /* Set the name */
     this.name = name;
   }
 
-  public static ELanguage getLanguage(String lang) {
+  /**
+   * Gets the language of the application based on the input string.
+   *
+   * @param lang the string representing the language code
+   */
+  public static void getLanguage(String lang) {
+
+    /* Loop through all the languages */
     for (ELanguage language : values()) {
-      if (language.name().substring(9).equalsIgnoreCase(lang)) {
-        return language;
+
+      /* Get the language code from the enum */
+      String languageCode = language.name().substring(9);
+
+      /* Check if the language code is valid */
+      if (languageCode.equalsIgnoreCase(lang)) {
+
+        /* Set the language */
+        return;
       }
     }
-    return LANGUAGE_EN;
   }
 }
