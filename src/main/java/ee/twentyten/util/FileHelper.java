102aad9fe56dd27d2125edcb4d8ccb17a97f8d43
sojlabjoi <ungurkristjan@gmail.com>
2023-02-14 22:20:50 +0200

    Update documentation

--- src/main/java/ee/twentyten/util/FileHelper.java
+++ src/main/java/ee/twentyten/util/FileHelper.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import javax.imageio.ImageIO;
 import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONObject;
@@ -25,59 +26,34 @@ public final class FileHelper {
     FileHelper.workingDirectory = LauncherHelper.getWorkingDirectory();
   }
 
-  /**
-   * Prevents instantiation of this class.
-   *
-   * @throws UnsupportedOperationException if this method is called.
-   */
   private FileHelper() {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
-  /**
-   * Reads an image file using the specified class and name.
-   *
-   * @param clazz The class to use to find the image file
-   * @param name  The name of the image file
-   * @return An optional that contains the image file's data as a
-   * `BufferedImage` if it is found and successfully read, or an empty optional
-   * if it is not found or there was a failure in reading the image file.
-   */
   public static Optional<BufferedImage> readImageFile(
       Class<?> clazz,
       String name
   ) {
-
-    /* Try to read the image file. */
     try {
 
       /* Get the URL of the image file. */
       URL input = clazz.getClassLoader().getResource(name);
 
-      /* If the URL is not null, read the image file and return an Optional
-       * containing the image file's data as a BufferedImage. */
-      if (input != null) {
-        return Optional.of(ImageIO.read(input));
-      }
+      /* 'NullPointerException' will be thrown here if the image file does not
+       * exist. */
+      Objects.requireNonNull(input, "input == null!");
+
+      /* Read the image file and return an Optional containing the image. */
+      return Optional.of(ImageIO.read(input));
     } catch (IOException ioe) {
-
-      /* Create a string for the error message. */
-      String errorString = "Failed to read image file";
-
-      /* Log the error. */
-      LoggerHelper.logError(errorString, ioe, true);
+      LoggerHelper.logError(
+          "Failed to read image file",
+          ioe, true
+      );
     }
     return Optional.empty();
   }
 
-  /**
-   * Reads the contents of a JSON file.
-   *
-   * @param src The file to be read.
-   * @return An Optional containing the JSONObject representation of the file if
-   * the file exists and is readable, or an empty Optional if the file does not
-   * exist or is not a file.
-   */
   public static Optional<JSONObject> readJsonFile(
       File src
   ) {
@@ -87,7 +63,6 @@ public final class FileHelper {
       return Optional.empty();
     }
 
-    /* Try to read the file's contents. */
     try {
 
       /* Read the file's contents into a byte array. */
@@ -96,27 +71,19 @@ public final class FileHelper {
       /* Convert the byte array to a string. */
       String json = new String(bytes, StandardCharsets.UTF_8);
 
-      /* Return an Optional containing the JSONObject representation of the
-       * file. */
-      return Optional.of(new JSONObject(json));
+      /* Create a new JSONObject from the string and return an Optional
+       * containing the JSONObject. */
+      JSONObject jsonObject = new JSONObject(json);
+      return Optional.of(jsonObject);
     } catch (IOException ioe) {
-
-      /* Create a string for the error message. */
-      String errorString = "Failed to read bytes from file";
-
-      /* Log the error. */
-      LoggerHelper.logError(errorString, ioe, true);
+      LoggerHelper.logError(
+          "Failed to read bytes from file",
+          ioe, true
+      );
     }
     return Optional.empty();
   }
 
-  /**
-   * Creates a directory at the specified location.
-   *
-   * @param parent the parent directory
-   * @param name   the name of the directory
-   * @return the newly created directory, or `null` if creation failed
-   */
   public static File createDirectory(
       File parent,
       String name
@@ -130,25 +97,16 @@ public final class FileHelper {
 
     /* Create the directory. */
     boolean isDirectoryCreated = directory.mkdirs();
-
-    /* If the directory was not created, log the error and return null. */
     if (!isDirectoryCreated) {
-
-      /* Create a new throwable to log. */
-      Throwable t = new Throwable("Failed to create directory");
-
-      /* Log the error. */
-      LoggerHelper.logError(t.getMessage(), t, true);
+      LoggerHelper.logError(
+          "Failed to create directory",
+          true
+      );
       return null;
     }
     return directory;
   }
 
-  /**
-   * Deletes a directory and its contents recursively.
-   *
-   * @param directory The directory to be deleted.
-   */
   public static void deleteDirectory(
       File directory
   ) {
@@ -158,14 +116,12 @@ public final class FileHelper {
       return;
     }
 
-    /* If the directory is not a directory, delete it and return. */
+    /* Get the directory's contents. */
     File[] files = directory.listFiles();
-    if (files == null) {
-      String listError = "Failed to list files in directory";
 
-      LoggerHelper.logError(listError, true);
-      return;
-    }
+    /* 'NullPointerException' will be thrown here when the directory is not
+     * a directory. */
+    Objects.requireNonNull(files, "files == null!");
 
     /* Delete the directory's contents. */
     for (File file : files) {
@@ -174,34 +130,24 @@ public final class FileHelper {
       } else {
         boolean isFileDeleted = file.delete();
         if (!isFileDeleted) {
-          Throwable t = new Throwable("Failed to delete file");
-
-          LoggerHelper.logError(t.getMessage(), t, true);
+          LoggerHelper.logError(
+              "Failed to delete file",
+              true
+          );
         }
       }
     }
 
     /* Delete the directory. */
     boolean isDirectoryDeleted = directory.delete();
-
-    /* If the directory was not deleted, log the error. */
     if (!isDirectoryDeleted) {
-
-      /* Create a new throwable to log. */
-      Throwable t = new Throwable("Failed to delete directory");
-
-      /* Log the error. */
-      LoggerHelper.logError(t.getMessage(), t, true);
+      LoggerHelper.logError(
+          "Failed to delete directory",
+          true
+      );
     }
   }
 
-  /**
-   * Downloads a file from a specified URL and saves it to the specified target
-   * file.
-   *
-   * @param url    The URL of the file to be downloaded.
-   * @param target The target file to save the downloaded file to.
-   */
   public static void downloadFile(
       String url,
       File target
@@ -211,31 +157,27 @@ public final class FileHelper {
     HttpsURLConnection connection = RequestHelper.performHttpsRequest(
         url,
         EMethod.GET,
-        RequestHelper.xWwwFormUrlencodedHeader);
+        RequestHelper.xWwwFormUrlencodedHeader
+    );
 
-    /* If the connection is null, log the error and return. */
-    if (connection == null) {
-      LoggerHelper.logError("Failed to establish connection", true);
-      return;
-    }
+    /* 'NullPointerException' will be thrown here because without a connection
+     * there is no point in continuing. */
+    Objects.requireNonNull(connection, "connection == null!");
 
-    /* Try to download the file. */
+    /* Get the connection's input stream. */
     try (InputStream is = connection.getInputStream()) {
 
       /* Copy the file to the target file. */
       Files.copy(
           is,
           target.toPath(),
-          StandardCopyOption.REPLACE_EXISTING);
+          StandardCopyOption.REPLACE_EXISTING
+      );
     } catch (IOException ioe) {
-
-      /* Create a string for the error message. */
-      String errorString = String.format(
-          "%s -> %s",
-          url, target.getAbsolutePath());
-
-      /* Log the error. */
-      LoggerHelper.logError(errorString, ioe, true);
+      LoggerHelper.logError(
+          "Failed to download file",
+          ioe, true
+      );
     }
   }
 }
