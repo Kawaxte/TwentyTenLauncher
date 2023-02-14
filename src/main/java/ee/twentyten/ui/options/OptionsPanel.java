66e93237542f996d2599e80b2533c0bf7bd38e7c
sojlabjoi <ungurkristjan@gmail.com>
2023-02-15 01:26:45 +0200

    Add 'createTopTabbedPane' & size variable(s)

--- src/main/java/ee/twentyten/ui/options/OptionsPanel.java
+++ src/main/java/ee/twentyten/ui/options/OptionsPanel.java
@@ -1,69 +1,95 @@
 package ee.twentyten.ui.options;
 
-import ee.twentyten.util.LanguageHelper;
+import ee.twentyten.lang.LauncherLanguage;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.border.Border;
 import lombok.Getter;
 
 @Getter
 public class OptionsPanel extends JPanel {
 
   private static final long serialVersionUID = 1L;
-  private final String versionTabTitle;
-  private final String languageTabTitle;
+  private final String versionTabText;
+  private final String languageTabText;
   private final String cancelButtonText;
   private final String openGameDirectoryButtonText;
   private final String saveOptionsButtonText;
+  private final OptionsVersionPanel optionsVersionPanel;
+  private final OptionsLanguagePanel optionsLanguagePanel;
   private JTabbedPane tabbedPane;
-  private OptionsVersionPanel optionsVersionPanel;
-  private OptionsLanguagePanel optionsLanguagePanel;
   private JButton cancelButton;
   private JButton openGameDirectoryButton;
   private JButton saveOptionsButton;
 
   {
-    this.versionTabTitle = LanguageHelper.getString("ovp.string.title.text");
-    this.languageTabTitle = LanguageHelper.getString("olp.string.title.text");
-    this.cancelButtonText = LanguageHelper.getString("op.button.cancelButton.text");
-    this.openGameDirectoryButtonText = LanguageHelper.getString(
-        "op.button.openGameDirectoryButton.text");
-    this.saveOptionsButtonText = LanguageHelper.getString("op.button.saveOptionsButton.text");
+    this.versionTabText = LauncherLanguage
+        .getString("ovp.string.title");
+    this.languageTabText = LauncherLanguage
+        .getString("olp.string.title");
+    this.cancelButtonText = LauncherLanguage
+        .getString("op.button.cancelButton");
+    this.openGameDirectoryButtonText = LauncherLanguage
+        .getString("op.button.openGameDirectoryButton");
+    this.saveOptionsButtonText = LauncherLanguage
+        .getString("op.button.saveOptionsButton");
   }
 
   public OptionsPanel() {
     super(new BorderLayout(), true);
 
-    this.initComponents();
+    Border border = BorderFactory.createEmptyBorder(
+        8, 8, 8, 8);
+    this.setBorder(border);
+
+    BorderLayout borderLayout = new BorderLayout(
+        0, 8);
+    this.optionsVersionPanel = new OptionsVersionPanel(
+        borderLayout, true
+    );
+    this.optionsLanguagePanel = new OptionsLanguagePanel(
+        borderLayout, true
+    );
+
+    this.tabbedPane.add(
+        this.versionTabText, this.optionsVersionPanel
+    );
+    this.tabbedPane.add(
+        this.languageTabText, this.optionsLanguagePanel
+    );
+
+    int orientation = JTabbedPane.TOP;
+    this.createTopTabbedPane(orientation);
+
+    FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
+    this.createBottomPanel(flowLayout);
   }
 
-  private void initComponents() {
-    this.tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+  private void createTopTabbedPane(int orientation) {
+    this.tabbedPane = new JTabbedPane(orientation);
     this.add(this.tabbedPane, BorderLayout.NORTH);
-
-    this.optionsVersionPanel = new OptionsVersionPanel();
-    this.tabbedPane.add(this.versionTabTitle, this.optionsVersionPanel);
-
-    this.optionsLanguagePanel = new OptionsLanguagePanel();
-    this.tabbedPane.add(this.languageTabTitle, this.optionsLanguagePanel);
-
-    this.createBottomPanel();
   }
 
-  private void createBottomPanel() {
-    int tabbedPaneWidth = this.tabbedPane.getPreferredSize().width;
-
-    JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER), true);
+  private void createBottomPanel(LayoutManager layout) {
+    JPanel bottomPanel = new JPanel(
+        layout, true
+    );
     this.add(bottomPanel, BorderLayout.SOUTH);
 
     this.cancelButton = new JButton(this.cancelButtonText);
     bottomPanel.add(this.cancelButton);
+
+    int tabbedPaneWidth = this.tabbedPane.getPreferredSize().width;
     bottomPanel.add(Box.createHorizontalStrut(tabbedPaneWidth >> 2));
 
-    this.openGameDirectoryButton = new JButton(this.openGameDirectoryButtonText);
+    this.openGameDirectoryButton = new JButton(
+        this.openGameDirectoryButtonText);
     bottomPanel.add(this.openGameDirectoryButton);
 
     this.saveOptionsButton = new JButton(this.saveOptionsButtonText);
