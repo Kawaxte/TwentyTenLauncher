b2b173008a7bef299762cfa78c1d9fe2acbbd312
sojlabjoi <ungurkristjan@gmail.com>
2023-02-15 22:25:55 +0200

    Initial commit

--- src/main/java/ee/twentyten/ui/launcher/LauncherNoNetworkPanel.java
+++ src/main/java/ee/twentyten/ui/launcher/LauncherNoNetworkPanel.java
@@ -2,11 +2,13 @@ package ee.twentyten.ui.launcher;
 
 import ee.twentyten.custom.CustomJPanel;
 import ee.twentyten.lang.LauncherLanguage;
+import ee.twentyten.ui.LauncherFrame;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import javax.swing.Box;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,7 +16,8 @@ import javax.swing.SwingConstants;
 import lombok.Getter;
 import net.minecraft.MinecraftLauncher;
 
-public class LauncherNoNetworkPanel extends CustomJPanel {
+public class LauncherNoNetworkPanel extends CustomJPanel implements
+    ActionListener {
 
   private static final long serialVersionUID = 1L;
   private final String playOnlineLabelText;
@@ -42,11 +45,15 @@ public class LauncherNoNetworkPanel extends CustomJPanel {
     super(new BorderLayout(0, 8), true);
 
     this.createTopPanel();
+    this.createMiddlePanel();
     this.createBottomPanel();
+
+    this.playOfflineButton.addActionListener(this);
+    this.tryAgainButton.addActionListener(this);
   }
 
   private void createTopPanel() {
-    JPanel topPanel = new JPanel(new BorderLayout(0, 8), true);
+    JPanel topPanel = new JPanel(new BorderLayout(), true);
     topPanel.setBackground(Color.GRAY);
     this.add(topPanel, BorderLayout.NORTH);
 
@@ -54,25 +61,46 @@ public class LauncherNoNetworkPanel extends CustomJPanel {
     this.errorLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 16));
     this.errorLabel.setForeground(Color.RED.darker());
     topPanel.add(this.errorLabel, BorderLayout.NORTH);
+  }
+
+  private void createMiddlePanel() {
+    JPanel middlePanel = new JPanel(new BorderLayout(), true);
+    middlePanel.setBackground(Color.GRAY);
+    this.add(middlePanel, BorderLayout.CENTER);
 
     this.playOnlineLabel = new JLabel(this.playOnlineLabelText,
         SwingConstants.LEFT);
     if (!MinecraftLauncher.isMinecraftCached()) {
-      topPanel.add(this.playOnlineLabel, BorderLayout.CENTER);
+      middlePanel.add(this.playOnlineLabel, BorderLayout.SOUTH);
     }
   }
 
   private void createBottomPanel() {
-    JPanel bottomPanel = new JPanel(new GridLayout(1, 2, 0, 0), true);
+    JPanel bottomPanel = new JPanel(new GridLayout(1, 2), true);
     bottomPanel.setBackground(Color.GRAY);
     this.add(bottomPanel, BorderLayout.SOUTH);
 
     this.playOfflineButton = new JButton(this.playOfflineButtonText);
     this.playOfflineButton.setEnabled(MinecraftLauncher.isMinecraftCached());
     bottomPanel.add(this.playOfflineButton, BorderLayout.WEST);
-    bottomPanel.add(Box.createHorizontalBox());
 
     this.tryAgainButton = new JButton(this.tryAgainButtonText);
     bottomPanel.add(this.tryAgainButton, BorderLayout.EAST);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent evt) {
+    Object src = evt.getSource();
+    if (src == this.playOfflineButton) {
+      LauncherFrame.instance.launchMinecraft(false);
+    }
+    if (src == this.tryAgainButton) {
+      LauncherPanel launcherPanel = LauncherFrame.instance.getLauncherPanel();
+      launcherPanel.getParent();
+      launcherPanel.removeAll();
+      launcherPanel.add(launcherPanel.getLauncherLoginPanel());
+      launcherPanel.validate();
+      launcherPanel.repaint();
+    }
   }
 }
