94820b643ab523348c2562a51acd11d1db959423
sojlabjoi <ungurkristjan@gmail.com>
2023-02-16 01:10:52 +0200

    Initial commit

--- src/main/java/ee/twentyten/ui/LauncherOptionPane.java
+++ src/main/java/ee/twentyten/ui/LauncherOptionPane.java
@@ -7,22 +7,24 @@ public class LauncherOptionPane extends JOptionPane {
 
   private static final long serialVersionUID = 1L;
   public static LauncherOptionPane instance;
+
+  static {
+    LauncherOptionPane.instance = new LauncherOptionPane();
+  }
+
   private final String optionPaneTitleErrorText;
   private final String optionPaneVersionErrorText;
 
   {
-    this.optionPaneTitleErrorText = LauncherLanguage
-        .getString("lp.string.title.error");
-    this.optionPaneVersionErrorText = LauncherLanguage
-        .getString("lp.string.error.version");
+    this.optionPaneTitleErrorText = LauncherLanguage.getString(
+        "lp.string.title.error");
+    this.optionPaneVersionErrorText = LauncherLanguage.getString(
+        "lp.string.error.version");
   }
 
   public void showVersionError() {
-    JOptionPane.showMessageDialog(
-        LauncherFrame.instance,
-        this.optionPaneVersionErrorText,
-        this.optionPaneTitleErrorText,
-        JOptionPane.ERROR_MESSAGE
-    );
+    JOptionPane.showMessageDialog(LauncherFrame.instance,
+        this.optionPaneVersionErrorText, this.optionPaneTitleErrorText,
+        JOptionPane.ERROR_MESSAGE);
   }
 }
