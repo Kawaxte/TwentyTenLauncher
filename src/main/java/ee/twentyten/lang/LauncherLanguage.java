e54146707d0176a67f02e27a83e28f8ea14e1d75
sojlabjoi <ungurkristjan@gmail.com>
2023-02-15 10:20:16 +0200

    Update documentation & null check(s)

--- src/main/java/ee/twentyten/lang/LauncherLanguage.java
+++ src/main/java/ee/twentyten/lang/LauncherLanguage.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.MissingResourceException;
+import java.util.Objects;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
@@ -14,11 +15,6 @@ public class LauncherLanguage {
 
   private static ResourceBundle resourceBundle;
 
-  /**
-   * Sets the language of the application.
-   *
-   * @param tag The tag of the language to set
-   */
   public static void setLanguage(
       String tag
   ) {
@@ -35,68 +31,33 @@ public class LauncherLanguage {
     /* Load the language file */
     try (InputStream is = LauncherLanguage.class.getClassLoader()
         .getResourceAsStream(languageFileName)) {
-
-      /* Check if the input stream is null */
-      if (is == null) {
-
-        /* Create a new throwable to log. */
-        Throwable t = new Throwable("Failed to return input stream");
-
-        /* Log the error */
-        LoggerHelper.logError(t.getMessage(), t, true);
-        return;
-      }
-
+      Objects.requireNonNull(is, "is == null!");
 
       /* Create a reader for the input stream */
       Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
 
-      /* Load the language file */
+      /* Create a new resource bundle */
       LauncherLanguage.resourceBundle = new PropertyResourceBundle(reader);
     } catch (IOException ioe) {
-
-      /* Create a string for the error */
-      String errorString = "Failed to load language file";
-
-      /* Log the error */
-      LoggerHelper.logError(errorString, ioe, true);
+      LoggerHelper.logError(
+          "Failed to load language file",
+          ioe, true
+      );
     }
   }
 
-  /**
-   * Returns the string value of the specified key from the resource bundle.
-   *
-   * @param key The key to retrieve the string value for.
-   * @return The string value of the specified key, or the key itself if the key
-   * cannot be found in the resource bundle.
-   */
   public static String getString(
       String key
   ) {
-
-    /* Check if the bundle is null */
-    if (LauncherLanguage.resourceBundle == null) {
-
-      /* Create a new throwable to log. */
-      Throwable t = new Throwable("Failed to return resource bundle");
-
-      /* Log the error */
-      LoggerHelper.logError(t.getMessage(), t, true);
-      return key;
-    }
-
-    /* Try to get the string value of the key */
+    Objects.requireNonNull(resourceBundle, "resourceBundle == null!");
     try {
       return LauncherLanguage.resourceBundle.getString(key);
     } catch (MissingResourceException mre) {
-
-      /* Create a string for the error */
-      String missingResource = "Failed to find key in bundle";
-
-      /* Log the error */
-      LoggerHelper.logError(missingResource, mre, true);
+      LoggerHelper.logError(
+          "Failed to find key in bundle",
+          mre, true
+      );
     }
-
     return key;
   }
 }
