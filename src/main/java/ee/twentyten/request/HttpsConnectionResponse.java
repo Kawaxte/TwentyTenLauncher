3fae781a80d8ce9f2da1860d66bab15a9f82f7fc
sojlabjoi <ungurkristjan@gmail.com>
2023-02-15 12:02:51 +0200

    Minor adjustment

--- src/main/java/ee/twentyten/request/HttpsConnectionResponse.java
+++ src/main/java/ee/twentyten/request/HttpsConnectionResponse.java
@@ -6,45 +6,22 @@ import javax.net.ssl.HttpsURLConnection;
 
 class HttpsConnectionResponse {
 
-  HttpsURLConnection getResponse(
-      HttpsURLConnection connection
-  ) {
+  HttpsURLConnection getResponse(HttpsURLConnection connection) {
     try {
-
-      /* Get the HTTP status code from the response */
       int responseCode = connection.getResponseCode();
-
-      /* Get the HTTP status message from the response */
       String responseMessage = connection.getResponseMessage();
-
-      /* Represent the URL of the connection as a string */
       String connectionUrl = connection.getURL().toString();
+      String formattedResponse = String.format("%d %s (%s)", responseCode,
+          responseMessage, connectionUrl);
 
-      /* Format the response code, message, and URL */
-      String formattedResponse = String.format(
-          "%d %s (%s)",
-          responseCode, responseMessage, connectionUrl
-      );
-
-      /* Log using INFO level if the response code is 2xx, otherwise log
-       * using ERROR level */
       if (responseCode / 100 == 2) {
-        LoggerHelper.logInfo(
-            formattedResponse,
-            false
-        );
+        LoggerHelper.logInfo(formattedResponse, false);
       } else {
-        LoggerHelper.logError(
-            formattedResponse,
-            false
-        );
+        LoggerHelper.logError(formattedResponse, false);
       }
       return connection;
     } catch (IOException ioe) {
-      LoggerHelper.logError(
-          "Failed to get Https response",
-          ioe, true
-      );
+      LoggerHelper.logError("Failed to get Https response", ioe, true);
     }
     return null;
   }
