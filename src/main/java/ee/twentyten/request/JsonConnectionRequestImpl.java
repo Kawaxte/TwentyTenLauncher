f2a6b2a688bef23a40ebf3b80dc2b0b955d65f3a
sojlabjoi <ungurkristjan@gmail.com>
2023-02-15 12:01:25 +0200

    Update exception message

--- src/main/java/ee/twentyten/request/JsonConnectionRequestImpl.java
+++ src/main/java/ee/twentyten/request/JsonConnectionRequestImpl.java
@@ -19,72 +19,41 @@ public class JsonConnectionRequestImpl extends JsonConnectionRequest {
   }
 
   @Override
-  public JSONObject performRequest(
-      URL url,
-      EMethod method,
-      Map<String, String> headers
-  ) {
+  public JSONObject performRequest(URL url, EMethod method,
+      Map<String, String> headers) {
     HttpsURLConnection connection = null;
     try {
-
-      /* Open a connection to the URL */
       connection = this.openConnection(url);
-
-      /* Set the request method */
       connection.setRequestMethod(method.name());
-
-      /* Loop through the headers and set them */
       for (Map.Entry<String, String> entry : headers.entrySet()) {
         connection.setRequestProperty(entry.getKey(), entry.getValue());
       }
     } catch (ProtocolException pe) {
-      LoggerHelper.logError(
-          "Failed to set request method",
-          pe, true
-      );
+      LoggerHelper.logError("Failed to set request method", pe, true);
     }
     return this.connectionResponse.getResponse(connection);
   }
 
   @Override
-  public JSONObject performRequest(
-      URL url,
-      EMethod method,
-      Map<String, String> headers,
-      Object data
-  ) {
+  public JSONObject performRequest(URL url, EMethod method,
+      Map<String, String> headers, Object data) {
     HttpsURLConnection connection = null;
     try {
-
-      /* Open a connection to the URL */
       connection = this.openConnection(url);
-
-      /* Set the request method */
       connection.setRequestMethod(method.name());
-
-      /* Loop through the headers and set them */
       for (Map.Entry<String, String> entry : headers.entrySet()) {
         connection.setRequestProperty(entry.getKey(), entry.getValue());
       }
 
-      /* Set the connection to output in order to write data to it */
       connection.setDoOutput(true);
-
-      /* Write the data to the output stream */
-      try (OutputStream os = connection.getOutputStream();
-          OutputStreamWriter osw = new OutputStreamWriter(os)) {
+      try (OutputStream os = connection.getOutputStream(); OutputStreamWriter osw = new OutputStreamWriter(
+          os)) {
         osw.write(data.toString());
       }
     } catch (ProtocolException pe) {
-      LoggerHelper.logError(
-          "Failed to set request method",
-          pe, true
-      );
+      LoggerHelper.logError("Failed to set request method", pe, true);
     } catch (IOException ioe) {
-      LoggerHelper.logError(
-          "Failed to write data to output stream",
-          ioe, true
-      );
+      LoggerHelper.logError("Failed to write data to output stream", ioe, true);
     }
     return this.connectionResponse.getResponse(connection);
   }
