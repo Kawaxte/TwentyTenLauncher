416be34c98771c92a97f068018190f865d99bb93
sojlabjoi <ungurkristjan@gmail.com>
2023-02-16 12:45:12 +0200

    Remove 'clientToken' from argument

--- src/main/java/com/mojang/YggdrasilAuthenticationImpl.java
+++ src/main/java/com/mojang/YggdrasilAuthenticationImpl.java
@@ -1,6 +1,8 @@
 package com.mojang;
 
 import com.mojang.util.YggdrasilHelper;
+import ee.twentyten.config.LauncherConfig;
+import ee.twentyten.request.EMethod;
 import ee.twentyten.util.RequestHelper;
 import org.json.JSONObject;
 
@@ -14,8 +16,10 @@ public class YggdrasilAuthenticationImpl extends YggdrasilAuthentication {
   }
 
   @Override
-  public JSONObject authenticate(String username, String password, String clientToken,
+  public JSONObject authenticate(String username, String password,
       boolean requestUser) {
+    String clientToken = LauncherConfig.instance.getClientToken();
+
     JSONObject payload = new JSONObject();
     payload.put("agent", this.getAgent());
     payload.put("username", username);
@@ -23,9 +27,10 @@ public class YggdrasilAuthenticationImpl extends YggdrasilAuthentication {
     payload.put("clientToken", clientToken);
     payload.put("requestUser", requestUser);
 
-    String authenticate = String.format(YggdrasilHelper.YGGDRASIL_AUTH_URL, "authenticate");
-    return RequestHelper.performJsonRequest(authenticate, "POST", RequestHelper.jsonHeader, true,
-        payload);
+    String authenticate = String.format(YggdrasilHelper.MOJANG_SERVER_URL,
+        "authenticate");
+    return RequestHelper.performJsonRequest(authenticate, EMethod.POST,
+        RequestHelper.jsonHeader, payload);
   }
 
   @Override
@@ -34,20 +39,23 @@ public class YggdrasilAuthenticationImpl extends YggdrasilAuthentication {
     payload.put("accessToken", accessToken);
     payload.put("clientToken", clientToken);
 
-    String validate = String.format(YggdrasilHelper.YGGDRASIL_AUTH_URL, "validate");
-    return RequestHelper.performJsonRequest(validate, "POST", RequestHelper.jsonHeader, true,
-        payload);
+    String validate = String.format(YggdrasilHelper.MOJANG_SERVER_URL,
+        "validate");
+    return RequestHelper.performJsonRequest(validate, EMethod.POST,
+        RequestHelper.jsonHeader, payload);
   }
 
   @Override
-  public JSONObject refresh(String accessToken, String clientToken, boolean requestUser) {
+  public JSONObject refresh(String accessToken, String clientToken,
+      boolean requestUser) {
     JSONObject payload = new JSONObject();
     payload.put("accessToken", accessToken);
     payload.put("clientToken", clientToken);
     payload.put("requestUser", requestUser);
 
-    String refresh = String.format(YggdrasilHelper.YGGDRASIL_AUTH_URL, "refresh");
-    return RequestHelper.performJsonRequest(refresh, "POST", RequestHelper.jsonHeader, true,
-        payload);
+    String refresh = String.format(YggdrasilHelper.MOJANG_SERVER_URL,
+        "refresh");
+    return RequestHelper.performJsonRequest(refresh, EMethod.POST,
+        RequestHelper.jsonHeader, payload);
   }
 }
