ca0a3de46928235c725841471f8e38f3ad8d3668
sojlabjoi <ungurkristjan@gmail.com>
2023-02-16 12:45:42 +0200

    Remove 'clientToken' from argument

--- src/main/java/com/mojang/YggdrasilAuthentication.java
+++ src/main/java/com/mojang/YggdrasilAuthentication.java
@@ -2,12 +2,13 @@ package com.mojang;
 
 import org.json.JSONObject;
 
-public abstract class YggdrasilAuthentication implements IYggdrasilAuthenticationService {
+public abstract class YggdrasilAuthentication {
 
-  public abstract JSONObject authenticate(String username, String password, String clientToken,
+  public abstract JSONObject authenticate(String username, String password,
       boolean requestUser);
 
   public abstract JSONObject validate(String accessToken, String clientToken);
 
-  public abstract JSONObject refresh(String accessToken, String clientToken, boolean requestUser);
+  public abstract JSONObject refresh(String accessToken, String clientToken,
+      boolean requestUser);
 }
