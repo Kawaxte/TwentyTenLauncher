41d324da5cf2416f50c2b66f0d8e4650af47479d
sojlabjoi <ungurkristjan@gmail.com>
2023-02-17 17:55:17 +0200

    Minor adjustment(s)

--- src/main/java/ee/twentyten/util/FileHelper.java
+++ src/main/java/ee/twentyten/util/FileHelper.java
@@ -10,20 +10,15 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
 import javax.imageio.ImageIO;
 import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONObject;
-import sun.security.util.Optional;
 
 public final class FileHelper {
 
-  public static long cacheExpirationTime;
   public static File workingDirectory;
 
   static {
-    FileHelper.cacheExpirationTime = 86400000L;
-
     FileHelper.workingDirectory = LauncherHelper.getWorkingDirectory();
   }
 
@@ -31,31 +26,31 @@ public final class FileHelper {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
-  public static Optional<BufferedImage> readImageFile(Class<?> clazz,
+  public static BufferedImage readImageFile(Class<?> clazz,
       String name) {
     try {
       URL input = clazz.getClassLoader().getResource(name);
       Objects.requireNonNull(input, "input == null!");
-      return Optional.of(ImageIO.read(input));
+      return ImageIO.read(input);
     } catch (IOException ioe) {
       LoggerHelper.logError("Failed to read image file", ioe, true);
     }
-    return Optional.empty();
+    return null;
   }
 
-  public static Optional<JSONObject> readJsonFile(File src) {
+  public static JSONObject readJsonFile(File src) {
     if (!src.exists() || !src.isFile()) {
-      return Optional.empty();
+      return new JSONObject();
     }
 
     try {
       byte[] bytes = Files.readAllBytes(src.toPath());
       String json = new String(bytes, StandardCharsets.UTF_8);
-      return Optional.of(new JSONObject(json));
+      return new JSONObject(json);
     } catch (IOException ioe) {
       LoggerHelper.logError("Failed to read bytes from file", ioe, true);
     }
-    return Optional.empty();
+    return new JSONObject();
   }
 
   public static File createDirectory(File parent, String name) {
@@ -96,37 +91,18 @@ public final class FileHelper {
     }
   }
 
-  public static void downloadFile(String url, final File target) {
+  public static void downloadFile(String url, File target) {
     HttpsURLConnection connection = RequestHelper.performHttpsRequest(url,
         EMethod.GET, RequestHelper.xWwwFormUrlencodedHeader);
     Objects.requireNonNull(connection, "connection == null!");
 
-    final CountDownLatch latch = new CountDownLatch(1);
-
-    try {
-      final InputStream is = connection.getInputStream();
-
-      Thread downloadThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-          try {
-            Files.copy(is, target.toPath(),
-                StandardCopyOption.REPLACE_EXISTING);
-          } catch (IOException ioe) {
-            LoggerHelper.logError("Failed to copy input stream to file", ioe,
-                true);
-          } finally {
-            latch.countDown();
-          }
-        }
-      }, "downloadFile");
-      downloadThread.start();
-
-      latch.await();
+    try (InputStream is = connection.getInputStream()) {
+      Files.copy(is, target.toPath(),
+          StandardCopyOption.REPLACE_EXISTING);
     } catch (IOException ioe) {
-      LoggerHelper.logError("Failed to get input stream", ioe, true);
-    } catch (InterruptedException ie) {
-      LoggerHelper.logError("Failed to wait for download thread", ie, true);
+      LoggerHelper.logError("Failed to copy input stream to file", ioe,
+          true);
     }
   }
+
 }
