7180fc610304a50015d2bd78c98a6be5517d4732
sojlabjoi <ungurkristjan@gmail.com>
2023-02-17 17:53:25 +0200

    Minor adjustment(s)

--- src/main/java/ee/twentyten/util/OptionsHelper.java
+++ src/main/java/ee/twentyten/util/OptionsHelper.java
@@ -22,71 +22,44 @@ public final class OptionsHelper {
   public static String[] versionTypes;
 
   static {
-    VERSIONS_JSON_URL = "https://raw.githubusercontent.com/"
-        + "sojlabjoi/"
-        + "AlphacraftLauncher/"
-        + "master/"
-        + "versions.json";
+    VERSIONS_JSON_URL = "https://raw.githubusercontent.com/" + "sojlabjoi/"
+        + "AlphacraftLauncher/" + "master/" + "versions.json";
 
     TYPES_TO_IDS = new HashMap<>();
     IDS_TO_PORTS = new HashMap<>();
 
-    OptionsHelper.versionTypes = new String[]
-        {
-            "beta", "alpha", "infdev"
-        };
+    OptionsHelper.versionTypes = new String[]{"beta", "alpha", "infdev"};
     OptionsHelper.formattedVersionIds = new HashMap<>();
-    OptionsHelper.formattedVersionIds.put(
-        OptionsHelper.versionTypes[0], "Beta %s"
-    );
-    OptionsHelper.formattedVersionIds.put(
-        OptionsHelper.versionTypes[1], "Alpha v%s"
-    );
-    OptionsHelper.formattedVersionIds.put(
-        OptionsHelper.versionTypes[2], "Infdev (%s)"
-    );
+    OptionsHelper.formattedVersionIds.put(OptionsHelper.versionTypes[0],
+        "Beta %s");
+    OptionsHelper.formattedVersionIds.put(OptionsHelper.versionTypes[1],
+        "Alpha v%s");
+    OptionsHelper.formattedVersionIds.put(OptionsHelper.versionTypes[2],
+        "Infdev (%s)");
   }
 
   private OptionsHelper() {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
-  public static List<String> getVersionIds(
-      String type
-  ) {
-
-    /* Get the list of ids from the map. */
+  public static List<String> getVersionIds(String type) {
     List<String> ids = TYPES_TO_IDS.get(type);
     if (ids == null) {
-
-      /* Create the versions directory and the versions file. */
       File versionsDirectory = FileHelper.createDirectory(
-          FileHelper.workingDirectory, "versions"
-      );
-      File versionsFile = new File(
-          versionsDirectory, "versions.json"
-      );
+          FileHelper.workingDirectory, "versions");
+      File versionsFile = new File(versionsDirectory, "versions.json");
 
-      /* Read the versions file and return an Optional containing the JSON data. */
-      JSONObject data = FileHelper.readJsonFile(versionsFile).get();
-      Objects.requireNonNull(
-          data, "data == null!"
-      );
+      JSONObject data = FileHelper.readJsonFile(versionsFile);
+      Objects.requireNonNull(data, "data == null!");
 
-      /* Get the array of versions by the type. */
       JSONArray array = data.getJSONArray(type);
       if (array == null) {
         return Collections.emptyList();
       }
 
-      /* Create a new list of ids. */
       ids = new ArrayList<>();
-
-      /* Loop through the array of objects and add each of the types'
-       * ids to the list of ids. */
-      for (int objIndex = 0; objIndex < array.length();
-          objIndex++) {
-        JSONObject obj = array.getJSONObject(objIndex);
+      for (int idIndex = 0; idIndex < array.length(); idIndex++) {
+        JSONObject obj = array.getJSONObject(idIndex);
         if (obj.has("id")) {
           ids.add(obj.getString("id"));
         }
@@ -96,32 +69,15 @@ public final class OptionsHelper {
     return ids;
   }
 
-  public static String getPortsFromIds(
-      String id
-  ) throws IOException {
+  public static String getPortsFromIds(String id) throws IOException {
+    File versionsDirectory = new File(FileHelper.workingDirectory, "versions");
+    File versionsFile = new File(versionsDirectory, "versions.json");
 
-    /* Create the versions directory and the versions file. */
-    File versionsDirectory = new File(
-        FileHelper.workingDirectory, "versions"
-    );
-    File versionsFile = new File(
-        versionsDirectory, "versions.json"
-    );
+    JSONObject data = FileHelper.readJsonFile(versionsFile);
+    Objects.requireNonNull(data, "data == null!");
 
-    /* Read the versions file and return an Optional containing the JSON data. */
-    JSONObject data = FileHelper.readJsonFile(versionsFile).get();
-    Objects.requireNonNull(
-        data, "data == null!"
-    );
-
-    /* Loop through the version types. */
     for (String type : OptionsHelper.versionTypes) {
-
-      /* Get the array of versions by the type. */
       JSONArray array = data.getJSONArray(type);
-
-      /* Loop through the array of versions and map each of the types'
-       * ids to their respective ports.*/
       for (int versionIndex = 0; versionIndex < array.length();
           versionIndex++) {
         JSONObject version = array.getJSONObject(versionIndex);
