6dc713fa8e4c473dbff0539f5e4e49540b24418a
sojlabjoi <ungurkristjan@gmail.com>
2023-02-23 17:12:10 +0200

    Separate runtime actions

--- src/main/java/ee/twentyten/Launcher.java
+++ src/main/java/ee/twentyten/Launcher.java
@@ -1,43 +1,19 @@
 package ee.twentyten;
 
 import ee.twentyten.ui.LauncherFrame;
-import ee.twentyten.util.LoggerHelper;
-import ee.twentyten.util.RuntimeHelper;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import ee.twentyten.util.LauncherUtils;
+import ee.twentyten.util.SystemUtils;
 
 public class Launcher {
 
-  public static void main(String[] args) {
-    if (RuntimeHelper.MAX_MEMORY < RuntimeHelper.MIN_MEMORY) {
-      List<String> arguments = new ArrayList<>();
-      arguments.add(
-          EPlatform.getPlatform() == EPlatform.WINDOWS ? "javaw" : "java");
-      arguments.add("-Xmx1024m");
-      arguments.add("-Dsun.java2d.d3d=false");
-      arguments.add("-Dsun.java2d.opengl=false");
-      arguments.add("-Dsun.java2d.pmoffscreen=false");
-      arguments.add("-cp");
-      arguments.add(System.getProperty("java.class.path"));
-      arguments.add(String.valueOf(Launcher.class));
-
-      ProcessBuilder pb = new ProcessBuilder(arguments);
-      try {
-        Process process = pb.start();
-
-        LoggerHelper.logInfo(pb.command().toString(), true);
-        if (process.waitFor() != 0) {
-          System.exit(process.exitValue());
-        }
-      } catch (IOException ioe) {
-        LoggerHelper.logError("Failed to start the process", ioe, true);
-      } catch (InterruptedException ie) {
-        LoggerHelper.logError("Interrupted while waiting for the process", ie,
-            true);
-      }
-    } else {
-      LauncherFrame.main(args);
+  public static void main(String... args) {
+    if (LauncherUtils.MIN_MEMORY > LauncherUtils.MAX_MEMORY) {
+      LauncherUtils.buildLowMemoryProcess();
     }
+
+    SystemUtils.setLauncherVersion(1, 23, 2, 23, 1, true);
+
+    LauncherUtils.readFromConfig();
+    LauncherFrame.main(args);
   }
 }
