cb6720cbf18c80b12d2e0385e25444bc7bdf9a01
sojlabjoi <ungurkristjan@gmail.com>
2023-02-25 20:30:35 +0200

    Refactor implementation; @Getter @Setter

--- src/main/java/ee/twentyten/config/LauncherConfigImpl.java
+++ src/main/java/ee/twentyten/config/LauncherConfigImpl.java
@@ -1,8 +1,9 @@
 package ee.twentyten.config;
 
-import ee.twentyten.custom.CustomLinkedProperties;
+import ee.twentyten.custom.CustomProperties;
 import ee.twentyten.log.ELogger;
 import ee.twentyten.util.ConfigUtils;
+import ee.twentyten.util.LauncherUtils;
 import ee.twentyten.util.LoggerUtils;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,25 +18,137 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import lombok.Getter;
 
+@Getter
 public class LauncherConfigImpl extends LauncherConfig {
+
+  /* GENERAL */
+  private String clientToken;
+  private String selectedLanguage;
+  private boolean isShowBetaVersionsSelected;
+  private boolean isShowAlphaVersionsSelected;
+  private boolean isShowInfdevVersionsSelected;
+  private String selectedVersion;
+
+  /* YGGDRASIL AUTHENTICATION */
+  private String yggdrasilUsername;
+  private String yggdrasilPassword;
+  private boolean isYggdrasilPasswordSaved;
+  private String yggdrasilAccessToken;
+  private String yggdrasilProfileId;
+  private String yggdrasilProfileName;
+
+  /* MICROSOFT AUTHENTICATION */
+  private String microsoftAccessToken;
+  private int microsoftAccessTokenExpiresIn;
+  private String microsoftRefreshToken;
+  private String microsoftProfileId;
+  private String microsoftProfileName;
+  private String yggdrasilSessionId;
+  private String microsoftSessionId;
+
+  private File getConfigFile() {
+    File configFile = new File(LauncherUtils.workingDirectory, "twentyten_settings.properties");
+    if (!configFile.exists()) {
+      boolean isConfigFileCreated;
+      try {
+        isConfigFileCreated = configFile.createNewFile();
+        if (!isConfigFileCreated) {
+          throw new IOException("Failed to create config file");
+        }
+      } catch (IOException ioe) {
+        LoggerUtils.log(ioe.getMessage(), ioe, ELogger.ERROR);
+      }
+    }
+    return configFile;
+  }
+
+  private void getGeneralProperties(CustomProperties clp) {
+    this.clientToken = clp.getProperty("clientToken", ConfigUtils.generateClientToken());
+    this.selectedLanguage = clp.getProperty("selectedLanguage", "en");
+    this.isShowBetaVersionsSelected = Boolean.parseBoolean(
+        clp.getProperty("isShowBetaVersionsSelected", "true"));
+    this.isShowAlphaVersionsSelected = Boolean.parseBoolean(
+        clp.getProperty("isShowAlphaVersionsSelected", "false"));
+    this.isShowInfdevVersionsSelected = Boolean.parseBoolean(
+        clp.getProperty("isShowInfdevVersionsSelected", "false"));
+    this.selectedVersion = clp.getProperty("selectedVersion", "b1.1_02");
+  }
+
+  private void getYggdrasilProperties(CustomProperties clp) {
+    this.yggdrasilUsername = clp.getProperty("yggdrasilUsername", null);
+    this.yggdrasilPassword = this.decrypt(clp.getProperty("yggdrasilPassword", null));
+    this.isYggdrasilPasswordSaved = Boolean.parseBoolean(
+        clp.getProperty("isYggdrasilPasswordSaved", "false"));
+    this.yggdrasilAccessToken = clp.getProperty("yggdrasilAccessToken", null);
+    this.yggdrasilProfileId = clp.getProperty("yggdrasilProfileId", null);
+    this.yggdrasilProfileName = clp.getProperty("yggdrasilProfileName", null);
+    this.yggdrasilSessionId = clp.getProperty("yggdrasilSessionId");
+  }
+
+  private void getMicrosoftProperties(CustomProperties clp) {
+    this.microsoftAccessToken = clp.getProperty("microsoftAccessToken", null);
+    this.microsoftAccessTokenExpiresIn = Integer.parseInt(
+        clp.getProperty("microsoftAccessTokenExpiresIn", Integer.toString(0)));
+    this.microsoftRefreshToken = clp.getProperty("microsoftRefreshToken", null);
+    this.microsoftProfileId = clp.getProperty("microsoftProfileId", null);
+    this.microsoftProfileName = clp.getProperty("microsoftProfileName", null);
+    this.microsoftSessionId = clp.getProperty("microsoftSessionId");
+  }
+
+  private void setGeneralProperties(CustomProperties clp) {
+    clp.setProperty("clientToken", this.clientToken);
+    clp.setProperty("selectedLanguage", this.selectedLanguage);
+    clp.setProperty("isShowBetaVersionsSelected",
+        Boolean.toString(this.isShowBetaVersionsSelected));
+    clp.setProperty("isShowAlphaVersionsSelected",
+        Boolean.toString(this.isShowAlphaVersionsSelected));
+    clp.setProperty("isShowInfdevVersionsSelected",
+        Boolean.toString(this.isShowInfdevVersionsSelected));
+    clp.setProperty("selectedVersion", this.selectedVersion);
+  }
+
+  private void setYggdrasilProperties(CustomProperties clp) {
+    clp.setProperty("yggdrasilUsername", this.yggdrasilUsername);
+    clp.setProperty("yggdrasilPassword", this.encrypt(this.yggdrasilPassword));
+    clp.setProperty("isYggdrasilPasswordSaved", Boolean.toString(this.isYggdrasilPasswordSaved));
+    clp.setProperty("yggdrasilAccessToken", this.yggdrasilAccessToken);
+    clp.setProperty("yggdrasilProfileId", this.yggdrasilProfileId);
+    clp.setProperty("yggdrasilProfileName", this.yggdrasilProfileName);
+    clp.setProperty("yggdrasilSessionId",
+        ConfigUtils.formatSessionId(this.clientToken, this.yggdrasilAccessToken,
+            this.yggdrasilProfileId));
+  }
+
+  private void setMicrosoftProperties(CustomProperties clp) {
+    clp.setProperty("microsoftAccessToken", this.microsoftAccessToken);
+    clp.setProperty("microsoftAccessTokenExpiresIn",
+        Integer.toString(this.microsoftAccessTokenExpiresIn));
+    clp.setProperty("microsoftRefreshToken", this.microsoftRefreshToken);
+    clp.setProperty("microsoftProfileId", this.microsoftProfileId);
+    clp.setProperty("microsoftProfileName", this.microsoftProfileName);
+    clp.setProperty("microsoftSessionId",
+        ConfigUtils.formatSessionId(this.clientToken, this.microsoftAccessToken,
+            this.microsoftProfileId));
+  }
 
   @Override
   public void load() {
     File configFile = this.getConfigFile();
     String configFilePath = configFile.getAbsolutePath();
     try (FileInputStream fis = new FileInputStream(configFile)) {
-      CustomLinkedProperties clp = new CustomLinkedProperties();
+      CustomProperties clp = new CustomProperties();
       clp.load(fis);
 
       this.getGeneralProperties(clp);
       this.getYggdrasilProperties(clp);
       this.getMicrosoftProperties(clp);
+
       if (clp.isEmpty()) {
         this.save();
       }
-
-      LoggerUtils.log(String.format("Loaded config file: %s", configFilePath), ELogger.INFO);
+      LoggerUtils.log(configFilePath, ELogger.INFO);
     } catch (FileNotFoundException fnfe) {
       LoggerUtils.log("Failed to find config file", fnfe, ELogger.ERROR);
     } catch (IOException ioe) {
@@ -50,19 +163,19 @@ public class LauncherConfigImpl extends LauncherConfig {
     try (FileOutputStream fos = new FileOutputStream(configFile)) {
       fos.write(ConfigUtils.generateConfigFileHeader().getBytes());
 
-      CustomLinkedProperties clpGeneral = new CustomLinkedProperties();
+      CustomProperties clpGeneral = new CustomProperties();
       this.setGeneralProperties(clpGeneral);
       clpGeneral.store(fos, "GENERAL");
 
-      CustomLinkedProperties clpYggdrasil = new CustomLinkedProperties();
+      CustomProperties clpYggdrasil = new CustomProperties();
       this.setYggdrasilProperties(clpYggdrasil);
       clpYggdrasil.store(fos, "MOJANG AUTHENTICATION");
 
-      CustomLinkedProperties clpMicrosoft = new CustomLinkedProperties();
+      CustomProperties clpMicrosoft = new CustomProperties();
       this.setMicrosoftProperties(clpMicrosoft);
       clpMicrosoft.store(fos, "MICROSOFT AUTHENTICATION");
 
-      LoggerUtils.log(String.format("Saved config file: %s", configFilePath), ELogger.INFO);
+      LoggerUtils.log(configFilePath, ELogger.INFO);
     } catch (FileNotFoundException fnfe) {
       LoggerUtils.log("Failed to find config file", fnfe, ELogger.ERROR);
     } catch (IOException ioe) {
