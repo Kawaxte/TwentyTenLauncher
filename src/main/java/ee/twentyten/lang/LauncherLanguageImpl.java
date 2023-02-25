c5cbb8d882a7c7baf57e43217d631b8c692e6e48
sojlabjoi <ungurkristjan@gmail.com>
2023-02-25 20:31:48 +0200

    Fix NPE on getString; Load defaults if empty/null

--- src/main/java/ee/twentyten/lang/LauncherLanguageImpl.java
+++ src/main/java/ee/twentyten/lang/LauncherLanguageImpl.java
@@ -5,6 +5,7 @@ import ee.twentyten.util.LoggerUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.MissingResourceException;
 import java.util.Objects;
@@ -18,33 +19,41 @@ public class LauncherLanguageImpl extends LauncherLanguage {
   private ResourceBundle locale;
 
   public String getString(String key) {
-    if (key == null) {
-      return "";
-    }
-
     try {
-      key = this.locale.getString(key);
+      String value = this.locale.getString(key);
+      if (value.isEmpty()) {
+        ResourceBundle defaultLocale = ResourceBundle.getBundle("language/locale_en");
+        value = defaultLocale.getString(key);
+      }
+      return value;
     } catch (MissingResourceException mre) {
       LoggerUtils.log("Failed to find key", mre, ELogger.ERROR);
+      return key;
     }
-    return key;
   }
 
   @Override
   public void load(String code) {
+    if (code == null) {
+      code = "en";
+    }
     ELanguage.getLanguage(code);
 
     String localeFileName = String.format("language/locale_%s.properties", code);
-    try (InputStream is = LauncherLanguageImpl.class.getClassLoader()
-        .getResourceAsStream(localeFileName)) {
+    try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(localeFileName)) {
       Objects.requireNonNull(is, "is == null!");
       try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
         this.locale = new PropertyResourceBundle(isr);
 
-        LoggerUtils.log(String.format("Loaded locale file: %s", localeFileName), ELogger.INFO);
+        URL localeFileInput = this.getClass().getClassLoader().getResource(localeFileName);
+        Objects.requireNonNull(localeFileInput, "localeFileInput == null!");
+        LoggerUtils.log(localeFileInput.getPath(), ELogger.INFO);
+      } catch (IOException ioe2) {
+        LoggerUtils.log("Failed to create InputStreamReader", ioe2, ELogger.ERROR);
       }
-    } catch (IOException ioe) {
-      LoggerUtils.log("Failed to load locale file", ioe, ELogger.ERROR);
+    } catch (IOException ioe1) {
+      LoggerUtils.log(String.format("Failed to load locale file: %s", localeFileName), ioe1,
+          ELogger.ERROR);
     }
   }
 }
