e0c8585180391f726f3ffb4e1d25e0fe0b5470c5
sojlabjoi <ungurkristjan@gmail.com>
2023-02-25 20:47:25 +0200

    Remove addComponent

--- src/main/java/ee/twentyten/custom/CustomJGroupBox.java
+++ src/main/java/ee/twentyten/custom/CustomJGroupBox.java
@@ -1,10 +1,7 @@
 package ee.twentyten.custom;
 
-import java.awt.Dimension;
 import javax.swing.BorderFactory;
-import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 public class CustomJGroupBox extends JPanel {
@@ -13,14 +10,9 @@ public class CustomJGroupBox extends JPanel {
 
   public CustomJGroupBox(String title) {
     super(true);
-
+    
     this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5),
         BorderFactory.createTitledBorder(title)));
-  }
-
-  public void addComponent(JComponent component) {
-    add(component);
-    add(Box.createRigidArea(new Dimension(0, 5)));
   }
 }
