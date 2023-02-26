9a21f156cdc66268b0897e8e38ed6650ccf31341
sojlabjoi <ungurkristjan@gmail.com>
2023-02-27 00:52:52 +0200

    Add additional getString method

--- src/main/java/ee/twentyten/lang/LauncherLanguageImpl.java
+++ src/main/java/ee/twentyten/lang/LauncherLanguageImpl.java
@@ -1,5 +1,6 @@
 package ee.twentyten.lang;
 
+import ee.twentyten.custom.UTF8ResourceBundle;
 import ee.twentyten.log.ELogger;
 import ee.twentyten.util.LoggerUtils;
 import java.io.IOException;
@@ -7,53 +8,54 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.MissingResourceException;
 import java.util.Objects;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
 import lombok.Getter;
 
 public class LauncherLanguageImpl extends LauncherLanguage {
 
   @Getter
-  private ResourceBundle locale;
+  private UTF8ResourceBundle bundle;
 
-  public String getString(String key) {
-    try {
-      String value = this.locale.getString(key);
-      if (value.isEmpty()) {
-        ResourceBundle defaultLocale = ResourceBundle.getBundle("language/locale_en");
-        value = defaultLocale.getString(key);
-      }
-      return value;
-    } catch (MissingResourceException mre) {
-      LoggerUtils.log("Failed to find key", mre, ELogger.ERROR);
-      return key;
+  public final String getString(String key) {
+    String value = this.bundle.getString(key);
+    if (value.isEmpty()) {
+      UTF8ResourceBundle defaultBundle = UTF8ResourceBundle.getCustomBundle(
+          "language/locale_en");
+      value = defaultBundle.getString(key);
     }
+    return value;
+  }
+
+  public final String getString(UTF8ResourceBundle bundle, String key) {
+    this.bundle = bundle;
+
+    String value = this.bundle.getString(key);
+    if (value.isEmpty()) {
+      UTF8ResourceBundle defaultBundle = UTF8ResourceBundle.getCustomBundle(
+          "language/locale_en");
+      value = defaultBundle.getString(key);
+    }
+    return value;
   }
 
   @Override
-  public void load(String code) {
-    if (code == null) {
-      code = "en";
-    }
-    ELanguage.getLanguage(code);
+  public void load(String baseName, String isoCode) {
+    ELanguage.getLanguage(isoCode);
 
-    String localeFileName = String.format("language/locale_%s.properties", code);
+    String localeFileName = String.format("%s_%s.properties", baseName, isoCode);
     try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(localeFileName)) {
       Objects.requireNonNull(is, "is == null!");
       try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
-        this.locale = new PropertyResourceBundle(isr);
-
-        URL localeFileInput = this.getClass().getClassLoader().getResource(localeFileName);
-        Objects.requireNonNull(localeFileInput, "localeFileInput == null!");
-        LoggerUtils.log(localeFileInput.getPath(), ELogger.INFO);
+        this.bundle = new UTF8ResourceBundle(isr);
       } catch (IOException ioe2) {
-        LoggerUtils.log("Failed to create InputStreamReader", ioe2, ELogger.ERROR);
+        LoggerUtils.log("Failed to load locale file", ioe2, ELogger.ERROR);
       }
+
+      URL localeFileInput = this.getClass().getClassLoader().getResource(localeFileName);
+      Objects.requireNonNull(localeFileInput, "localeFileInput == null!");
+      LoggerUtils.log(localeFileInput.getPath(), ELogger.INFO);
     } catch (IOException ioe1) {
-      LoggerUtils.log(String.format("Failed to load locale file: %s", localeFileName), ioe1,
-          ELogger.ERROR);
+      LoggerUtils.log("Failed to load locale file", ioe1, ELogger.ERROR);
     }
   }
 }
