77a4f1dc9d2781c0a5a926a68e6dc21439181dc7
sojlabjoi <ungurkristjan@gmail.com>
2023-02-27 00:47:44 +0200

    Renamed from CustomJGroupBox

--- src/main/java/ee/twentyten/custom/JGroupBox.java
+++ src/main/java/ee/twentyten/custom/JGroupBox.java
@@ -4,14 +4,18 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
-public class CustomJGroupBox extends JPanel {
+public class JGroupBox extends JPanel {
 
   private static final long serialVersionUID = 1L;
 
-  public CustomJGroupBox(String title) {
+  public JGroupBox(String title) {
     super(true);
-    
+
+    this.setTitle(title);
     this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+  }
+
+  public void setTitle(String title) {
     this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5),
         BorderFactory.createTitledBorder(title)));
   }
