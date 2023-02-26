ecfe6254c58f6a364037d51bd7c6f5730e73fe9a
sojlabjoi <ungurkristjan@gmail.com>
2023-02-27 00:56:06 +0200

    Minor refactoring

--- src/main/java/ee/twentyten/ui/launcher/LauncherMicrosoftLoginPanel.java
+++ src/main/java/ee/twentyten/ui/launcher/LauncherMicrosoftLoginPanel.java
@@ -1,10 +1,12 @@
 package ee.twentyten.ui.launcher;
 
 import ee.twentyten.custom.CustomJPanel;
-import ee.twentyten.lang.LauncherLanguage;
-import ee.twentyten.ui.LauncherFrame;
-import ee.twentyten.util.RuntimeHelper;
+import ee.twentyten.custom.TransparentJButton;
+import ee.twentyten.custom.TransparentPanelUI;
+import ee.twentyten.custom.UTF8ResourceBundle;
+import ee.twentyten.util.LanguageUtils;
 import java.awt.BorderLayout;
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
@@ -14,60 +16,49 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
-import javax.swing.SwingConstants;
-import javax.swing.Timer;
 import lombok.Getter;
 import lombok.Setter;
 
-public class LauncherMicrosoftLoginPanel extends CustomJPanel implements
-    ActionListener {
+public class LauncherMicrosoftLoginPanel extends CustomJPanel implements ActionListener {
 
   private static final long serialVersionUID = 1L;
-  private final LauncherPanel panel;
-  private final String copyUserCodeLabelText;
-  private final String openBrowserButtonText;
-  private final String cancelButtonText;
-  @Setter
-  private String verificationUri;
   @Getter
-  private JLabel userCodeLabel;
-  private JProgressBar expiresInProgressBar;
-  private JButton openBrowserButton;
-  private JButton cancelButton;
   @Setter
-  private int expiresIn;
+  public static LauncherMicrosoftLoginPanel instance;
+  private final JLabel copyUserCodeLabel;
+  private final JLabel userCodeLabel;
+  private final JProgressBar expiresInProgressBar;
+  private final TransparentJButton openBrowserButton;
+  private final TransparentJButton cancelButton;
 
   {
-    this.panel = LauncherFrame.instance.getPanel();
-    this.panel.getParent();
-
-    this.copyUserCodeLabelText = LauncherLanguage.getString(
-        "lmlp.label.copyUserCodeLabel");
-    this.openBrowserButtonText = LauncherLanguage.getString(
-        "lmlp.button.openBrowserButton");
-    this.cancelButtonText = LauncherLanguage.getString(
-        "lmlp.button.cancelButton");
-  }
-
-  public LauncherMicrosoftLoginPanel() {
-    super(new BorderLayout(0, 8), true);
-
-    this.createTopPanel();
-    this.createMiddlePanel();
-    this.createBottomPanel();
+    this.copyUserCodeLabel = new JLabel(LanguageUtils.copyUserCodeLabelKey, JLabel.CENTER);
+    this.userCodeLabel = new JLabel("\u00A0", JLabel.CENTER);
+    this.expiresInProgressBar = new JProgressBar();
+    this.openBrowserButton = new TransparentJButton(LanguageUtils.openBrowserButtonKey);
+    this.cancelButton = new TransparentJButton(LanguageUtils.cancelButtonKey);
 
     MouseAdapter adapter = new MouseAdapter() {
       @Override
-      public void mouseClicked(MouseEvent evt) {
-        StringSelection selection = new StringSelection(
-            LauncherMicrosoftLoginPanel.this.userCodeLabel.getText());
-
+      public void mouseClicked(MouseEvent event) {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        clipboard.setContents(selection, selection);
+        StringSelection ss = new StringSelection(
+            LauncherMicrosoftLoginPanel.this.userCodeLabel.getText());
+        clipboard.setContents(ss, ss);
+      }
+
+      @Override
+      public void mousePressed(MouseEvent event) {
+        LauncherMicrosoftLoginPanel.this.openBrowserButton.setEnabled(true);
+      }
+
+      @Override
+      public void mouseEntered(MouseEvent event) {
+        Cursor handCursor = new Cursor(Cursor.HAND_CURSOR);
+        LauncherMicrosoftLoginPanel.this.userCodeLabel.setCursor(handCursor);
       }
     };
     this.userCodeLabel.addMouseListener(adapter);
@@ -75,87 +66,58 @@ public class LauncherMicrosoftLoginPanel extends CustomJPanel implements
     this.cancelButton.addActionListener(this);
   }
 
-  private void createTopPanel() {
-    JPanel topPanel = new JPanel(new BorderLayout(), true);
-    topPanel.setBackground(this.getBackground());
-    this.add(topPanel, BorderLayout.NORTH);
+  public LauncherMicrosoftLoginPanel() {
+    super(new BorderLayout(0, 10), true);
 
-    JLabel copyUserCodeLabel = new JLabel(this.copyUserCodeLabelText);
-    topPanel.add(copyUserCodeLabel, BorderLayout.NORTH);
+    LauncherMicrosoftLoginPanel.setInstance(this);
+    this.buildTopPanel();
+    this.buildMiddlePanel();
+    this.buildBottomPanel();
+
+    this.setTextToComponents(LanguageUtils.getBundle());
   }
 
-  private void createMiddlePanel() {
-    JPanel middlePanel = new JPanel(new BorderLayout(), true);
-    middlePanel.setBackground(this.getBackground());
-    this.add(middlePanel, BorderLayout.CENTER);
+  public void setTextToComponents(UTF8ResourceBundle bundle) {
+    LanguageUtils.setTextToComponent(bundle, this.copyUserCodeLabel,
+        LanguageUtils.copyUserCodeLabelKey);
+    LanguageUtils.setTextToComponent(bundle, this.openBrowserButton,
+        LanguageUtils.openBrowserButtonKey);
+    LanguageUtils.setTextToComponent(bundle, this.cancelButton, LanguageUtils.cancelButtonKey);
+  }
 
+  private void buildTopPanel() {
     Font userCodeFont = new Font(Font.SANS_SERIF, Font.BOLD, 24);
-    this.userCodeLabel = new JLabel("\u00A0", SwingConstants.CENTER);
     this.userCodeLabel.setFont(userCodeFont);
-    middlePanel.add(this.userCodeLabel, BorderLayout.CENTER);
+
+    JPanel topPanel = new JPanel(new BorderLayout(), true);
+    topPanel.setUI(new TransparentPanelUI());
+    topPanel.add(this.copyUserCodeLabel, BorderLayout.NORTH);
+    topPanel.add(this.userCodeLabel, BorderLayout.CENTER);
+    this.add(topPanel, BorderLayout.NORTH);
   }
 
-  private void createBottomPanel() {
-    JPanel northernBottomPanel = new JPanel(new BorderLayout(0, 8), true);
-    northernBottomPanel.setBackground(this.getBackground());
-    this.add(northernBottomPanel, BorderLayout.SOUTH);
-
-    this.expiresInProgressBar = new JProgressBar();
-    northernBottomPanel.add(this.expiresInProgressBar, BorderLayout.NORTH);
-
-    JPanel southernBottomPanel = new JPanel(new GridLayout(1, 2), true);
-    southernBottomPanel.setBackground(this.getBackground());
-    northernBottomPanel.add(southernBottomPanel, BorderLayout.SOUTH);
-
-    this.openBrowserButton = new JButton(this.openBrowserButtonText);
-    southernBottomPanel.add(this.openBrowserButton, 0);
-
-    this.cancelButton = new JButton(this.cancelButtonText);
-    southernBottomPanel.add(this.cancelButton, 1);
+  private void buildMiddlePanel() {
+    JPanel middlePanel = new JPanel(new BorderLayout(), true);
+    middlePanel.setUI(new TransparentPanelUI());
+    middlePanel.add(this.expiresInProgressBar, BorderLayout.NORTH);
+    this.add(middlePanel, BorderLayout.CENTER);
   }
 
-  public void startProgressBar() {
-    this.expiresInProgressBar.setMinimum(0);
-    this.expiresInProgressBar.setMaximum(this.expiresIn);
-    this.expiresInProgressBar.setValue(this.expiresIn);
+  private void buildBottomPanel() {
+    this.openBrowserButton.setEnabled(false);
 
-    Timer expireInTimer = new Timer(1000, new ActionListener() {
-
-      @Override
-      public void actionPerformed(ActionEvent evt) {
-        Object src = evt.getSource();
-
-        int currentValue = LauncherMicrosoftLoginPanel.this.expiresInProgressBar.getValue();
-        if (currentValue > 1) {
-          currentValue--;
-
-          LauncherMicrosoftLoginPanel.this.expiresInProgressBar.setValue(
-              currentValue);
-        } else {
-          ((Timer) src).stop();
-        }
-      }
-    });
-    expireInTimer.start();
-  }
-
-  private void addLauncherPanel() {
-    this.panel.removeAll();
-
-    this.panel.add(new LauncherPanel());
-
-    this.panel.revalidate();
-    this.panel.repaint();
+    JPanel bottomPanel = new JPanel(new GridLayout(1, 2), true);
+    bottomPanel.setUI(new TransparentPanelUI());
+    bottomPanel.add(this.openBrowserButton, 0);
+    bottomPanel.add(this.cancelButton, 1);
+    this.add(bottomPanel, BorderLayout.SOUTH);
   }
 
   @Override
-  public void actionPerformed(ActionEvent evt) {
-    Object src = evt.getSource();
-    if (src == this.openBrowserButton) {
-      RuntimeHelper.openBrowser(this.verificationUri);
-    }
-    if (src == this.cancelButton) {
-      this.addLauncherPanel();
+  public void actionPerformed(ActionEvent event) {
+    Object source = event.getSource();
+    if (source == this.cancelButton) {
+      LauncherPanel.getInstance().show(new LauncherPanel());
     }
   }
 }
