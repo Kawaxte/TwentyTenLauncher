6bf6afe31828d4364d8b9e5bf46d8a3384d7b5c7
sojlabjoi <ungurkristjan@gmail.com>
2023-02-27 00:54:16 +0200

    Minor adjustment(s)

--- src/main/java/ee/twentyten/ui/OptionsDialog.java
+++ src/main/java/ee/twentyten/ui/OptionsDialog.java
@@ -1,29 +1,36 @@
 package ee.twentyten.ui;
 
-import ee.twentyten.ui.options.OptionsLanguagePanel;
+import ee.twentyten.custom.UTF8ResourceBundle;
 import ee.twentyten.ui.options.OptionsPanel;
-import ee.twentyten.ui.options.OptionsVersionPanel;
+import ee.twentyten.util.LanguageUtils;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
+import lombok.Getter;
+import lombok.Setter;
 
 public class OptionsDialog extends JDialog {
 
   private static final long serialVersionUID = 1L;
+  @Getter
+  @Setter
+  public static OptionsDialog instance;
 
-  public OptionsDialog(String title, LauncherFrame frame) {
-    super(frame, title, true);
+  public OptionsDialog(JFrame owner, boolean modal) {
+    super(owner, LanguageUtils.getString(LanguageUtils.optionsDialogTitleKey), modal);
 
-    OptionsPanel panel = new OptionsPanel();
-    this.setContentPane(panel);
-
-    OptionsVersionPanel versionPanel = panel.getVersionPanel();
-    OptionsLanguagePanel languagePanel = panel.getLanguagePanel();
-    versionPanel.updateUseVersionList();
-    languagePanel.updateSetLanguageList();
-
+    OptionsDialog.setInstance(this);
+    this.add(new OptionsPanel());
     this.pack();
-    this.setResizable(false);
 
+    this.setLocation(this.getOwner().getLocation());
     this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-    this.setLocation(frame.getX(), frame.getY());
+    this.setResizable(false);
+    this.setVisible(true);
+
+    this.setTextToContainers(LanguageUtils.getBundle());
+  }
+
+  public void setTextToContainers(UTF8ResourceBundle bundle) {
+    LanguageUtils.setTextToContainer(bundle, this, LanguageUtils.optionsDialogTitleKey);
   }
 }
