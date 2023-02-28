6998a90e2419e4cbd781157c1537a3a7ce0be74b
sojlabjoi <ungurkristjan@gmail.com>
2023-02-28 02:39:27 +0200

    Add readJsonFileContents & downloadFile method

--- src/main/java/ee/twentyten/util/FileUtils.java
+++ src/main/java/ee/twentyten/util/FileUtils.java
@@ -1,14 +1,21 @@
 package ee.twentyten.util;
 
 import ee.twentyten.log.ELogger;
+import ee.twentyten.request.ERequestHeader;
+import ee.twentyten.request.ERequestMethod;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
+import org.json.JSONObject;
 
 public final class FileUtils {
 
@@ -17,10 +24,10 @@ public final class FileUtils {
   }
 
   public static BufferedImage readImageResource(String name, Class<?> clazz) {
-    URL input = clazz.getClassLoader().getResource(name);
-    if (input != null) {
+    URL imageFileInput = clazz.getClassLoader().getResource(name);
+    if (imageFileInput != null) {
       try {
-        return ImageIO.read(input);
+        return ImageIO.read(imageFileInput);
       } catch (IOException ioe) {
         LoggerUtils.log("Failed to read image resource", ioe, ELogger.ERROR);
       }
@@ -28,10 +35,10 @@ public final class FileUtils {
     return null;
   }
 
-  public static String readFileContents(String filePath) {
+  public static String readFileContents(String fileName) {
     StringBuilder sb = new StringBuilder();
     try (FileInputStream fis = new FileInputStream(
-        filePath); InputStreamReader isr = new InputStreamReader(fis,
+        fileName); InputStreamReader isr = new InputStreamReader(fis,
         StandardCharsets.UTF_8); BufferedReader br = new BufferedReader(isr)) {
       String line;
       while ((line = br.readLine()) != null) {
@@ -41,5 +48,26 @@ public final class FileUtils {
       LoggerUtils.log("Failed to read file contents", ioe, ELogger.ERROR);
     }
     return sb.toString();
+  }
+
+  public static JSONObject readJsonFileContents(File f) {
+    try {
+      byte[] fileBytes = Files.readAllBytes(f.toPath());
+      String json = new String(fileBytes, StandardCharsets.UTF_8);
+      return new JSONObject(json);
+    } catch (IOException ioe) {
+      LoggerUtils.log("Failed to read JSON file contents", ioe, ELogger.ERROR);
+    }
+    return new JSONObject();
+  }
+
+  public static void downloadFile(URL url, File f) {
+    HttpsURLConnection connection = RequestUtils.performHttpsRequest(url, ERequestMethod.GET,
+        ERequestHeader.X_WWW_FORM_URLENCODED);
+    try (InputStream is = connection.getInputStream()) {
+      Files.copy(is, f.toPath());
+    } catch (IOException ioe) {
+      LoggerUtils.log("Failed to download file", ioe, ELogger.ERROR);
+    }
   }
 }
