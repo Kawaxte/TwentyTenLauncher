3f8e631af7670c3fd3be7c1e8a96409c208711b5
sojlabjoi <ungurkristjan@gmail.com>
2023-03-01 19:48:12 +0200

    Add errorLabel label

--- src/main/java/ee/twentyten/ui/launcher/LauncherNoNetworkPanel.java
+++ src/main/java/ee/twentyten/ui/launcher/LauncherNoNetworkPanel.java
@@ -1,11 +1,14 @@
 package ee.twentyten.ui.launcher;
 
-import ee.twentyten.custom.CustomJPanel;
-import ee.twentyten.custom.TransparentJButton;
-import ee.twentyten.custom.TransparentPanelUI;
 import ee.twentyten.custom.UTF8ResourceBundle;
+import ee.twentyten.custom.component.TransparentJButton;
+import ee.twentyten.custom.ui.CustomJPanel;
+import ee.twentyten.custom.ui.TransparentPanelUI;
 import ee.twentyten.util.LanguageUtils;
+import ee.twentyten.util.LauncherUtils;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,20 +16,24 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import lombok.Getter;
 import lombok.Setter;
+import net.minecraft.util.MinecraftUtils;
 
 public class LauncherNoNetworkPanel extends CustomJPanel implements ActionListener {
-  
+
   @Getter
   @Setter
-  public static LauncherNoNetworkPanel instance;
+  private static LauncherNoNetworkPanel instance;
+  @Getter
+  private final JLabel errorLabel;
   private final JLabel playOnlineLabel;
   private final TransparentJButton playOfflineButton;
   private final TransparentJButton tryAgainButton;
 
   {
-    this.playOnlineLabel = new JLabel(LanguageUtils.playOnlineLabelKey);
-    this.playOfflineButton = new TransparentJButton(LanguageUtils.playOfflineButtonKey);
-    this.tryAgainButton = new TransparentJButton(LanguageUtils.tryAgainButtonKey);
+    this.errorLabel = new JLabel("\u00A0", JLabel.CENTER);
+    this.playOnlineLabel = new JLabel("lnnp.label.playOnlineLabel");
+    this.playOfflineButton = new TransparentJButton("lnnp.button.playOfflineButton");
+    this.tryAgainButton = new TransparentJButton("lnnp.button.tryAgainButton");
 
     this.playOfflineButton.addActionListener(this);
     this.tryAgainButton.addActionListener(this);
@@ -45,32 +52,34 @@ public class LauncherNoNetworkPanel extends CustomJPanel implements ActionListen
 
   public void setTextToComponents(UTF8ResourceBundle bundle) {
     LanguageUtils.setTextToComponent(bundle, this.playOnlineLabel,
-        LanguageUtils.playOnlineLabelKey);
+        "lnnp.label.playOnlineLabel");
     LanguageUtils.setTextToComponent(bundle, this.playOfflineButton,
-        LanguageUtils.playOfflineButtonKey);
-    LanguageUtils.setTextToComponent(bundle, this.tryAgainButton, LanguageUtils.tryAgainButtonKey);
+        "lnnp.button.playOfflineButton");
+    LanguageUtils.setTextToComponent(bundle, this.tryAgainButton, "lnnp.button.tryAgainButton");
   }
 
   private void buildTopPanel() {
+    this.errorLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 16));
+    this.errorLabel.setForeground(Color.RED.darker());
+
     JPanel topPanel = new JPanel(new BorderLayout(), true);
     topPanel.setUI(new TransparentPanelUI());
-    topPanel.add(LauncherLoginPanel.getInstance().getErrorLabel(), BorderLayout.NORTH);
+    topPanel.add(this.errorLabel, BorderLayout.NORTH);
     this.add(topPanel, BorderLayout.NORTH);
   }
 
   private void buildMiddlePanel() {
     JPanel middlePanel = new JPanel(new BorderLayout(), true);
     middlePanel.setUI(new TransparentPanelUI());
-    middlePanel.add(this.playOnlineLabel, BorderLayout.SOUTH);
-    /*
-    if (!MinecraftLauncher.isMinecraftCached()) {
+    if (!MinecraftUtils.isMinecraftCached()) {
       middlePanel.add(this.playOnlineLabel, BorderLayout.SOUTH);
     }
-    */
     this.add(middlePanel, BorderLayout.CENTER);
   }
 
   private void buildBottomPanel() {
+    this.playOfflineButton.setEnabled(MinecraftUtils.isMinecraftCached());
+
     JPanel bottomPanel = new JPanel(new GridLayout(1, 2), true);
     bottomPanel.setUI(new TransparentPanelUI());
     bottomPanel.add(this.playOfflineButton, BorderLayout.WEST);
@@ -82,7 +91,7 @@ public class LauncherNoNetworkPanel extends CustomJPanel implements ActionListen
   public void actionPerformed(ActionEvent event) {
     Object source = event.getSource();
     if (source == this.tryAgainButton) {
-      LauncherPanel.getInstance().show(new LauncherPanel());
+      LauncherUtils.addPanel(LauncherPanel.getInstance(), new LauncherPanel());
     }
   }
 }
