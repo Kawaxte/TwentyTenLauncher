a02b86857252e6af8335523211aa6647ab6287b0
sojlabjoi <ungurkristjan@gmail.com>
2023-03-02 11:48:51 +0200

    Move vars,getters,setters <= LauncherConfigImpl.java

--- src/main/java/ee/twentyten/config/LauncherConfig.java
+++ src/main/java/ee/twentyten/config/LauncherConfig.java
@@ -1,6 +1,128 @@
 package ee.twentyten.config;
 
+import ee.twentyten.custom.LinkedProperties;
+import ee.twentyten.log.ELevel;
+import ee.twentyten.util.CipherUtils;
+import ee.twentyten.util.ConfigUtils;
+import ee.twentyten.util.LauncherUtils;
+import ee.twentyten.util.LoggerUtils;
+import java.io.File;
+import java.io.IOException;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 abstract class LauncherConfig {
+
+  /* GENERAL */
+  String selectedLanguage;
+  boolean isShowBetaVersionsSelected;
+  boolean isShowAlphaVersionsSelected;
+  boolean isShowInfdevVersionsSelected;
+  String selectedVersion;
+  String clientToken;
+
+  /* YGGDRASIL AUTHENTICATION */
+  String yggdrasilUsername;
+  String yggdrasilPassword;
+  boolean isYggdrasilPasswordSaved;
+  String yggdrasilAccessToken;
+  String yggdrasilProfileId;
+  String yggdrasilProfileName;
+
+  /* MICROSOFT AUTHENTICATION */
+  String microsoftAccessToken;
+  int microsoftAccessTokenExpiresIn;
+  String microsoftRefreshToken;
+  String microsoftProfileId;
+  String microsoftProfileName;
+  String yggdrasilSessionId;
+  String microsoftSessionId;
+
+  File getConfigFile() {
+    File configFile = new File(LauncherUtils.workingDirectory, "twentyten_settings.properties");
+    if (!configFile.exists()) {
+      try {
+        boolean isFileCreated = configFile.createNewFile();
+        if (!isFileCreated) {
+          throw new IOException("Failed to create config file");
+        }
+      } catch (IOException ioe) {
+        LoggerUtils.log(ioe.getMessage(), ioe, ELevel.ERROR);
+      }
+    }
+    return configFile;
+  }
+
+  void getGeneralProperties(LinkedProperties clp) {
+    this.selectedLanguage = clp.getProperty("selectedLanguage", "en");
+    this.isShowBetaVersionsSelected = Boolean.parseBoolean(
+        clp.getProperty("isShowBetaVersionsSelected", "true"));
+    this.isShowAlphaVersionsSelected = Boolean.parseBoolean(
+        clp.getProperty("isShowAlphaVersionsSelected", "false"));
+    this.isShowInfdevVersionsSelected = Boolean.parseBoolean(
+        clp.getProperty("isShowInfdevVersionsSelected", "false"));
+    this.selectedVersion = clp.getProperty("selectedVersion", "b1.1_02");
+    this.clientToken = clp.getProperty("clientToken", ConfigUtils.generateClientToken());
+  }
+
+  void getYggdrasilProperties(LinkedProperties clp) {
+    this.yggdrasilUsername = clp.getProperty("yggdrasilUsername", null);
+    this.yggdrasilPassword = CipherUtils.decryptValue(clp.getProperty("yggdrasilPassword", null));
+    this.isYggdrasilPasswordSaved = Boolean.parseBoolean(
+        clp.getProperty("isYggdrasilPasswordSaved", "false"));
+    this.yggdrasilAccessToken = clp.getProperty("yggdrasilAccessToken", null);
+    this.yggdrasilProfileId = clp.getProperty("yggdrasilProfileId", null);
+    this.yggdrasilProfileName = clp.getProperty("yggdrasilProfileName", null);
+    this.yggdrasilSessionId = clp.getProperty("yggdrasilSessionId");
+  }
+
+  void getMicrosoftProperties(LinkedProperties clp) {
+    this.microsoftAccessToken = clp.getProperty("microsoftAccessToken", null);
+    this.microsoftAccessTokenExpiresIn = Integer.parseInt(
+        clp.getProperty("microsoftAccessTokenExpiresIn", Integer.toString(0)));
+    this.microsoftRefreshToken = clp.getProperty("microsoftRefreshToken", null);
+    this.microsoftProfileId = clp.getProperty("microsoftProfileId", null);
+    this.microsoftProfileName = clp.getProperty("microsoftProfileName", null);
+    this.microsoftSessionId = clp.getProperty("microsoftSessionId");
+  }
+
+  void setGeneralProperties(LinkedProperties clp) {
+    clp.setProperty("selectedLanguage", this.selectedLanguage);
+    clp.setProperty("isShowBetaVersionsSelected",
+        Boolean.toString(this.isShowBetaVersionsSelected));
+    clp.setProperty("isShowAlphaVersionsSelected",
+        Boolean.toString(this.isShowAlphaVersionsSelected));
+    clp.setProperty("isShowInfdevVersionsSelected",
+        Boolean.toString(this.isShowInfdevVersionsSelected));
+    clp.setProperty("selectedVersion", this.selectedVersion);
+    clp.setProperty("clientToken", this.clientToken);
+  }
+
+  void setYggdrasilProperties(LinkedProperties clp) {
+    clp.setProperty("yggdrasilUsername", this.yggdrasilUsername);
+    clp.setProperty("yggdrasilPassword", CipherUtils.encryptValue(this.yggdrasilPassword));
+    clp.setProperty("isYggdrasilPasswordSaved", Boolean.toString(this.isYggdrasilPasswordSaved));
+    clp.setProperty("yggdrasilAccessToken", this.yggdrasilAccessToken);
+    clp.setProperty("yggdrasilProfileId", this.yggdrasilProfileId);
+    clp.setProperty("yggdrasilProfileName", this.yggdrasilProfileName);
+    clp.setProperty("yggdrasilSessionId",
+        ConfigUtils.formatSessionId(this.clientToken, this.yggdrasilAccessToken,
+            this.yggdrasilProfileId));
+  }
+
+  void setMicrosoftProperties(LinkedProperties clp) {
+    clp.setProperty("microsoftAccessToken", this.microsoftAccessToken);
+    clp.setProperty("microsoftAccessTokenExpiresIn",
+        Integer.toString(this.microsoftAccessTokenExpiresIn));
+    clp.setProperty("microsoftRefreshToken", this.microsoftRefreshToken);
+    clp.setProperty("microsoftProfileId", this.microsoftProfileId);
+    clp.setProperty("microsoftProfileName", this.microsoftProfileName);
+    clp.setProperty("microsoftSessionId",
+        ConfigUtils.formatSessionId(this.clientToken, this.microsoftAccessToken,
+            this.microsoftProfileId));
+  }
 
   public abstract void load();
 
