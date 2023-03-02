e840ceef5545e89719c43079c45dc2e8a687618a
sojlabjoi <ungurkristjan@gmail.com>
2023-03-02 11:46:16 +0200

    Relocate vars,getters => LauncherLanguage.java

--- src/main/java/ee/twentyten/lang/LauncherLanguageImpl.java
+++ src/main/java/ee/twentyten/lang/LauncherLanguageImpl.java
@@ -1,7 +1,7 @@
 package ee.twentyten.lang;
 
 import ee.twentyten.custom.UTF8ResourceBundle;
-import ee.twentyten.log.ELoggerLevel;
+import ee.twentyten.log.ELevel;
 import ee.twentyten.util.LoggerUtils;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,34 +10,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.Objects;
-import lombok.Getter;
 
 public class LauncherLanguageImpl extends LauncherLanguage {
-
-  @Getter
-  private UTF8ResourceBundle bundle;
-
-  public final String getString(String key) {
-    String value = this.bundle.getString(key);
-    if (value.isEmpty()) {
-      UTF8ResourceBundle defaultBundle = UTF8ResourceBundle.getCustomBundle(
-          "language/locale_en");
-      value = defaultBundle.getString(key);
-    }
-    return value;
-  }
-
-  public final String getString(UTF8ResourceBundle bundle, String key) {
-    this.bundle = bundle;
-
-    String value = this.bundle.getString(key);
-    if (value.isEmpty()) {
-      UTF8ResourceBundle defaultBundle = UTF8ResourceBundle.getCustomBundle(
-          "language/locale_en");
-      value = defaultBundle.getString(key);
-    }
-    return value;
-  }
 
   @Override
   public void load(String baseName, String isoCode) {
@@ -49,14 +23,14 @@ public class LauncherLanguageImpl extends LauncherLanguage {
       try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
         this.bundle = new UTF8ResourceBundle(isr);
       } catch (IOException ioe2) {
-        LoggerUtils.log("Failed to load locale file", ioe2, ELoggerLevel.ERROR);
+        LoggerUtils.log("Failed to load locale file", ioe2, ELevel.ERROR);
       }
 
       URL localeFileInput = this.getClass().getClassLoader().getResource(localeFileName);
       Objects.requireNonNull(localeFileInput, "localeFileInput == null!");
-      LoggerUtils.log(localeFileInput.getPath(), ELoggerLevel.INFO);
+      LoggerUtils.log(localeFileInput.getPath(), ELevel.INFO);
     } catch (IOException ioe1) {
-      LoggerUtils.log("Failed to load locale file", ioe1, ELoggerLevel.ERROR);
+      LoggerUtils.log("Failed to load locale file", ioe1, ELevel.ERROR);
     }
   }
 }
