1a4029c15716dbf3a85f9c525ab2f950982c7287
sojlabjoi <ungurkristjan@gmail.com>
2023-03-05 00:48:47 +0200

    Add createURL method & minor adjustment(s)

--- src/main/java/ee/twentyten/util/RequestUtils.java
+++ src/main/java/ee/twentyten/util/RequestUtils.java
@@ -1,18 +1,19 @@
 package ee.twentyten.util;
 
 import ee.twentyten.log.ELevel;
-import ee.twentyten.request.EHeader;
 import ee.twentyten.request.EMethod;
 import ee.twentyten.request.HttpsConnectionRequestImpl;
 import ee.twentyten.request.JsonConnectionRequestImpl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
 import java.text.MessageFormat;
+import java.util.Map;
 import java.util.Objects;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -39,10 +40,10 @@ public final class RequestUtils {
       String connectionUrl = connection.getURL().toString();
       String formattedResponse = MessageFormat.format("{0} {1} ({2})", responseCode,
           responseMessage, connectionUrl);
-      LoggerUtils.log(formattedResponse,
+      LoggerUtils.logMessage(formattedResponse,
           responseCode / 100 != 2 ? ELevel.ERROR : ELevel.INFO);
     } catch (IOException ioe) {
-      LoggerUtils.log("Failed to get HTTPS response", ioe, ELevel.ERROR);
+      LoggerUtils.logMessage("Failed to get HTTPS response", ioe, ELevel.ERROR);
     }
     return connection;
   }
@@ -55,7 +56,7 @@ public final class RequestUtils {
       String connectionUrl = connection.getURL().toString();
       String formattedResponse = MessageFormat.format("{0} {1} ({2})", responseCode,
           responseMessage, connectionUrl);
-      LoggerUtils.log(formattedResponse,
+      LoggerUtils.logMessage(formattedResponse,
           responseCode / 100 != 2 ? ELevel.ERROR : ELevel.INFO);
 
       try (InputStreamReader isr = new InputStreamReader(
@@ -69,11 +70,20 @@ public final class RequestUtils {
         jsonResponse = sb.toString();
       }
     } catch (IOException ioe) {
-      LoggerUtils.log("Failed to get HTTPS response", ioe, ELevel.ERROR);
+      LoggerUtils.logMessage("Failed to get HTTPS response", ioe, ELevel.ERROR);
     }
 
     Objects.requireNonNull(jsonResponse, "jsonResponse == null!");
     return jsonResponse.isEmpty() ? new JSONObject() : new JSONObject(jsonResponse);
+  }
+
+  public static URL createURL(String url) {
+    try {
+      return new URL(url);
+    } catch (MalformedURLException murle) {
+      LoggerUtils.logMessage("Failed to create URL", murle, ELevel.ERROR);
+    }
+    return null;
   }
 
   public static HttpsURLConnection openHttpsConnection(URL url) {
@@ -88,29 +98,29 @@ public final class RequestUtils {
       connection.setConnectTimeout(15000);
       connection.setReadTimeout(60000);
     } catch (GeneralSecurityException gse) {
-      LoggerUtils.log("Failed to create SSL context", gse, ELevel.ERROR);
+      LoggerUtils.logMessage("Failed to create SSL context", gse, ELevel.ERROR);
     } catch (IOException ioe) {
-      LoggerUtils.log("Failed to open HTTPS connection", ioe, ELevel.ERROR);
+      LoggerUtils.logMessage("Failed to open HTTPS connection", ioe, ELevel.ERROR);
     }
     return connection;
   }
 
   public static HttpsURLConnection performHttpsRequest(URL url, EMethod method,
-      EHeader header) {
+      Map<String, String> header) {
     return RequestUtils.httpsRequest.perform(url, method, header);
   }
 
   public static HttpsURLConnection performHttpsRequest(URL url, EMethod method,
-      EHeader header, Object data) {
+      Map<String, String> header, Object data) {
     return RequestUtils.httpsRequest.perform(url, method, header, data);
   }
 
   public static JSONObject performJsonRequest(URL url, EMethod method,
-      EHeader header) {
+      Map<String, String> header) {
     return RequestUtils.jsonRequest.perform(url, method, header);
   }
 
-  public static JSONObject performJsonRequest(URL url, EMethod method, EHeader header,
+  public static JSONObject performJsonRequest(URL url, EMethod method, Map<String, String> header,
       Object data) {
     return RequestUtils.jsonRequest.perform(url, method, header, data);
   }
