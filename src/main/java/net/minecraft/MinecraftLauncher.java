69490134889acc7bb5445bd16bea6794dd7da5ab
sojlabjoi <31629699+sojlabjoi@users.noreply.github.com>
2023-03-08 18:25:40 +0200

    Relocate launch method(s) -> MinecraftLauncherImpl.java

--- src/main/java/net/minecraft/MinecraftLauncher.java
+++ src/main/java/net/minecraft/MinecraftLauncher.java
@@ -1,15 +1,11 @@
 package net.minecraft;
 
-import ee.twentyten.ui.LauncherFrame;
-import ee.twentyten.ui.launcher.LauncherPanel;
 import ee.twentyten.util.ConfigUtils;
-import ee.twentyten.util.LauncherUtils;
 import java.text.MessageFormat;
-import net.minecraft.update.ui.GameUpdaterApplet;
 
-public class MinecraftLauncher {
+abstract class MinecraftLauncher {
 
-  private String formatTitle() {
+  String getTitle() {
     String selectedVersion = ConfigUtils.getInstance().getSelectedVersion();
     switch (selectedVersion.charAt(0)) {
       case 'b':
@@ -21,30 +17,13 @@ public class MinecraftLauncher {
       case 'i':
         selectedVersion = "Infdev";
         break;
+      default:
+        break;
     }
     return MessageFormat.format("Minecraft {0}", selectedVersion);
   }
 
-  public void launch() {
-    GameUpdaterApplet updaterApplet = new GameUpdaterApplet();
-    updaterApplet.parameters.put("username", "Player");
-    this.initAndStart(updaterApplet);
-  }
+  public abstract void launch();
 
-  public void launch(String username, String sessionId) {
-    GameUpdaterApplet updaterApplet = new GameUpdaterApplet();
-    updaterApplet.parameters.put("username", username);
-    updaterApplet.parameters.put("sessionid", sessionId);
-    this.initAndStart(updaterApplet);
-  }
-
-  private void initAndStart(GameUpdaterApplet gua) {
-    gua.init();
-
-    LauncherUtils.setContentPaneToContainer(LauncherPanel.getInstance(),
-        LauncherFrame.getInstance(), gua);
-
-    gua.start();
-    LauncherFrame.getInstance().setTitle(this.formatTitle());
-  }
+  public abstract void launch(String username, String sessionId);
 }
