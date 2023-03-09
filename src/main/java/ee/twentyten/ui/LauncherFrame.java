ec1b55b52822a162a5158b2d5e7f143eb6ebf6b3
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-09 07:03:10 +0200

    From ``1,9,3,23,4,true`` -> ``1,9,3,2,3,4,false``

--- src/main/java/ee/twentyten/ui/LauncherFrame.java
+++ src/main/java/ee/twentyten/ui/LauncherFrame.java
@@ -40,7 +40,7 @@ public class LauncherFrame extends JFrame {
 
   public static void main(String... args) {
     LookAndFeelUtils.setLookAndFeel();
-    SystemUtils.setLauncherVersion(1, 9, 3, 23, 4, true);
+    SystemUtils.setLauncherVersion(1, 9, 3, 23, 4, false);
 
     if (AuthenticationUtils.isMicrosoftSessionValid(
         ConfigUtils.getInstance().getMicrosoftAccessToken(),
