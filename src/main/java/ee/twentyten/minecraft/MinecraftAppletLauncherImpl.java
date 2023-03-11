01623b225bec855fb584afee8ad156be1d3a6954
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-11 16:36:41 +0200

    Set value for ``username``

--- src/main/java/ee/twentyten/minecraft/MinecraftAppletLauncherImpl.java
+++ src/main/java/ee/twentyten/minecraft/MinecraftAppletLauncherImpl.java
@@ -3,34 +3,33 @@ package ee.twentyten.minecraft;
 import ee.twentyten.minecraft.update.ui.MinecraftUpdaterApplet;
 import ee.twentyten.ui.LauncherFrame;
 import ee.twentyten.ui.launcher.LauncherPanel;
-import ee.twentyten.util.LauncherUtils;
+import ee.twentyten.util.launcher.LauncherUtils;
 
 public class MinecraftAppletLauncherImpl extends MinecraftAppletLauncher {
 
   @Override
   public void launch() {
-    MinecraftUpdaterApplet gua = new MinecraftUpdaterApplet();
-    gua.parameters.put("username", "Player");
+    this.setUsername("Player");
+
+    MinecraftUpdaterApplet gua = new MinecraftUpdaterApplet("Player", null);
     gua.init();
 
     LauncherUtils.setContentPaneToContainer(LauncherPanel.getInstance(),
         LauncherFrame.getInstance(), gua);
 
     gua.start();
-    LauncherFrame.getInstance().setTitle(this.getTitle());
   }
 
   @Override
   public void launch(String username, String sessionId) {
-    MinecraftUpdaterApplet gua = new MinecraftUpdaterApplet();
-    gua.parameters.put("username", username);
-    gua.parameters.put("sessionid", sessionId);
+    this.setUsername(username);
+
+    MinecraftUpdaterApplet gua = new MinecraftUpdaterApplet(username, sessionId);
     gua.init();
 
     LauncherUtils.setContentPaneToContainer(LauncherPanel.getInstance(),
         LauncherFrame.getInstance(), gua);
 
     gua.start();
-    LauncherFrame.getInstance().setTitle(this.getTitle());
   }
 }
