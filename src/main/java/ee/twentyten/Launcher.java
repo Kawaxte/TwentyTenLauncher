c457e1958d406d1aea2eb3b1d8d3da158a86893c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-11 20:21:42 +0200

    Call `addShutdownHook`,`buildAndUpdateRichPresence`,`discordRunCallbacks` -> ``main``

--- src/main/java/ee/twentyten/Launcher.java
+++ src/main/java/ee/twentyten/Launcher.java
@@ -2,13 +2,9 @@ package ee.twentyten;
 
 import ee.twentyten.ui.LauncherFrame;
 import ee.twentyten.util.config.ConfigUtils;
-import ee.twentyten.util.discord.DiscordRichPresenceUtils;
 import ee.twentyten.util.launcher.LauncherUtils;
 import ee.twentyten.util.launcher.options.LanguageUtils;
 import ee.twentyten.util.launcher.options.VersionUtils;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Launcher {
 
@@ -20,23 +16,6 @@ public class Launcher {
     ConfigUtils.readFromConfig();
     LanguageUtils.loadLocale();
     VersionUtils.getVersionsFile();
-
-    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-      @Override
-      public void run() {
-        DiscordRichPresenceUtils.discordShutdown();
-      }
-    }));
-
-    DiscordRichPresenceUtils.buildAndUpdateRichPresence("", "Idle");
-
-    ScheduledExecutorService rpcService = Executors.newSingleThreadScheduledExecutor();
-    rpcService.scheduleAtFixedRate(new Runnable() {
-      @Override
-      public void run() {
-        DiscordRichPresenceUtils.discordRunCallbacks();
-      }
-    }, 0, 2, TimeUnit.SECONDS);
 
     LauncherFrame.main(args);
   }
