891a68b69463f030c9a52a00b850999d1c37fc7d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-11 20:25:08 +0200

    Call ``addWindowListener`` -> ``OptionsDialog`` constr

--- src/main/java/ee/twentyten/ui/OptionsDialog.java
+++ src/main/java/ee/twentyten/ui/OptionsDialog.java
@@ -2,7 +2,10 @@ package ee.twentyten.ui;
 
 import ee.twentyten.custom.UTF8ResourceBundle;
 import ee.twentyten.ui.options.OptionsPanel;
+import ee.twentyten.util.discord.DiscordRichPresenceUtils;
 import ee.twentyten.util.launcher.options.LanguageUtils;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import lombok.Getter;
@@ -27,6 +30,13 @@ public class OptionsDialog extends JDialog {
     this.setVisible(true);
 
     this.setTextToContainers(LanguageUtils.getBundle());
+
+    this.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosed(WindowEvent event) {
+        DiscordRichPresenceUtils.updateRichPresence("");
+      }
+    });
   }
 
   public void setTextToContainers(UTF8ResourceBundle bundle) {
