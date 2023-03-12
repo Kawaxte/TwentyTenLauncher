4e181f2b940f216cb52eccd46a709d349a8107a0
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-12 19:22:43 +0200

    Minor adjustment(s)

--- src/main/java/ee/twentyten/Launcher.java
+++ src/main/java/ee/twentyten/Launcher.java
@@ -1,10 +1,10 @@
 package ee.twentyten;
 
 import ee.twentyten.ui.LauncherFrame;
-import ee.twentyten.util.config.ConfigUtils;
-import ee.twentyten.util.launcher.LauncherUtils;
-import ee.twentyten.util.launcher.options.LanguageUtils;
-import ee.twentyten.util.launcher.options.VersionUtils;
+import ee.twentyten.util.ConfigUtils;
+import ee.twentyten.util.LanguageUtils;
+import ee.twentyten.util.LauncherUtils;
+import ee.twentyten.util.VersionUtils;
 
 public class Launcher {
 
