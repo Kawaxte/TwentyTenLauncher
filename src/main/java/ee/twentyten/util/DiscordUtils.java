1146ed0d0bfda88ca627852e3a253048d59ea8c3
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-12 19:22:20 +0200

    Call `setSmallImage` -> `buildAndUpdateRichPresence`

--- src/main/java/ee/twentyten/util/DiscordUtils.java
+++ src/main/java/ee/twentyten/util/DiscordUtils.java
@@ -1,4 +1,4 @@
-package ee.twentyten.util.discord;
+package ee.twentyten.util;
 
 import ee.twentyten.EPlatform;
 import ee.twentyten.discord.DiscordEventHandlers;
@@ -7,8 +7,6 @@ import ee.twentyten.discord.DiscordUser;
 import ee.twentyten.discord.IDiscordDynamicLib;
 import ee.twentyten.discord.callback.IReadyCallback;
 import ee.twentyten.log.ELevel;
-import ee.twentyten.util.SystemUtils;
-import ee.twentyten.util.log.LoggerUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,17 +20,17 @@ import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 
-public final class DiscordRichPresenceUtils {
+public final class DiscordUtils {
 
   @Getter
   @Setter
   private static DiscordRichPresence instance;
 
   static {
-    DiscordRichPresenceUtils.loadLibraryForPlatform();
+    DiscordUtils.loadLibraryForPlatform();
   }
 
-  private DiscordRichPresenceUtils() {
+  private DiscordUtils() {
     throw new UnsupportedOperationException("Can't instantiate utility class");
   }
 
@@ -59,12 +57,11 @@ public final class DiscordRichPresenceUtils {
   }
 
   public static void updateRichPresence(final String state) {
-    if (DiscordRichPresenceUtils.getInstance() != null) {
-      DiscordRichPresenceUtils.setInstance(new DiscordRichPresence.Builder(
-          DiscordRichPresenceUtils.getInstance())
+    if (DiscordUtils.getInstance() != null) {
+      DiscordUtils.setInstance(new DiscordRichPresence.Builder(DiscordUtils.getInstance())
           .setState(state)
           .build());
-      DiscordRichPresenceUtils.discordUpdatePresence(DiscordRichPresenceUtils.getInstance());
+      DiscordUtils.discordUpdatePresence(DiscordUtils.getInstance());
     }
   }
 
@@ -77,26 +74,27 @@ public final class DiscordRichPresenceUtils {
                 MessageFormat.format("{0}:{1}'#'{2}", request.getUserId(), request.getUsername(),
                     request.getDiscriminator()), ELevel.INFO);
 
-            DiscordRichPresenceUtils.setInstance(new DiscordRichPresence.Builder()
+            DiscordUtils.setInstance(new DiscordRichPresence.Builder()
                 .setState(state)
                 .setStartTimestamp(System.currentTimeMillis() / 1000L)
                 .setLargeImage("favicon_rpc", "https://github.com/Kawaxte/TwentyTenLauncher")
+                .setSmallImage(SystemUtils.launcherVersion.contains("_pre")
+                        ? "favicon_rpc_small_nightly" : "favicon_rpc_small_stable",
+                    SystemUtils.launcherVersion)
                 .build());
-            DiscordRichPresenceUtils.discordUpdatePresence(
-                DiscordRichPresenceUtils.getInstance());
+            DiscordUtils.discordUpdatePresence(DiscordUtils.getInstance());
           }
         }).build();
-    DiscordRichPresenceUtils.discordInitialise("1058488125313253457", handlers, true, null);
+    DiscordUtils.discordInitialise("1058488125313253457", handlers, true, null);
   }
 
   public static void updateRichPresence(final String state, final String details) {
-    if (DiscordRichPresenceUtils.getInstance() != null) {
-      DiscordRichPresenceUtils.setInstance(new DiscordRichPresence.Builder(
-          DiscordRichPresenceUtils.getInstance())
+    if (DiscordUtils.getInstance() != null) {
+      DiscordUtils.setInstance(new DiscordRichPresence.Builder(DiscordUtils.getInstance())
           .setState(state)
           .setDetails(details)
           .build());
-      DiscordRichPresenceUtils.discordUpdatePresence(DiscordRichPresenceUtils.getInstance());
+      DiscordUtils.discordUpdatePresence(DiscordUtils.getInstance());
     }
   }
 
@@ -122,23 +120,24 @@ public final class DiscordRichPresenceUtils {
     EPlatform platform = EPlatform.getPlatform();
     Objects.requireNonNull(platform, "platform == null!");
 
-    URL libraryFileUrl;
-    File tempDirectory;
-    Path tempLibraryPath;
+    File tempDirectory = null;
+    Path tempLibraryPath = null;
+
+    URL libraryFileUrl = null;
     switch (platform) {
       case MACOSX:
         tempDirectory = Paths.get(SystemUtils.userHome, "Library", "Application Support",
             "discord-rpc").toFile();
         tempLibraryPath = Paths.get(tempDirectory.getAbsolutePath(), mappedLibraryName);
 
-        libraryFileUrl = DiscordRichPresenceUtils.class.getResource(
+        libraryFileUrl = DiscordUtils.class.getResource(
             MessageFormat.format("/rpc/amd64/{0}", mappedLibraryName));
         break;
       case LINUX:
         tempDirectory = Paths.get(SystemUtils.userHome, ".discord-rpc").toFile();
         tempLibraryPath = Paths.get(tempDirectory.getAbsolutePath(), mappedLibraryName);
 
-        libraryFileUrl = DiscordRichPresenceUtils.class.getResource(
+        libraryFileUrl = DiscordUtils.class.getResource(
             MessageFormat.format("/rpc/amd64/{0}", mappedLibraryName));
         break;
       case WINDOWS:
@@ -147,17 +146,18 @@ public final class DiscordRichPresenceUtils {
 
         boolean isAMD64 =
             SystemUtils.sunArchDataModel.equals("64") && SystemUtils.osArch.equals("amd64");
-        libraryFileUrl = DiscordRichPresenceUtils.class.getResource(
+        libraryFileUrl = DiscordUtils.class.getResource(
             MessageFormat.format("/rpc/{0}/{1}", isAMD64 ? "amd64" : "x86", mappedLibraryName));
         break;
       default:
-        throw new UnsupportedOperationException(String.valueOf(platform));
+        LoggerUtils.logMessage(String.valueOf(platform), ELevel.ERROR);
+        break;
     }
 
     if (!tempDirectory.exists() && !tempDirectory.mkdirs()) {
       LoggerUtils.logMessage("Failed to create temp directory", ELevel.ERROR);
       return;
     }
-    DiscordRichPresenceUtils.loadLibrary(tempLibraryPath, libraryFileUrl);
+    DiscordUtils.loadLibrary(tempLibraryPath, libraryFileUrl);
   }
 }
