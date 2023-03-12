faa6eaf20250370066a1a884e2821b97d323966d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-12 20:04:32 +0200

    Remove commit

--- src/main/java/ee/twentyten/discord/callback/IDisconnectedCallback.java
+++
@@ -1,9 +0,0 @@
-package ee.twentyten.discord.callback;
-
-import com.sun.jna.Callback;
-
-public interface IDisconnectedCallback extends Callback {
-
-  void apply(int errorCode, String message);
-
-}
--- src/main/java/ee/twentyten/discord/callback/IErroredCallback.java
+++
@@ -1,8 +0,0 @@
-package ee.twentyten.discord.callback;
-
-import com.sun.jna.Callback;
-
-public interface IErroredCallback extends Callback {
-
-  void apply(int errorCode, String message);
-}
