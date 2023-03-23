adb524933a055fd0ac9e76683aa22cfd3d43c34b
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-23 13:12:47 +0200

    min: adjustments

--- src/main/java/com/github/kawaxte/ttl/launcher/Launcher.java
+++ src/main/java/com/github/kawaxte/ttl/launcher/Launcher.java
@@ -1,19 +1,26 @@
 package com.github.kawaxte.ttl.launcher;
 
 import com.github.kawaxte.ttl.launcher.ui.LauncherFrame;
-import com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public final class Launcher {
+
   private Launcher() {
   }
 
-  public static void main(String[] args) {
+  public static void main(String... args) {
     try {
-      UIManager.setLookAndFeel(WindowsClassicLookAndFeel.class.getName());
-    } catch (Exception e) {
-      e.printStackTrace();
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException cnfe) {
+      cnfe.printStackTrace();
+    } catch (InstantiationException ie) {
+      ie.printStackTrace();
+    } catch (IllegalAccessException iae) {
+      iae.printStackTrace();
+    } catch (UnsupportedLookAndFeelException ulafe) {
+      ulafe.printStackTrace();
     }
 
     SwingUtilities.invokeLater(LauncherFrame::new);
