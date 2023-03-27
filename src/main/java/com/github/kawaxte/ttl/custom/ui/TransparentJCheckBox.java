4152a94ba4d436e841ec4ebd48d989bebd109389
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-27 22:23:42 +0300

    min: adjustments

--- src/main/java/com/github/kawaxte/ttl/custom/ui/TransparentJCheckBox.java
+++ src/main/java/com/github/kawaxte/ttl/custom/ui/TransparentJCheckBox.java
@@ -4,14 +4,12 @@ import javax.swing.JCheckBox;
 
 public class TransparentJCheckBox extends JCheckBox {
 
-    private static final long serialVersionUID = 1L;
+  public TransparentJCheckBox(String text) {
+    super(text);
+  }
 
-    public TransparentJCheckBox(String text) {
-      super(text);
-    }
-
-    @Override
-    public boolean isOpaque() {
-      return false;
-    }
+  @Override
+  public boolean isOpaque() {
+    return false;
+  }
 }
