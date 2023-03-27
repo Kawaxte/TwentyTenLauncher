f29d9d3fef22a1241e6e2d6e2551811f18cdcdb5
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-27 22:24:49 +0300

    fix: LAF gap between `g2d.drawString` & `YggdrasilLoginPanel()`

--- src/main/java/com/github/kawaxte/ttl/launcher/ui/LauncherFrame.java
+++ src/main/java/com/github/kawaxte/ttl/launcher/ui/LauncherFrame.java
@@ -2,27 +2,26 @@ package com.github.kawaxte.ttl.launcher.ui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Transparency;
 import java.awt.image.VolatileImage;
 import java.util.stream.IntStream;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
 
 public class LauncherFrame extends JFrame {
 
   private static final long serialVersionUID = 1L;
 
   public LauncherFrame() {
-    super("TwentyTen Launcher");
-    this.setContentPane(new JPanel(new GridBagLayout(), true) {
+    super();
+
+    this.setContentPane(new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 135), true) {
 
       private static final long serialVersionUID = 1L;
 
@@ -32,8 +31,8 @@ public class LauncherFrame extends JFrame {
 
         Graphics2D g2d = (Graphics2D) g;
 
-        Image bgImage = this.getToolkit().getImage(this.getClass()
-            .getClassLoader().getResource("dirt.png"));
+        Image bgImage = this.getToolkit().getImage(this.getClass().getClassLoader().getResource(
+            "dirt.png"));
 
         int bgWidth = bgImage.getWidth(this) << 1;
         int bgHeight = bgImage.getHeight(this) << 1;
@@ -59,9 +58,17 @@ public class LauncherFrame extends JFrame {
           FontMetrics fm = vImageG2d.getFontMetrics();
           int titleWidth = fm.stringWidth(title);
           int titleHeight = fm.getHeight();
-          int titleX = (frameWidth >> 1 >> 1) - (titleWidth >> 1);
-          int titleY = (frameHeight >> 1 >> 1) - (titleHeight << 1);
+          int titleX = (frameWidth >> 2) - (titleWidth >> 1);
+          int titleY = (frameHeight >> 2) - (titleHeight << 1);
           vImageG2d.drawString(title, titleX, titleY);
+
+          // the RED line represents the exact bottom Y coordinate of the text
+          vImageG2d.setColor(Color.RED);
+          vImageG2d.drawLine(titleX, titleY - 1, titleX + titleWidth, titleY - 1);
+
+          //the GREEN line represents the required Y coordinate of the JPanel's top edge
+          vImageG2d.setColor(Color.GREEN);
+          vImageG2d.drawLine(titleX, titleY + 10, titleX + titleWidth, titleY + 10);
         } finally {
           vImageG2d.dispose();
         }
@@ -73,31 +80,16 @@ public class LauncherFrame extends JFrame {
     this.setMinimumSize(new Dimension(640, 480));
     this.setPreferredSize(new Dimension(854, 480));
 
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.anchor = GridBagConstraints.CENTER;
-    gbc.fill = GridBagConstraints.NONE;
-
-    switch (UIManager.getLookAndFeel().getClass().getCanonicalName()) {
-      case "com.apple.laf.AquaLookAndFeel":
-        gbc.insets.top = 29;
-        break;
-      case "com.sun.java.swing.plaf.motif.MotifLookAndFeel":
-        gbc.insets.top = 31;
-        break;
-      case "javax.swing.plaf.metal.MetalLookAndFeel":
-        gbc.insets.top = 11;
-        break;
-      case "javax.swing.plaf.nimbus.NimbusLookAndFeel":
-        gbc.insets.top = 21;
-        break;
-      default:
-        break;
-    }
-    this.getContentPane().add(new YggdrasilLoginPanel(), gbc);
+    this.getContentPane().add(new YggdrasilLoginPanel());
     this.pack();
 
     this.setLocationRelativeTo(null);
     this.setVisible(true);
+  }
+
+  @Override
+  public String getTitle() {
+    return "TwentyTen Launcher";
   }
 
   @Override
