befe08e6c07b36983779ff2409d7aff58fb424d8
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-29 14:24:49 +0300

    add: `finally` statement

--- src/main/java/com/github/kawaxte/twentyten/Launcher.java
+++ src/main/java/com/github/kawaxte/twentyten/Launcher.java
@@ -2,20 +2,16 @@ package com.github.kawaxte.twentyten;
 
 import com.github.kawaxte.twentyten.log.LauncherLogger;
 import com.github.kawaxte.twentyten.ui.LauncherFrame;
-import com.github.kawaxte.twentyten.util.LoggerUtils.ELevel;
-import java.util.Arrays;
+import com.github.kawaxte.twentyten.util.LauncherLoggerUtils.ELevel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-public final class Launcher {
+public class Launcher {
 
   public static void main(String... args) {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      Arrays.stream(UIManager.getInstalledLookAndFeels()).forEachOrdered(
-          ilaf -> LauncherLogger.log(ELevel.INFO, "Available LAFs: {0}", ilaf.getClassName()));
-      LauncherLogger.log(ELevel.INFO, "Using LAF: {0}", UIManager.getLookAndFeel().getClass());
     } catch (ClassNotFoundException cnfe) {
       LauncherLogger.log(cnfe, "{0} could not found", cnfe.getMessage());
     } catch (InstantiationException ie) {
@@ -24,6 +20,9 @@ public final class Launcher {
       LauncherLogger.log(iae, "{0} is not accessible", iae.getMessage());
     } catch (UnsupportedLookAndFeelException ulafe) {
       LauncherLogger.log(ulafe, "{0} is not supported", ulafe.getMessage());
+    } finally {
+      LauncherLogger.log(ELevel.INFO, "Using Look and Feel: {0}",
+          UIManager.getLookAndFeel().getClass().getCanonicalName());
     }
 
     SwingUtilities.invokeLater(LauncherFrame::new);
