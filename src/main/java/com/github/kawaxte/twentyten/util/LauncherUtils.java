86b8e365acb2989a8564c6c24ae3873269a02a1f
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-29 14:28:42 +0300

    add: `isWindows()`, `isOSX()`, `isLinux()` -> `EPlatform`

--- src/main/java/com/github/kawaxte/twentyten/util/LauncherUtils.java
+++ src/main/java/com/github/kawaxte/twentyten/util/LauncherUtils.java
@@ -1,5 +1,7 @@
 package com.github.kawaxte.twentyten.util;
 
+import com.github.kawaxte.twentyten.log.LauncherLogger;
+import com.github.kawaxte.twentyten.util.LauncherLoggerUtils.ELevel;
 import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.nio.file.Path;
@@ -9,11 +11,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 public final class LauncherUtils {
 
-  public static final boolean DEBUG = ManagementFactory.getRuntimeMXBean().getInputArguments()
-      .toString().indexOf("-agentlib:jdwp") > 0;
+  public static boolean DEBUG = ManagementFactory.getRuntimeMXBean().getInputArguments().toString()
+      .indexOf("-agentlib:jdwp") > 0;
   public static final Path WORKING_DIRECTORY;
 
   static {
@@ -24,29 +27,32 @@ public final class LauncherUtils {
   }
 
   private static Path getWorkingDirectory() {
-    EPlatform platform = EPlatform.getPlatform();
+    String userHome = System.getProperty("user.home", ".");
+    String appData = System.getenv("APPDATA");
 
-    Map<EPlatform, Path> platformPaths = Collections.unmodifiableMap(
+    Map<EPlatform, Path> workingDirectories = Collections.unmodifiableMap(
         new HashMap<EPlatform, Path>() {
           {
-            put(EPlatform.LINUX, Paths.get(System.getProperty("user.home"), ".twentyten"));
-            put(EPlatform.MACOS, Paths.get(System.getProperty("user.home"), "Library",
-                "Application Support", "twentyten"));
-            put(EPlatform.WINDOWS, Paths.get(System.getenv("APPDATA"), ".twentyten"));
+            put(EPlatform.LINUX, Paths.get(userHome, ".twentyten"));
+            put(EPlatform.OSX, Paths.get(userHome, "Library", "Application Support", "twentyten"));
+            put(EPlatform.WINDOWS, Paths.get(appData, ".twentyten"));
           }
         });
 
-    File workingDirectory = platformPaths.get(platform).toFile();
-    return !workingDirectory.exists() && !workingDirectory.mkdirs() ? null
-        : workingDirectory.toPath();
+    File workingDirectory = workingDirectories.get(EPlatform.getPlatform()).toFile();
+    if (!workingDirectory.exists() && !workingDirectory.mkdirs()) {
+      LauncherLogger.log(ELevel.WARNING, "{0} could not be created", workingDirectory);
+      return null;
+    }
+    return workingDirectory.toPath();
   }
 
   public enum EPlatform {
     LINUX("nux"),
-    MACOS("mac"),
+    OSX("mac"),
     WINDOWS("win");
 
-    private final String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+    private static final String OS_NAME = System.getProperty("os.name");
     private final String[] osNames;
 
     EPlatform(String... osNames) {
@@ -54,9 +60,22 @@ public final class LauncherUtils {
     }
 
     public static EPlatform getPlatform() {
-      return Arrays.stream(values())
-          .filter(platform -> Arrays.stream(platform.osNames).anyMatch(platform.osName::contains))
-          .findFirst().orElse(null);
+      return Arrays.stream(values()).filter(platform -> Arrays.stream(platform.osNames)
+              .anyMatch(OS_NAME.toLowerCase(Locale.ROOT)::contains))
+          .findFirst()
+          .orElse(null);
+    }
+
+    public static boolean isWindows() {
+      return Objects.equals(WINDOWS, getPlatform());
+    }
+
+    public static boolean isOSX() {
+      return Objects.equals(OSX, getPlatform());
+    }
+
+    public static boolean isLinux() {
+      return Objects.equals(LINUX, getPlatform());
     }
   }
 }
