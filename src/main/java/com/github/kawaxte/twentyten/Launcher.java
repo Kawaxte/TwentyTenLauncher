ee668e3b9dac836aac7a4f90063975c753601d00
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-08 21:55:37 +0300

    Minor adjustment(s)

--- src/main/java/com/github/kawaxte/twentyten/Launcher.java
+++ src/main/java/com/github/kawaxte/twentyten/Launcher.java
@@ -1,32 +1,47 @@
 package com.github.kawaxte.twentyten;
 
 import com.github.kawaxte.twentyten.lang.LauncherLanguage;
-import com.github.kawaxte.twentyten.log.LauncherLogger;
 import com.github.kawaxte.twentyten.ui.LauncherFrame;
-import com.github.kawaxte.twentyten.util.LauncherLoggerUtils.ELevel;
+import com.github.kawaxte.twentyten.util.LauncherUtils;
+import com.github.kawaxte.twentyten.util.LauncherUtils.EPlatform;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import org.apache.logging.log4j.LogManager;
+
 
 public class Launcher {
+
+  static {
+    LauncherUtils.logger = LogManager.getLogger(Launcher.class);
+  }
 
   public static void main(String... args) {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     } catch (ClassNotFoundException cnfe) {
-      LauncherLogger.log(cnfe, "{0} could not found", cnfe.getMessage());
+      LauncherUtils.logger.error("Class \"{}\" could not be found",
+          cnfe.getMessage(),
+          cnfe);
     } catch (InstantiationException ie) {
-      LauncherLogger.log(ie, "{0} is not instantiable", ie.getMessage());
+      LauncherUtils.logger.error("Class \"{}\" cannot be instantiated",
+          ie.getMessage(),
+          ie);
     } catch (IllegalAccessException iae) {
-      LauncherLogger.log(iae, "{0} is not accessible", iae.getMessage());
+      LauncherUtils.logger.error("Class \"{}\" is not accessible",
+          iae.getMessage(),
+          iae);
     } catch (UnsupportedLookAndFeelException ulafe) {
-      LauncherLogger.log(ulafe, "{0} is not supported", ulafe.getMessage());
+      LauncherUtils.logger.error("Class \"{}\" is not supported on {}",
+          ulafe.getMessage(),
+          EPlatform.getPlatform(),
+          ulafe);
     } finally {
-      LauncherLogger.log(ELevel.INFO, "Using Look and Feel: {0}",
-          UIManager.getLookAndFeel().getClass().getCanonicalName());
+      LauncherUtils.logger.info("Look and Feel \"{}\" set",
+          UIManager.getLookAndFeel().getName());
     }
 
-    LauncherLanguage.load("messages", "en");
+    LauncherLanguage.loadLanguage("messages", "en");
 
     SwingUtilities.invokeLater(LauncherFrame::new);
   }
