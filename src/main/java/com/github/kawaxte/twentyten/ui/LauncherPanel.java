0ed9c749f47b9eca61c53ba5cc1c361aa7fab8e4
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-08 21:56:02 +0300

    Minor adjustment(s)

--- src/main/java/com/github/kawaxte/twentyten/ui/LauncherPanel.java
+++ src/main/java/com/github/kawaxte/twentyten/ui/LauncherPanel.java
@@ -2,17 +2,14 @@ package com.github.kawaxte.twentyten.ui;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Transparency;
-import java.awt.image.VolatileImage;
-import java.net.URL;
 import java.util.stream.IntStream;
 import javax.swing.JPanel;
+import lombok.var;
 
 public class LauncherPanel extends JPanel {
 
@@ -31,19 +28,19 @@ public class LauncherPanel extends JPanel {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
 
-    Graphics2D g2d = (Graphics2D) g;
-
-    URL bgImageUrl = this.getClass().getClassLoader().getResource("dirt.png");
-    Image bgImage = this.getToolkit().getImage(bgImageUrl);
+    var g2d = (Graphics2D) g;
+    var bgImageUrl = this.getClass().getClassLoader().getResource("dirt.png");
+    var bgImage = this.getToolkit().getImage(bgImageUrl);
 
     int bgWidth = bgImage.getWidth(this) << 1;
     int bgHeight = bgImage.getHeight(this) << 1;
     int panelWidth = this.getWidth();
     int panelHeight = this.getHeight();
-    VolatileImage vImage = g2d.getDeviceConfiguration().createCompatibleVolatileImage(
-        panelWidth >> 1, panelHeight >> 1, Transparency.OPAQUE);
 
-    Graphics2D vImageG2d = vImage.createGraphics();
+    var vImage = g2d.getDeviceConfiguration()
+        .createCompatibleVolatileImage(panelWidth >> 1, panelHeight >> 1, Transparency.OPAQUE);
+    var vImageG2d = vImage.createGraphics();
+
     try {
       int gridWidth = (panelWidth + bgWidth) >> 5;
       int gridHeight = (panelHeight + bgHeight) >> 5;
@@ -57,20 +54,12 @@ public class LauncherPanel extends JPanel {
       vImageG2d.setFont(this.getFont().deriveFont(Font.BOLD, 20F));
       vImageG2d.setColor(Color.LIGHT_GRAY);
 
-      FontMetrics fm = vImageG2d.getFontMetrics();
-      int titleWidth = fm.stringWidth(title);
-      int titleHeight = fm.getHeight();
+      var vImageG2dFontMetrics = vImageG2d.getFontMetrics();
+      int titleWidth = vImageG2dFontMetrics.stringWidth(title);
+      int titleHeight = vImageG2dFontMetrics.getHeight();
       int titleX = (panelWidth >> 2) - (titleWidth >> 1);
       int titleY = (panelHeight >> 2) - (titleHeight << 1);
       vImageG2d.drawString(title, titleX, titleY);
-
-      /* DEBUG
-      vImageG2d.setColor(Color.RED);
-      vImageG2d.drawLine(0, titleY - 1, titleX + panelWidth, titleY - 1);
-
-      vImageG2d.setColor(Color.GREEN);
-      vImageG2d.drawLine(0, titleY + 10, titleX + panelWidth, titleY + 10);
-       */
     } finally {
       vImageG2d.dispose();
     }
