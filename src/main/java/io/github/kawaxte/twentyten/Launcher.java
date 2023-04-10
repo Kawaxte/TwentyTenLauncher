b28e4310d52c5f46fbb37ea32ef774182331b7b7
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-10 13:35:22 +0300

    Replace multiple ex with `ReflectiveOperationException`

--- src/main/java/io/github/kawaxte/twentyten/Launcher.java
+++ src/main/java/io/github/kawaxte/twentyten/Launcher.java
@@ -3,10 +3,10 @@ package io.github.kawaxte.twentyten;
 import io.github.kawaxte.twentyten.lang.LauncherLanguage;
 import io.github.kawaxte.twentyten.ui.LauncherFrame;
 import io.github.kawaxte.twentyten.util.LauncherUtils;
-import io.github.kawaxte.twentyten.util.LauncherUtils.EPlatform;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import lombok.val;
 import org.apache.logging.log4j.LogManager;
 
 
@@ -19,31 +19,28 @@ public class Launcher {
   public static void main(String... args) {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } catch (ClassNotFoundException cnfe) {
-      LauncherUtils.logger.error("{} not found",
-          cnfe.getMessage(),
-          cnfe);
-    } catch (InstantiationException ie) {
-      LauncherUtils.logger.error("{} is not instantiable",
-          ie.getMessage(),
-          ie);
-    } catch (IllegalAccessException iae) {
-      LauncherUtils.logger.error("{} is not accessible",
-          iae.getMessage(),
-          iae);
+    } catch (ReflectiveOperationException roe) {
+      LauncherUtils.logger.error("Failed to set '{}' as look and feel on '{}'",
+          roe.getMessage(),
+          System.getProperty("os.name"),
+          roe);
     } catch (UnsupportedLookAndFeelException ulafe) {
-      LauncherUtils.logger.error("{} is not supported on {}",
-          ulafe.getMessage(),
-          EPlatform.getPlatform(),
+      LauncherUtils.logger.error("'{}' is not supported on '{}'",
+          UIManager.getLookAndFeel().getName(),
+          System.getProperty("os.name"),
           ulafe);
     } finally {
-      LauncherUtils.logger.info("Using {} look and feel",
-          UIManager.getLookAndFeel().getName());
+      LauncherUtils.logger.info("Using '{}' as look and feel on '{}'",
+          UIManager.getLookAndFeel().getName(),
+          System.getProperty("os.name"));
     }
 
     LauncherLanguage.loadLanguage("messages",
         System.getProperty("user.language"));
 
-    SwingUtilities.invokeLater(LauncherFrame::new);
+    SwingUtilities.invokeLater(() -> {
+      val launcherFrame = new LauncherFrame();
+      launcherFrame.setVisible(true);
+    });
   }
 }
