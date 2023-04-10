731adc10ac4931eb4f67b980673fd3e6db089764
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-10 13:27:57 +0300

    Remove `getTitle()` & `setTitle(String)`

--- src/main/java/io/github/kawaxte/twentyten/misc/ui/JGroupBox.java
+++ src/main/java/io/github/kawaxte/twentyten/misc/ui/JGroupBox.java
@@ -2,12 +2,10 @@ package io.github.kawaxte.twentyten.misc.ui;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
 
 public class JGroupBox extends JPanel {
 
   private static final long serialVersionUID = 1L;
-  private TitledBorder border;
 
   public JGroupBox(String title, boolean isDoubleBuffered) {
     super(isDoubleBuffered);
@@ -15,19 +13,10 @@ public class JGroupBox extends JPanel {
     this.setTitledBorder(title);
   }
 
-  public String getTitle() {
-    return this.border.getTitle();
-  }
-
-  public void setTitle(String title) {
-    this.border.setTitle(title);
-  }
-
-  public void setTitledBorder(String title) {
-    this.border = BorderFactory.createTitledBorder(title);
+  public String setTitledBorder(String title) {
     super.setBorder(BorderFactory.createCompoundBorder(
-        BorderFactory.createEmptyBorder(5, 5, 5, 5),
-        BorderFactory.createTitledBorder(title))
-    );
+        BorderFactory.createEmptyBorder(),
+        BorderFactory.createTitledBorder(title)));
+    return title;
   }
 }
