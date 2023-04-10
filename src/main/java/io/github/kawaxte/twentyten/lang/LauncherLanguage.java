a8f365ee31806483afe42fe9f3118d5056ecf49d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-10 20:45:59 +0300

    Add `updateLanguageComboBox(LanguageGroupBox)` & `ELanguage` enum

--- src/main/java/io/github/kawaxte/twentyten/lang/LauncherLanguage.java
+++ src/main/java/io/github/kawaxte/twentyten/lang/LauncherLanguage.java
@@ -1,14 +1,70 @@
 package io.github.kawaxte.twentyten.lang;
 
 import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
+import io.github.kawaxte.twentyten.ui.options.LanguageGroupBox;
+import io.github.kawaxte.twentyten.util.LauncherUtils;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.DefaultComboBoxModel;
+import lombok.Getter;
+import lombok.val;
+import org.apache.logging.log4j.LogManager;
 
 public final class LauncherLanguage {
+
+  private LauncherLanguage() {
+  }
+
+  public static Map<String, String> languageLookup;
+
+  static {
+    LauncherUtils.logger = LogManager.getLogger(LauncherLanguage.class);
+  }
 
   public static UTF8ResourceBundle getUtf8Bundle() {
     return AbstractLauncherLanguageImpl.INSTANCE.getUtf8Bundle();
   }
 
+  public static void updateLanguageComboBox(LanguageGroupBox lgb) {
+    languageLookup = new HashMap<>();
+
+    val defaultComboBoxModel = new DefaultComboBoxModel<String>();
+
+    Arrays.stream(ELanguage.values()).forEachOrdered(language -> {
+      defaultComboBoxModel.addElement(language.getName());
+      languageLookup.put(language.getName(), language.toString().toLowerCase());
+    });
+    // TODO: Rewrite this when config is re-implemented
+    //    for (Map.Entry<String, String> entry : LanguageUtils.languageMap.entrySet()) {
+    //      if (entry.getValue().equals(ConfigUtils.getInstance().getSelectedLanguage())) {
+    //        languageModel.setSelectedItem(entry.getKey());
+    //        break;
+    //      }
+    //    }
+
+    lgb.getLanguageComboBox().setModel(defaultComboBoxModel);
+  }
+
   public static void loadLanguage(String baseName, String isoCode) {
     AbstractLauncherLanguageImpl.INSTANCE.loadLanguage(baseName, isoCode);
+  }
+
+  public enum ELanguage {
+    EN("English");
+
+    @Getter
+    private final String name;
+
+    ELanguage(String name) {
+      this.name = name;
+    }
+
+    public static ELanguage getLanguage(String isoCode) {
+      return Arrays.stream(ELanguage.values())
+          .filter(language -> language.name().equalsIgnoreCase(isoCode))
+          .findFirst()
+          .orElse(null);
+    }
   }
 }
