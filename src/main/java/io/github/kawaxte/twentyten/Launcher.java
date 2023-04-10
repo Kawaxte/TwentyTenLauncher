a23c4c97e6adcbdb3cfae06184d5551e03ceff73
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-10 20:46:09 +0300

    Add `EPlatform` enum

--- src/main/java/io/github/kawaxte/twentyten/Launcher.java
+++ src/main/java/io/github/kawaxte/twentyten/Launcher.java
@@ -3,6 +3,11 @@ package io.github.kawaxte.twentyten;
 import io.github.kawaxte.twentyten.lang.LauncherLanguage;
 import io.github.kawaxte.twentyten.ui.LauncherFrame;
 import io.github.kawaxte.twentyten.util.LauncherUtils;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -10,7 +15,10 @@ import lombok.val;
 import org.apache.logging.log4j.LogManager;
 
 
-public class Launcher {
+public final class Launcher {
+
+  private Launcher() {
+  }
 
   static {
     LauncherUtils.logger = LogManager.getLogger(Launcher.class);
@@ -42,5 +50,37 @@ public class Launcher {
       val launcherFrame = new LauncherFrame();
       launcherFrame.setVisible(true);
     });
+  }
+
+  public enum EPlatform {
+    LINUX("aix,", "nix", "nux"),
+    MACOS("darwin", "mac"),
+    WINDOWS("win");
+
+    private static final String OS_NAME = System.getProperty("os.name");
+    private final List<String> osNames;
+
+    EPlatform(String... osNames) {
+      this.osNames = Collections.unmodifiableList(Arrays.asList(osNames));
+    }
+
+    public static EPlatform getPlatform() {
+      return Arrays.stream(values()).filter(platform -> platform.osNames.stream()
+              .anyMatch(osName -> OS_NAME.toLowerCase(Locale.ROOT).contains(osName)))
+          .findFirst()
+          .orElse(null);
+    }
+
+    public static boolean isWindows() {
+      return Objects.equals(WINDOWS, getPlatform());
+    }
+
+    public static boolean isMacOs() {
+      return Objects.equals(MACOS, getPlatform());
+    }
+
+    public static boolean isLinux() {
+      return Objects.equals(LINUX, getPlatform());
+    }
   }
 }
