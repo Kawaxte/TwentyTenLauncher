049842b837a7bf4076a06bc604d6969f43b35bef
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-10 13:31:31 +0300

    Add `updateComponentKeyValues()`; Align buttons to EAST; Dispose `OptionsDialog.java` on `cancelButton` event

--- src/main/java/io/github/kawaxte/twentyten/ui/options/OptionsPanel.java
+++ src/main/java/io/github/kawaxte/twentyten/ui/options/OptionsPanel.java
@@ -1,10 +1,14 @@
 package io.github.kawaxte.twentyten.ui.options;
 
+import io.github.kawaxte.twentyten.util.LauncherUtils;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.LayoutStyle;
+import javax.swing.SwingUtilities;
 import lombok.val;
 
 public class OptionsPanel extends JPanel implements ActionListener {
@@ -19,33 +23,39 @@ public class OptionsPanel extends JPanel implements ActionListener {
   {
     this.languageGroupBox = new LanguageGroupBox();
     this.versionGroupBox = new VersionGroupBox();
-
-    this.okButton = new JButton("od.okButton");
-    this.okButton.addActionListener(this);
-
-    this.cancelButton = new JButton("od.cancelButton");
-    this.cancelButton.addActionListener(this);
-
-    this.applyButton = new JButton("od.applyButton");
-    this.applyButton.addActionListener(this);
+    this.okButton = new JButton("op.okButton");
+    this.cancelButton = new JButton("op.cancelButton");
+    this.applyButton = new JButton("op.applyButton");
   }
 
   public OptionsPanel() {
     super(true);
 
+    this.setLayout(this.getGroupLayout());
+
+    this.cancelButton.addActionListener(this);
+    this.okButton.addActionListener(this);
+    this.applyButton.addActionListener(this);
+
+    this.updateComponentKeyValues();
+  }
+
+  private GroupLayout getGroupLayout() {
     val groupLayout = new GroupLayout(this);
     groupLayout.setAutoCreateContainerGaps(true);
     groupLayout.setAutoCreateGaps(true);
-
     groupLayout.setHorizontalGroup(
         groupLayout.createSequentialGroup()
             .addGroup(groupLayout.createParallelGroup()
                 .addComponent(this.languageGroupBox)
                 .addComponent(this.versionGroupBox)
                 .addGroup(groupLayout.createSequentialGroup()
-                    .addComponent(this.okButton)
-                    .addComponent(this.cancelButton)
-                    .addComponent(this.applyButton)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+                        GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGap(0, 0, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(this.okButton, 75, 75, 75)
+                    .addComponent(this.cancelButton, 75, 75, 75)
+                    .addComponent(this.applyButton, 75, 75, 75)
                 )
             )
     );
@@ -59,12 +69,32 @@ public class OptionsPanel extends JPanel implements ActionListener {
                 .addComponent(this.applyButton)
             )
     );
+    return groupLayout;
+  }
 
-    this.setLayout(groupLayout);
+  private void updateComponentKeyValues() {
+    LauncherUtils.updateComponentKeyValue(LauncherUtils.getUtf8Bundle(),
+        this.languageGroupBox,
+        this.languageGroupBox.setTitledBorder("op.languageGroupBox"));
+    LauncherUtils.updateComponentKeyValue(LauncherUtils.getUtf8Bundle(),
+        this.versionGroupBox,
+        this.versionGroupBox.setTitledBorder("op.versionGroupBox"));
+    LauncherUtils.updateComponentKeyValue(LauncherUtils.getUtf8Bundle(),
+        this.okButton,
+        this.okButton.getText());
+    LauncherUtils.updateComponentKeyValue(LauncherUtils.getUtf8Bundle(),
+        this.cancelButton,
+        this.cancelButton.getText());
+    LauncherUtils.updateComponentKeyValue(LauncherUtils.getUtf8Bundle(),
+        this.applyButton,
+        this.applyButton.getText());
   }
 
   @Override
   public void actionPerformed(ActionEvent event) {
-    Object source = event.getSource();
+    val source = event.getSource();
+    if (Objects.equals(source, this.cancelButton)) {
+      SwingUtilities.getWindowAncestor(this).dispose();
+    }
   }
 }
