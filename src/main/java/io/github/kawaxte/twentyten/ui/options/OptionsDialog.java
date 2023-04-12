3951be8d218cb5a50e45ae1d07fe837504c3dd9d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-12 14:30:24 +0300

    Minor adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/ui/options/OptionsDialog.java
+++ src/main/java/io/github/kawaxte/twentyten/ui/options/OptionsDialog.java
@@ -1,17 +1,24 @@
 package io.github.kawaxte.twentyten.ui.options;
 
+import io.github.kawaxte.twentyten.conf.AbstractLauncherConfigImpl;
+import io.github.kawaxte.twentyten.lang.LauncherLanguage;
+import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
 import io.github.kawaxte.twentyten.util.LauncherUtils;
 import java.awt.Window;
+import java.util.Objects;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import lombok.val;
 
 public class OptionsDialog extends JDialog {
 
   public static final long serialVersionUID = 1L;
+  public static OptionsDialog instance;
 
   public OptionsDialog(Window owner) {
     super((JFrame) owner, true);
 
+    OptionsDialog.instance = this;
     this.setContentPane(new OptionsPanel());
 
     this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -21,13 +28,16 @@ public class OptionsDialog extends JDialog {
     this.setLocation(this.getOwner().getLocation());
     this.setResizable(false);
 
-    this.updateContainerKeyValues();
+    val selectedLanguage = AbstractLauncherConfigImpl.INSTANCE.getSelectedLanguage();
+    this.updateContainerKeyValues(Objects.nonNull(selectedLanguage)
+        ? LauncherLanguage.getUtf8Bundle(selectedLanguage)
+        : LauncherLanguage.getUtf8Bundle());
   }
 
-  private void updateContainerKeyValues() {
-    LauncherUtils.updateContainerKeyValue(LauncherUtils.getUtf8Bundle(),
+  public void updateContainerKeyValues(UTF8ResourceBundle bundle) {
+    LauncherUtils.updateContainerKeyValue(bundle,
         this,
-        this.getTitle());
+        "od.title");
   }
 
   @Override
