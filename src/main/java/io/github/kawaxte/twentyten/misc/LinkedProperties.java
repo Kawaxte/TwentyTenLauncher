9b2d9d95c7f18843b4409bdd359cd3fdceca1072
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-12 14:32:56 +0300

    Remove constr(s) & various method(s)

--- src/main/java/io/github/kawaxte/twentyten/misc/LinkedProperties.java
+++ src/main/java/io/github/kawaxte/twentyten/misc/LinkedProperties.java
@@ -2,11 +2,6 @@ package io.github.kawaxte.twentyten.misc;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.text.MessageFormat;
-import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -21,18 +16,9 @@ public class LinkedProperties extends Properties {
 
   private static final long serialVersionUID = 1L;
   private final Map<Object, Object> linkedMap;
-  protected LinkedProperties linkedDefaults;
 
   {
     this.linkedMap = new LinkedHashMap<>();
-  }
-
-  public LinkedProperties() {
-    this(null);
-  }
-
-  public LinkedProperties(LinkedProperties defaults) {
-    this.linkedDefaults = defaults;
   }
 
   @Override
@@ -68,125 +54,6 @@ public class LinkedProperties extends Properties {
       }
     }
     os.flush();
-  }
-
-  @Override
-  public String getProperty(String key) {
-    val oval = this.linkedMap.get(key);
-    val sval = oval instanceof String
-        ? (String) oval
-        : null;
-    return ((sval == null)
-        && (this.linkedDefaults != null))
-        ? this.linkedDefaults.getProperty(key)
-        : sval;
-  }
-
-  @Override
-  public String getProperty(String key, String defaultValue) {
-    val val = this.getProperty(key);
-    return (val == null)
-        ? defaultValue
-        : val;
-  }
-
-  @Override
-  public void list(PrintStream out) {
-    out.println("-- listing linked properties --");
-    synchronized (this) {
-      for (val entry : this.linkedMap.entrySet()) {
-        val key = entry.getKey();
-        val val = entry.getValue();
-
-        val sb = new StringBuilder();
-        sb.append(key);
-        sb.append("=");
-        sb.append(val instanceof String
-            ? (((String) val).length() > 40)
-            ? (MessageFormat.format("{0}...",
-            ((String) val).substring(0, 37)))
-            : val : val);
-        out.println(sb);
-      }
-    }
-  }
-
-  @Override
-  public void list(PrintWriter out) {
-    out.println("-- listing linked properties --");
-    synchronized (this) {
-      for (val entry : this.linkedMap.entrySet()) {
-        val key = entry.getKey();
-        val val = entry.getValue();
-
-        val sb = new StringBuilder();
-        sb.append(key);
-        sb.append("=");
-        sb.append(val instanceof String
-            ? (((String) val).length() > 40)
-            ? (MessageFormat.format("{0}...",
-            ((String) val).substring(0, 37)))
-            : val : val);
-        out.println(sb);
-      }
-    }
-  }
-
-  @Override
-  public synchronized Enumeration<Object> keys() {
-    return new Enumeration<Object>() {
-      private final Iterator<Object> iterator;
-
-      {
-        this.iterator = LinkedProperties.this.linkedMap.keySet().iterator();
-      }
-
-      @Override
-      public boolean hasMoreElements() {
-        return this.iterator.hasNext();
-      }
-
-      @Override
-      public Object nextElement() {
-        return this.iterator.next();
-      }
-    };
-  }
-
-  @Override
-  public synchronized Enumeration<Object> elements() {
-    return new Enumeration<Object>() {
-      private final Iterator<Object> iterator;
-
-      {
-        this.iterator = LinkedProperties.this.linkedMap.values().iterator();
-      }
-
-      @Override
-      public boolean hasMoreElements() {
-        return this.iterator.hasNext();
-      }
-
-      @Override
-      public Object nextElement() {
-        return this.iterator.next();
-      }
-    };
-  }
-
-  @Override
-  public synchronized boolean isEmpty() {
-    return this.linkedMap.isEmpty();
-  }
-
-  @Override
-  public synchronized int size() {
-    return this.linkedMap.size();
-  }
-
-  @Override
-  public synchronized void clear() {
-    this.linkedMap.clear();
   }
 
   @Override
