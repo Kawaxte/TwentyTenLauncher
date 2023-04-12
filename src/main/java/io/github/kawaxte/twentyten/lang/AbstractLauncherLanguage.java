1027ff14023d15056efca94949497de0f3d90422
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-12 14:36:00 +0300

    Minor adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguage.java
+++ src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguage.java
@@ -1,15 +1,9 @@
 package io.github.kawaxte.twentyten.lang;
 
 import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
-import io.github.kawaxte.twentyten.util.LauncherUtils;
 import lombok.Getter;
-import org.apache.logging.log4j.LogManager;
 
-public abstract class AbstractLauncherLanguage {
-
-  static {
-    LauncherUtils.logger = LogManager.getLogger(AbstractLauncherLanguage.class);
-  }
+abstract class AbstractLauncherLanguage {
 
   @Getter
   UTF8ResourceBundle utf8Bundle;
