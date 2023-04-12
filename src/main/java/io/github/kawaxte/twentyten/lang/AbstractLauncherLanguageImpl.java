02767c5c344adfdf1ce66e2586cab62de4c3a610
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-12 14:35:57 +0300

    Minor adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguageImpl.java
+++ src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguageImpl.java
@@ -2,7 +2,6 @@ package io.github.kawaxte.twentyten.lang;
 
 import io.github.kawaxte.twentyten.lang.LauncherLanguage.ELanguage;
 import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
-import io.github.kawaxte.twentyten.util.LauncherUtils;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -10,13 +9,18 @@ import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.Optional;
 import lombok.val;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AbstractLauncherLanguageImpl extends AbstractLauncherLanguage {
 
   public static final AbstractLauncherLanguageImpl INSTANCE;
+  static Logger logger;
 
   static {
     INSTANCE = new AbstractLauncherLanguageImpl();
+
+    logger = LogManager.getLogger(AbstractLauncherLanguageImpl.class);
   }
 
   @Override
@@ -32,11 +36,11 @@ public class AbstractLauncherLanguageImpl extends AbstractLauncherLanguage {
           val isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
         this.utf8Bundle = new UTF8ResourceBundle(isr);
       } catch (IOException ioe) {
-        LauncherUtils.logger.error("Failed to load language file '{}'",
+        logger.error("Failed to load language file '{}'",
             ELanguage.getLanguage(isoCode).getName(),
             ioe);
       } finally {
-        LauncherUtils.logger.info("Using '{}' as language",
+        logger.info("Using '{}' as language",
             ELanguage.getLanguage(isoCode).getName());
       }
     });
