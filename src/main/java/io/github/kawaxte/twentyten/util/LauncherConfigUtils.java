847ab7ef648ab11762d59389d48511817b76ad8c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-12 14:34:55 +0300

    Complete `updateSelectedLanguage(LanguageGroupBox)`

--- src/main/java/io/github/kawaxte/twentyten/util/LauncherConfigUtils.java
+++ src/main/java/io/github/kawaxte/twentyten/util/LauncherConfigUtils.java
@@ -3,11 +3,13 @@ package io.github.kawaxte.twentyten.util;
 import io.github.kawaxte.twentyten.conf.AbstractLauncherConfigImpl;
 import io.github.kawaxte.twentyten.conf.LauncherConfig;
 import io.github.kawaxte.twentyten.lang.LauncherLanguage;
-import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
-import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle.UTF8Control;
+import io.github.kawaxte.twentyten.ui.LauncherOfflinePanel;
+import io.github.kawaxte.twentyten.ui.MicrosoftLoginPanel;
 import io.github.kawaxte.twentyten.ui.YggdrasilLoginPanel;
 import io.github.kawaxte.twentyten.ui.options.LanguageGroupBox;
-import java.util.Locale;
+import io.github.kawaxte.twentyten.ui.options.OptionsDialog;
+import io.github.kawaxte.twentyten.ui.options.OptionsPanel;
+import io.github.kawaxte.twentyten.ui.options.VersionGroupBox;
 import java.util.Objects;
 import javax.swing.SwingUtilities;
 import lombok.val;
@@ -18,20 +20,41 @@ public final class LauncherConfigUtils {
   private LauncherConfigUtils() {
   }
 
-  public static void updateLanguage(LanguageGroupBox lgb) {
+  public static void updateSelectedLanguage(LanguageGroupBox lgb) {
     var selectedItem = (String) lgb.getLanguageComboBox().getSelectedItem();
     selectedItem = LauncherLanguage.languageLookup.get(selectedItem);
 
     val selectedLanguage = AbstractLauncherConfigImpl.INSTANCE.getSelectedLanguage();
     boolean languageChanged = !Objects.equals(selectedItem, selectedLanguage);
     if (languageChanged) {
-      AbstractLauncherConfigImpl.INSTANCE.setSelectedLanguage(selectedLanguage);
+      AbstractLauncherConfigImpl.INSTANCE.setSelectedLanguage(selectedItem);
 
+      val finalSelectedItem = selectedItem;
       SwingUtilities.invokeLater(() -> {
-        val newUtf8Bundle = (UTF8ResourceBundle) UTF8ResourceBundle.getBundle("messages",
-            Locale.forLanguageTag(selectedLanguage),
-            new UTF8Control());
-        YggdrasilLoginPanel.INSTANCE.updateComponentKeyValues(newUtf8Bundle);
+        val newUtf8Bundle = LauncherLanguage.getUtf8Bundle(finalSelectedItem);
+        if (newUtf8Bundle != null) {
+          if (LanguageGroupBox.instance != null) {
+            LanguageGroupBox.instance.updateComponentKeyValues(newUtf8Bundle);
+          }
+          if (OptionsDialog.instance != null) {
+            OptionsDialog.instance.updateContainerKeyValues(newUtf8Bundle);
+          }
+          if (OptionsPanel.instance != null) {
+            OptionsPanel.instance.updateComponentKeyValues(newUtf8Bundle);
+          }
+          if (VersionGroupBox.instance != null) {
+            VersionGroupBox.instance.updateComponentKeyValues(newUtf8Bundle);
+          }
+          if (LauncherOfflinePanel.instance != null) {
+            LauncherOfflinePanel.instance.updateComponentKeyValues(newUtf8Bundle);
+          }
+          if (MicrosoftLoginPanel.instance != null) {
+            MicrosoftLoginPanel.instance.updateComponentKeyValues(newUtf8Bundle);
+          }
+          if (YggdrasilLoginPanel.instance != null) {
+            YggdrasilLoginPanel.instance.updateComponentKeyValues(newUtf8Bundle);
+          }
+        }
       });
 
       LauncherConfig.saveConfig();
