af9f8e693e78e5bf40b4fcc710f1d8931f04a096
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-14 01:48:04 +0300

    Minor adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/conf/AbstractLauncherConfigImpl.java
+++ src/main/java/io/github/kawaxte/twentyten/conf/AbstractLauncherConfigImpl.java
@@ -5,9 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.MessageFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import lombok.val;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,12 +30,14 @@ public class AbstractLauncherConfigImpl extends AbstractLauncherConfig {
       this.getMicrosoftLoginProperties(linkedProperties);
       this.getOptionsProperties(linkedProperties);
     } catch (FileNotFoundException fnfe) {
-      logger.warn("Config file '{}' not found",
-          this.getConfigFilePath().getFileName());
+      logger.error("Failed to locate {}",
+          this.getConfigFilePath().toAbsolutePath(),
+          fnfe);
     } finally {
       if (!linkedProperties.isEmpty()) {
-        logger.info("Loading config file '{}'",
-            this.getConfigFilePath().getFileName());
+        logger.info("Loading {} from {}",
+            this.getConfigFilePath().getFileName(),
+            this.getConfigFilePath().toAbsolutePath().getParent());
       } else {
         this.saveConfig();
       }
@@ -48,30 +47,21 @@ public class AbstractLauncherConfigImpl extends AbstractLauncherConfig {
   @Override
   public void saveConfig() throws IOException {
     try (val fos = new FileOutputStream(this.getConfigFilePath().toFile())) {
-      val sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-      fos.write((MessageFormat.format("# {0}", sdf.format(new Date())).getBytes()));
-      fos.write(System.lineSeparator().getBytes());
-
-      val optionsLinkedProperties = new LinkedProperties();
-      this.setOptionsProperties(optionsLinkedProperties);
-      optionsLinkedProperties.store(fos, "OPTIONS");
-
-      val microsoftLoginLinkedProperties = new LinkedProperties();
-      this.setMicrosoftLoginProperties(microsoftLoginLinkedProperties);
-      microsoftLoginLinkedProperties.store(fos, "MICROSOFT AUTH");
-
-      val yggdrasilLoginLinkedProperties = new LinkedProperties();
-      this.setYggdrasilLoginProperties(yggdrasilLoginLinkedProperties);
-      yggdrasilLoginLinkedProperties.store(fos, "YGGDRASIL AUTH");
+      val linkedProperties = new LinkedProperties();
+      this.setOptionsProperties(linkedProperties);
+      this.setMicrosoftLoginProperties(linkedProperties);
+      this.setYggdrasilLoginProperties(linkedProperties);
+      linkedProperties.store(fos, "TwentyTen Launcher");
 
       fos.flush();
     } catch (FileNotFoundException fnfe) {
-      logger.error("Config file '{}' not found",
-          this.getConfigFilePath().getFileName(),
+      logger.error("Failed to locate {}",
+          this.getConfigFilePath().toAbsolutePath(),
           fnfe);
     } finally {
-      logger.info("Saving config file '{}'",
-          this.getConfigFilePath().getFileName());
+      logger.info("Saving {} to {}",
+          this.getConfigFilePath().getFileName(),
+          this.getConfigFilePath().toAbsolutePath().getParent());
     }
   }
 }
