e89ae7f4053088679600976c105c713e373c6b7b
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-28 20:17:25 +0300

    minor formatting adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguageImpl.java
+++ src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguageImpl.java
@@ -23,34 +23,39 @@ public class AbstractLauncherLanguageImpl extends AbstractLauncherLanguage {
 
   @Override
   public void loadLanguage(String baseName, String isoCode) {
-    val languageFileName = MessageFormat.format("{0}_{1}.properties",
-        baseName,
-        isoCode);
-    val languageFileUrl = Optional.ofNullable(AbstractLauncherLanguageImpl.class
-        .getClassLoader()
-        .getResource(languageFileName));
-    languageFileUrl.ifPresent(url -> {
-      try (val is = url.openConnection().getInputStream();
-          val isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
-        this.utf8Bundle = new UTF8ResourceBundle(isr);
-      } catch (IOException ioe) {
-        logger.error("Failed to load {} from {}",
-            languageFileUrl.get().getFile()
-                .substring(languageFileUrl.get().getFile()
-                    .lastIndexOf("/") + 1),
-            languageFileUrl.get().getFile()
-                .substring(0, languageFileUrl.get().getFile()
-                    .lastIndexOf("/")),
-            ioe);
-      } finally {
-        logger.info("Loading {} from {}",
-            languageFileUrl.get().getFile()
-                .substring(languageFileUrl.get().getFile()
-                    .lastIndexOf("/") + 1),
-            languageFileUrl.get().getFile()
-                .substring(0, languageFileUrl.get().getFile()
-                    .lastIndexOf("/")));
-      }
-    });
+    val languageFileName = MessageFormat.format("{0}_{1}.properties", baseName, isoCode);
+    val languageFileUrl =
+        Optional.ofNullable(
+            AbstractLauncherLanguageImpl.class.getClassLoader().getResource(languageFileName));
+    languageFileUrl.ifPresent(
+        url -> {
+          try (val is = url.openConnection().getInputStream();
+              val isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            this.utf8Bundle = new UTF8ResourceBundle(isr);
+          } catch (IOException ioe) {
+            logger.error(
+                "Failed to load {} from {}",
+                languageFileUrl
+                    .get()
+                    .getFile()
+                    .substring(languageFileUrl.get().getFile().lastIndexOf("/") + 1),
+                languageFileUrl
+                    .get()
+                    .getFile()
+                    .substring(0, languageFileUrl.get().getFile().lastIndexOf("/")),
+                ioe);
+          } finally {
+            logger.info(
+                "Loading {} from {}",
+                languageFileUrl
+                    .get()
+                    .getFile()
+                    .substring(languageFileUrl.get().getFile().lastIndexOf("/") + 1),
+                languageFileUrl
+                    .get()
+                    .getFile()
+                    .substring(0, languageFileUrl.get().getFile().lastIndexOf("/")));
+          }
+        });
   }
 }
