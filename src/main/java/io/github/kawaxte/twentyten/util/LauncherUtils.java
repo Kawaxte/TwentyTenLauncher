9aa4c0dba7dc0c28eefba341f4e1f70db210e776
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-28 20:19:37 +0300

    minor formatting adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/util/LauncherUtils.java
+++ src/main/java/io/github/kawaxte/twentyten/util/LauncherUtils.java
@@ -1,6 +1,6 @@
 package io.github.kawaxte.twentyten.util;
 
-import io.github.kawaxte.twentyten.Launcher.EPlatform;
+import io.github.kawaxte.twentyten.EPlatform;
 import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
 import io.github.kawaxte.twentyten.misc.ui.JGroupBox;
 import java.awt.Container;
@@ -26,7 +26,6 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import lombok.val;
-import lombok.var;
 import org.apache.hc.client5.http.fluent.Request;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -45,32 +44,38 @@ public final class LauncherUtils {
     logger = LogManager.getLogger(LauncherUtils.class);
 
     try {
-      githubReleasesUrl = new URL(
-          new StringBuilder().append("https://api.github.com/")
-              .append("repos/")
-              .append("Kawaxte/")
-              .append("TwentyTenLauncher/")
-              .append("releases").toString());
+      githubReleasesUrl =
+          new URL(
+              new StringBuilder()
+                  .append("https://api.github.com/")
+                  .append("repos/")
+                  .append("Kawaxte/")
+                  .append("TwentyTenLauncher/")
+                  .append("releases")
+                  .toString());
     } catch (IOException ioe) {
       logger.error("Failed to create URL for GitHub API", ioe);
     }
     outdated = null;
   }
 
-  private LauncherUtils() {
-  }
+  private LauncherUtils() {}
 
   public static Path getWorkingDir() {
     String userHome = System.getProperty("user.home", ".");
     String appData = System.getenv("APPDATA");
 
-    val workingDirLookup = Collections.unmodifiableMap(new HashMap<EPlatform, Path>() {
-      {
-        put(EPlatform.LINUX, Paths.get(userHome, ".twentyten"));
-        put(EPlatform.MACOS, Paths.get(userHome, "Library", "Application Support", "twentyten"));
-        put(EPlatform.WINDOWS, Paths.get(appData, ".twentyten"));
-      }
-    });
+    val workingDirLookup =
+        Collections.unmodifiableMap(
+            new HashMap<EPlatform, Path>() {
+              {
+                put(EPlatform.LINUX, Paths.get(userHome, ".twentyten"));
+                put(
+                    EPlatform.MACOS,
+                    Paths.get(userHome, "Library", "Application Support", "twentyten"));
+                put(EPlatform.WINDOWS, Paths.get(appData, ".twentyten"));
+              }
+            });
 
     val workingDirFile = workingDirLookup.get(EPlatform.getPlatform()).toFile();
     if (!workingDirFile.exists() && !workingDirFile.mkdirs()) {
@@ -81,28 +86,20 @@ public final class LauncherUtils {
   }
 
   public static String getManifestAttribute(String name) {
-    val jarFileUrl = Optional.ofNullable(LauncherUtils.class
-            .getProtectionDomain()
-            .getCodeSource()
-            .getLocation())
-        .orElseThrow(() -> new RuntimeException("Failed to get code source location"));
+    val jarFileUrl =
+        Optional.ofNullable(LauncherUtils.class.getProtectionDomain().getCodeSource().getLocation())
+            .orElseThrow(() -> new RuntimeException("Failed to get code source location"));
     try (val jarFile = new JarFile(new File(jarFileUrl.toURI()))) {
       val manifest = jarFile.getManifest();
       val attributes = manifest.getMainAttributes();
       return attributes.getValue(name);
     } catch (IOException ioe) {
-      logger.error("Failed to read manifest from {}",
-          jarFileUrl,
-          ioe);
+      logger.error("Failed to read manifest from {}", jarFileUrl, ioe);
     } catch (URISyntaxException urise) {
-      logger.error("Failed to parse {} as URI",
-          jarFileUrl,
-          urise);
+      logger.error("Failed to parse {} as URI", jarFileUrl, urise);
     } finally {
       if (jarFileUrl.getFile().endsWith(".jar")) {
-        logger.info("Retrieving '{}' from {}",
-            name,
-            jarFileUrl);
+        logger.info("Retrieving '{}' from {}", name, jarFileUrl);
       }
     }
     return null;
@@ -110,41 +107,39 @@ public final class LauncherUtils {
 
   public static boolean isOutdated() {
     if (outdated == null) {
-      val worker = new SwingWorker<Boolean, Void>() {
-        @Override
-        protected Boolean doInBackground() {
-          try {
-            val request = Request.get(githubReleasesUrl.toURI())
-                .addHeader("accept", "application/vnd.github+json")
-                .addHeader("X-GitHub-Api-Version", "2022-11-28").execute();
-            val response = request.returnContent();
-            val body = new JSONArray(response.asString());
-            val tagName = body.getJSONObject(0).getString("tag_name");
+      val worker =
+          new SwingWorker<Boolean, Void>() {
+            @Override
+            protected Boolean doInBackground() {
+              try {
+                val request =
+                    Request.get(githubReleasesUrl.toURI())
+                        .addHeader("accept", "application/vnd.github+json")
+                        .addHeader("X-GitHub-Api-Version", "2022-11-28")
+                        .execute();
+                val response = request.returnContent();
+                val body = new JSONArray(response.asString());
+                val tagName = body.getJSONObject(0).getString("tag_name");
 
-            val buildTime = getManifestAttribute("Build-Time");
-            return Objects.compare(buildTime, tagName, String::compareTo) < 0;
-          } catch (IOException ioe) {
-            logger.error("Failed to check for updates",
-                ioe);
-          } catch (URISyntaxException urise) {
-            logger.error("Failed to parse {} as URI",
-                githubReleasesUrl,
-                urise);
-          }
-          return false;
-        }
-      };
+                val buildTime = getManifestAttribute("Build-Time");
+                return Objects.compare(buildTime, tagName, String::compareTo) < 0;
+              } catch (IOException ioe) {
+                logger.error("Failed to check for updates", ioe);
+              } catch (URISyntaxException urise) {
+                logger.error("Failed to parse {} as URI", githubReleasesUrl, urise);
+              }
+              return false;
+            }
+          };
       worker.execute();
 
       try {
         outdated = worker.get();
       } catch (ExecutionException ee) {
-        logger.error("Exception while checking for updates",
-            ee);
+        logger.error("Exception while checking for updates", ee);
         outdated = false;
       } catch (InterruptedException ie) {
-        logger.error("Interrupted while checking for updates",
-            ie);
+        logger.error("Interrupted while checking for updates", ie);
         outdated = false;
       } finally {
         worker.cancel(true);
@@ -160,17 +155,18 @@ public final class LauncherUtils {
       c1.revalidate();
       c1.repaint();
     } else {
-      SwingUtilities.invokeLater(() -> {
-        c1.removeAll();
-        c1.add(c2);
-        c1.revalidate();
-        c1.repaint();
-      });
+      SwingUtilities.invokeLater(
+          () -> {
+            c1.removeAll();
+            c1.add(c2);
+            c1.revalidate();
+            c1.repaint();
+          });
     }
   }
 
-  public static void updateContainerKeyValue(UTF8ResourceBundle bundle, Container c, String key,
-      Object... args) {
+  public static void updateContainerKeyValue(
+      UTF8ResourceBundle bundle, Container c, String key, Object... args) {
     if (c instanceof JFrame) {
       val frame = (JFrame) c;
       frame.setTitle(MessageFormat.format(bundle.getString(key), args));
@@ -181,18 +177,18 @@ public final class LauncherUtils {
     }
   }
 
-  public static void updateComponentKeyValue(UTF8ResourceBundle bundle, JComponent c, String key,
-      Object... args) {
+  public static void updateComponentKeyValue(
+      UTF8ResourceBundle bundle, JComponent c, String key, Object... args) {
     if (c instanceof AbstractButton) {
-      var button = (AbstractButton) c;
+      val button = (AbstractButton) c;
       button.setText(MessageFormat.format(bundle.getString(key), args));
     }
     if (c instanceof JGroupBox) {
-      var groupBox = (JGroupBox) c;
+      val groupBox = (JGroupBox) c;
       groupBox.setTitledBorder(MessageFormat.format(bundle.getString(key), args));
     }
     if (c instanceof JLabel) {
-      var label = (JLabel) c;
+      val label = (JLabel) c;
       label.setText(MessageFormat.format(bundle.getString(key), args));
     }
   }
@@ -203,9 +199,9 @@ public final class LauncherUtils {
         Desktop.getDesktop().browse(url.toURI());
       }
     } catch (IOException ioe) {
-      LauncherUtils.logger.error("Failed to open '{}' in browser", url, ioe);
+      LauncherUtils.logger.error("Failed to browse {}", url, ioe);
     } catch (URISyntaxException urise) {
-      LauncherUtils.logger.error("Failed to parse '{}' as URI", url, urise);
+      LauncherUtils.logger.error("Failed to parse {} as URI", url, urise);
     }
   }
 
@@ -215,7 +211,7 @@ public final class LauncherUtils {
         Desktop.getDesktop().open(p.toFile());
       }
     } catch (IOException ioe) {
-      LauncherUtils.logger.error("Failed to open '{}' in desktop", p, ioe);
+      LauncherUtils.logger.error("Failed to open {}", p, ioe);
     }
   }
 }
