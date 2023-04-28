2398187c96367be4f01bd3a5172edd795b8729ce
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-28 20:17:33 +0300

    minor formatting adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/misc/ui/JHyperlink.java
+++ src/main/java/io/github/kawaxte/twentyten/misc/ui/JHyperlink.java
@@ -8,17 +8,14 @@ import javax.swing.JLabel;
 public class JHyperlink extends JLabel {
 
   public JHyperlink(String text, int horizontalAlignment) {
-    super(MessageFormat.format("<html><u>{0}</u></html>",
-        text,
-        horizontalAlignment));
+    super(MessageFormat.format("<html><u>{0}</u></html>", text, horizontalAlignment));
 
     this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
   }
 
   @Override
   public void setText(String text) {
-    super.setText(MessageFormat.format("<html><u>{0}</u></html>",
-        text));
+    super.setText(MessageFormat.format("<html><u>{0}</u></html>", text));
   }
 
   @Override
