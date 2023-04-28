a178a8ca287b835635edfd8d0abce781169c92ea
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-28 20:18:29 +0300

    minor formatting adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/conf/LauncherConfig.java
+++ src/main/java/io/github/kawaxte/twentyten/conf/LauncherConfig.java
@@ -1,7 +1,6 @@
 package io.github.kawaxte.twentyten.conf;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,28 +20,29 @@ public final class LauncherConfig {
     logger = LogManager.getLogger(LauncherConfig.class);
 
     try {
-      versionsFileUrl = new URL(new StringBuilder()
-          .append("https://raw.githubusercontent.com/")
-          .append("Kawaxte/")
-          .append("TwentyTenLauncher/")
-          .append("nightly/")
-          .append("versions.json").toString());
-    } catch (MalformedURLException murle) {
-      logger.error("Failed to create URL for versions file", murle);
+      versionsFileUrl =
+          new URL(
+              new StringBuilder()
+                  .append("https://raw.githubusercontent.com/")
+                  .append("Kawaxte/")
+                  .append("TwentyTenLauncher/")
+                  .append("nightly/")
+                  .append("versions.json")
+                  .toString());
+    } catch (IOException ioe) {
+      logger.error("Failed to create URL for {}", versionsFileUrl.toString(), ioe);
     }
 
     VERSION_TYPES = Collections.unmodifiableList(Arrays.asList("beta", "alpha", "infdev"));
   }
 
-  private LauncherConfig() {
-  }
+  private LauncherConfig() {}
 
   public static void loadConfig() {
     try {
       AbstractLauncherConfigImpl.INSTANCE.loadConfig();
     } catch (IOException ioe) {
-      logger.error("Failed to load config file",
-          ioe);
+      logger.error("Failed to load config file", ioe);
     }
   }
 
@@ -50,8 +50,7 @@ public final class LauncherConfig {
     try {
       AbstractLauncherConfigImpl.INSTANCE.saveConfig();
     } catch (IOException ioe) {
-      logger.error("Failed to save config file",
-          ioe);
+      logger.error("Failed to save config file", ioe);
     }
   }
 }
