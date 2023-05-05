7f7bc294edbf2058a8620e6380f6edc89500da66
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-06 01:11:14 +0300

    rename `yggdrasil` to `mojang`

--- src/main/java/io/github/kawaxte/twentyten/conf/AbstractLauncherConfigImpl.java
+++ src/main/java/io/github/kawaxte/twentyten/conf/AbstractLauncherConfigImpl.java
@@ -6,58 +6,53 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import lombok.val;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class AbstractLauncherConfigImpl extends AbstractLauncherConfig {
 
   public static final AbstractLauncherConfigImpl INSTANCE;
-  static Logger logger;
 
   static {
     INSTANCE = new AbstractLauncherConfigImpl();
-
-    logger = LogManager.getLogger(AbstractLauncherConfigImpl.class);
   }
 
   @Override
-  public void loadConfig() throws IOException {
+  public void load() throws IOException {
     val linkedProperties = new LinkedProperties();
     try (val fis = new FileInputStream(this.getConfigFilePath().toFile())) {
       linkedProperties.load(fis);
 
-      this.getYggdrasilLoginProperties(linkedProperties);
-      this.getMicrosoftLoginProperties(linkedProperties);
+      this.getMojangAuthProperties(linkedProperties);
+      this.getMicrosoftAuthProperties(linkedProperties);
       this.getOptionsProperties(linkedProperties);
     } catch (FileNotFoundException fnfe) {
-      logger.error("Failed to locate {}", this.getConfigFilePath().toAbsolutePath(), fnfe);
+      LOGGER.error("Failed to locate {}", this.getConfigFilePath().toAbsolutePath(), fnfe);
     } finally {
       if (!linkedProperties.isEmpty()) {
-        logger.info(
-            "Loading {} from {}",
+        LOGGER.info(
+            "Load {} from {}",
             this.getConfigFilePath().getFileName(),
             this.getConfigFilePath().toAbsolutePath().getParent());
       } else {
-        this.saveConfig();
+        this.save();
       }
     }
   }
 
   @Override
-  public void saveConfig() throws IOException {
+  public void save() throws IOException {
     try (val fos = new FileOutputStream(this.getConfigFilePath().toFile())) {
       val linkedProperties = new LinkedProperties();
       this.setOptionsProperties(linkedProperties);
-      this.setMicrosoftLoginProperties(linkedProperties);
-      this.setYggdrasilLoginProperties(linkedProperties);
+      this.setMicrosoftAuthProperties(linkedProperties);
+      this.setMojangAuthProperties(linkedProperties);
       linkedProperties.store(fos, "TwentyTen Launcher");
 
       fos.flush();
     } catch (FileNotFoundException fnfe) {
-      logger.error("Failed to locate {}", this.getConfigFilePath().toAbsolutePath(), fnfe);
+      LOGGER.error("Failed to locate {}", this.getConfigFilePath().toAbsolutePath(), fnfe);
     } finally {
-      logger.info(
-          "Saving {} to {}",
+      LOGGER.info(
+          "Saved {} to {}",
           this.getConfigFilePath().getFileName(),
           this.getConfigFilePath().toAbsolutePath().getParent());
     }
