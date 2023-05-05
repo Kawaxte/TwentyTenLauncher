848c1392e4fc75e33057e0191dd49e099965f43c
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-06 01:11:28 +0300

    add `LOGGER` constant

--- src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguage.java
+++ src/main/java/io/github/kawaxte/twentyten/lang/AbstractLauncherLanguage.java
@@ -2,8 +2,16 @@ package io.github.kawaxte.twentyten.lang;
 
 import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
 import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 abstract class AbstractLauncherLanguage {
+
+  static final Logger LOGGER;
+
+  static {
+    LOGGER = LogManager.getLogger(AbstractLauncherLanguage.class);
+  }
 
   @Getter UTF8ResourceBundle utf8Bundle;
 
