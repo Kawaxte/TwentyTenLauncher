60269725ce7a920d444bf8268d4466fc4af23ded
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-06 21:43:33 +0300

    minor adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/launcher/AbstractLauncherLanguage.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/AbstractLauncherLanguage.java
@@ -1,6 +1,6 @@
-package io.github.kawaxte.twentyten.lang;
+package io.github.kawaxte.twentyten.launcher;
 
-import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
+import io.github.kawaxte.twentyten.UTF8ResourceBundle;
 import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,11 +13,11 @@ abstract class AbstractLauncherLanguage {
     LOGGER = LogManager.getLogger(AbstractLauncherLanguage.class);
   }
 
-  @Getter UTF8ResourceBundle utf8Bundle;
+  @Getter UTF8ResourceBundle bundle;
 
   {
-    this.utf8Bundle = new UTF8ResourceBundle();
+    this.bundle = new UTF8ResourceBundle();
   }
 
-  public abstract void loadLanguage(String baseName, String isoCode);
+  public abstract void load(String baseName, String isoCode);
 }
