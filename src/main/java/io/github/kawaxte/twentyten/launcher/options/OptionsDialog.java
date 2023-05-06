dd2d2e8738da3dfeefff3f4254c099b30b6ed8af
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-06 21:46:23 +0300

    minor adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/launcher/options/OptionsDialog.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/options/OptionsDialog.java
@@ -1,9 +1,11 @@
-package io.github.kawaxte.twentyten.ui.options;
+package io.github.kawaxte.twentyten.launcher.options;
 
-import io.github.kawaxte.twentyten.conf.AbstractLauncherConfigImpl;
-import io.github.kawaxte.twentyten.lang.LauncherLanguage;
-import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
-import io.github.kawaxte.twentyten.util.LauncherUtils;
+import static io.github.kawaxte.twentyten.launcher.util.LauncherConfigUtils.CONFIG;
+import static io.github.kawaxte.twentyten.launcher.util.LauncherConfigUtils.LANGUAGE;
+
+import io.github.kawaxte.twentyten.UTF8ResourceBundle;
+import io.github.kawaxte.twentyten.launcher.util.LauncherLanguageUtils;
+import io.github.kawaxte.twentyten.launcher.util.LauncherUtils;
 import java.awt.Window;
 import java.util.Objects;
 import javax.swing.JDialog;
@@ -28,11 +30,11 @@ public class OptionsDialog extends JDialog {
     this.setLocation(this.getOwner().getLocation());
     this.setResizable(false);
 
-    val selectedLanguage = AbstractLauncherConfigImpl.INSTANCE.getSelectedLanguage();
+    val selectedLanguage = CONFIG.getSelectedLanguage();
     this.updateContainerKeyValues(
         Objects.nonNull(selectedLanguage)
-            ? LauncherLanguage.getUtf8Bundle(selectedLanguage)
-            : LauncherLanguage.getUtf8Bundle());
+            ? LauncherLanguageUtils.getUTF8Bundle(selectedLanguage)
+            : LANGUAGE.getBundle());
   }
 
   public void updateContainerKeyValues(UTF8ResourceBundle bundle) {
