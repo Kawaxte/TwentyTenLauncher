6d8bf358ba7cb0fb4d8b1eef22907003171fc8db
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-06 21:47:24 +0300

    add `showVersionCheckboxes`, call `updateVersionComboBox(...)` on selection

--- src/main/java/io/github/kawaxte/twentyten/launcher/options/VersionGroupBox.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/options/VersionGroupBox.java
@@ -1,13 +1,18 @@
-package io.github.kawaxte.twentyten.ui.options;
+package io.github.kawaxte.twentyten.launcher.options;
 
-import io.github.kawaxte.twentyten.conf.AbstractLauncherConfigImpl;
-import io.github.kawaxte.twentyten.lang.LauncherLanguage;
-import io.github.kawaxte.twentyten.misc.UTF8ResourceBundle;
-import io.github.kawaxte.twentyten.misc.ui.JGroupBox;
-import io.github.kawaxte.twentyten.util.LauncherUtils;
+import static io.github.kawaxte.twentyten.launcher.util.LauncherConfigUtils.CONFIG;
+import static io.github.kawaxte.twentyten.launcher.util.LauncherConfigUtils.LANGUAGE;
+
+import io.github.kawaxte.twentyten.UTF8ResourceBundle;
+import io.github.kawaxte.twentyten.launcher.util.LauncherLanguageUtils;
+import io.github.kawaxte.twentyten.launcher.util.LauncherUtils;
+import io.github.kawaxte.twentyten.launcher.util.LauncherVersionUtils;
+import io.github.kawaxte.twentyten.ui.JGroupBox;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -15,6 +20,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import lombok.Getter;
 import lombok.val;
 
 public class VersionGroupBox extends JGroupBox implements ActionListener {
@@ -24,13 +30,20 @@ public class VersionGroupBox extends JGroupBox implements ActionListener {
   private final JCheckBox showBetaVersionsCheckBox;
   private final JCheckBox showAlphaVersionsCheckBox;
   private final JCheckBox showInfdevVersionsCheckBox;
+  private final JCheckBox[] showVersionCheckBoxes;
   private final JLabel useVersionLabel;
-  private final JComboBox<String> versionComboBox;
+  @Getter private final JComboBox<String> versionComboBox;
 
   {
     this.showBetaVersionsCheckBox = new JCheckBox("vgb.showVersionsCheckBox");
     this.showAlphaVersionsCheckBox = new JCheckBox("vgb.showVersionsCheckBox");
     this.showInfdevVersionsCheckBox = new JCheckBox("vgb.showVersionsCheckBox");
+    this.showVersionCheckBoxes =
+        new JCheckBox[] {
+          this.showBetaVersionsCheckBox,
+          this.showAlphaVersionsCheckBox,
+          this.showInfdevVersionsCheckBox
+        };
     this.useVersionLabel = new JLabel("vgb.useVersionLabel", SwingConstants.RIGHT);
     this.versionComboBox = new JComboBox<>();
   }
@@ -41,36 +54,50 @@ public class VersionGroupBox extends JGroupBox implements ActionListener {
     VersionGroupBox.instance = this;
     this.setLayout(this.getGroupLayout());
 
+    this.showBetaVersionsCheckBox.setSelected(CONFIG.isShowBetaVersionsSelected());
+    this.showAlphaVersionsCheckBox.setSelected(CONFIG.isShowAlphaVersionsSelected());
+    this.showInfdevVersionsCheckBox.setSelected(CONFIG.isShowInfdevVersionsSelected());
+
     this.showBetaVersionsCheckBox.addActionListener(this);
     this.showAlphaVersionsCheckBox.addActionListener(this);
     this.showInfdevVersionsCheckBox.addActionListener(this);
 
-    val selectedLanguage = AbstractLauncherConfigImpl.INSTANCE.getSelectedLanguage();
+    val selectedLanguage = CONFIG.getSelectedLanguage();
     this.updateComponentKeyValues(
         Objects.nonNull(selectedLanguage)
-            ? LauncherLanguage.getUtf8Bundle(selectedLanguage)
-            : LauncherLanguage.getUtf8Bundle());
+            ? LauncherLanguageUtils.getUTF8Bundle(selectedLanguage)
+            : LANGUAGE.getBundle());
+
+    LauncherVersionUtils.updateVersionComboBox(this);
   }
 
   public void updateComponentKeyValues(UTF8ResourceBundle bundle) {
+    val versionType = Collections.unmodifiableList(Arrays.asList("Beta", "Alpha", "Infdev"));
+    val versionReleaseRange =
+        Collections.unmodifiableList(
+            Arrays.asList(
+                "2010-12-20 -> 2011-09-15",
+                "2010-06-30 -> 2010-12-03",
+                "2010-02-27 -> 2010-06-30"));
+
     LauncherUtils.updateComponentKeyValue(
         bundle,
         this.showBetaVersionsCheckBox,
         "vgb.showVersionsCheckBox",
-        "Beta",
-        "2010-12-20 -> 2011-01-21");
+        versionType.get(0),
+        versionReleaseRange.get(0));
     LauncherUtils.updateComponentKeyValue(
         bundle,
         this.showAlphaVersionsCheckBox,
         "vgb.showVersionsCheckBox",
-        "Alpha",
-        "2010-07-02 -> 2010-12-03");
+        versionType.get(1),
+        versionReleaseRange.get(1));
     LauncherUtils.updateComponentKeyValue(
         bundle,
         this.showInfdevVersionsCheckBox,
         "vgb.showVersionsCheckBox",
-        "Infdev",
-        "2010-06-29 -> 2010-06-30");
+        versionType.get(2),
+        versionReleaseRange.get(2));
     LauncherUtils.updateComponentKeyValue(bundle, this.useVersionLabel, "vgb.useVersionLabel");
   }
 
@@ -109,5 +136,15 @@ public class VersionGroupBox extends JGroupBox implements ActionListener {
   @Override
   public void actionPerformed(ActionEvent event) {
     val source = event.getSource();
+
+    for (val versionCheckBox : this.showVersionCheckBoxes) {
+      if (source == versionCheckBox) {
+        CONFIG.setShowBetaVersionsSelected(this.showBetaVersionsCheckBox.isSelected());
+        CONFIG.setShowAlphaVersionsSelected(this.showAlphaVersionsCheckBox.isSelected());
+        CONFIG.setShowInfdevVersionsSelected(this.showInfdevVersionsCheckBox.isSelected());
+        LauncherVersionUtils.updateVersionComboBox(this);
+        break;
+      }
+    }
   }
 }
