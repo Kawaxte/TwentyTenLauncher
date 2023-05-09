7e87db0cb344627353534f7dfc5c443937892aa0
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-09 23:31:41 +0300

    remove disable/enable on YggdrasilAuthPanel comps

--- src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
@@ -1,10 +1,9 @@
 package io.github.kawaxte.twentyten.launcher;
 
 import io.github.kawaxte.twentyten.launcher.ui.LauncherFrame;
-import io.github.kawaxte.twentyten.launcher.ui.YggdrasilAuthPanel;
 import io.github.kawaxte.twentyten.launcher.util.MicrosoftAuthUtils;
 import io.github.kawaxte.twentyten.launcher.util.YggdrasilAuthUtils;
-import java.util.Arrays;
+import java.util.Objects;
 import javax.swing.SwingUtilities;
 import lombok.val;
 
@@ -14,26 +13,21 @@ public class Launcher {
     ELookAndFeel.setLookAndFeel();
 
     LauncherConfig.loadConfig();
-    LauncherLanguage.loadLanguage("messages", ELanguage.getLanguage());
+    LauncherLanguage.loadLanguage(
+        "messages",
+        Objects.nonNull(LauncherConfig.lookup.get("selectedLanguage"))
+            ? (String) LauncherConfig.lookup.get("selectedLanguage")
+            : ELanguage.USER_LANGUAGE);
 
-    SwingUtilities.invokeLater(
-        () -> {
-          new LauncherFrame().setVisible(true);
-
-          Arrays.stream(YggdrasilAuthPanel.instance.getComponents())
-              .forEachOrdered(component -> component.setEnabled(false));
-        });
+    SwingUtilities.invokeLater(() -> new LauncherFrame().setVisible(true));
 
     val microsoftAccessTokenExpired = MicrosoftAuthUtils.isAccessTokenExpired();
     val mojangAccessTokenExpired = YggdrasilAuthUtils.isAccessTokenExpired();
-    if (microsoftAccessTokenExpired) {
-      MicrosoftAuthUtils.refreshAccessToken();
-    }
     if (mojangAccessTokenExpired) {
       YggdrasilAuthUtils.refreshAccessToken();
     }
-
-    Arrays.stream(YggdrasilAuthPanel.instance.getComponents())
-        .forEachOrdered(component -> component.setEnabled(true));
+    if (microsoftAccessTokenExpired) {
+      MicrosoftAuthUtils.refreshAccessToken();
+    }
   }
 }
