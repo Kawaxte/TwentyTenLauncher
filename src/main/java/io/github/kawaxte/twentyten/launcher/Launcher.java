6e5a1388b2d37d2c5bb98303c8c28d9c2649f595
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-09 21:53:38 +0300

    disable YggdrasilAuthPanel's comps on start-up

--- src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
@@ -1,8 +1,11 @@
 package io.github.kawaxte.twentyten.launcher;
 
 import io.github.kawaxte.twentyten.launcher.ui.LauncherFrame;
+import io.github.kawaxte.twentyten.launcher.ui.YggdrasilAuthPanel;
 import io.github.kawaxte.twentyten.launcher.util.MicrosoftAuthUtils;
 import io.github.kawaxte.twentyten.launcher.util.YggdrasilAuthUtils;
+import java.util.Arrays;
+import java.util.Objects;
 import javax.swing.SwingUtilities;
 
 public class Launcher {
@@ -13,7 +16,15 @@ public class Launcher {
     LauncherConfig.loadConfig();
     LauncherLanguage.loadLanguage("messages", ELanguage.getLanguage());
 
-    SwingUtilities.invokeLater(() -> new LauncherFrame().setVisible(true));
+    SwingUtilities.invokeLater(
+        () -> {
+          new LauncherFrame().setVisible(true);
+
+          if (Objects.nonNull(YggdrasilAuthPanel.instance)) {
+            Arrays.stream(YggdrasilAuthPanel.instance.getComponents())
+                .forEachOrdered(component -> component.setEnabled(false));
+          }
+        });
 
     MicrosoftAuthUtils.checkAndRefreshAccessToken();
     YggdrasilAuthUtils.validateAndRefreshAccessToken();
