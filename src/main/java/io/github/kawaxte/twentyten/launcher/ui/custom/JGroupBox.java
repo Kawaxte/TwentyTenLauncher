420093762c1eeba99077c34a2fe4ccd0b0dc144b
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-10 17:26:12 +0300

    add documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/JGroupBox.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/JGroupBox.java
@@ -2,17 +2,40 @@ package io.github.kawaxte.twentyten.launcher.ui.custom;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
+/**
+ * This class extends the {@code JPanel} class to create a group box, which is simply a panel with a
+ * title and a border surrounding it.
+ *
+ * @author Kawaxte
+ */
 public class JGroupBox extends JPanel {
 
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Creates a new {@link JGroupBox} with the given title and sets the titled border.
+   *
+   * @param title the title of the group box
+   * @param isDoubleBuffered a boolean indicating whether or not to use double-buffering
+   */
   public JGroupBox(String title, boolean isDoubleBuffered) {
     super(isDoubleBuffered);
 
     this.setTitledBorder(title);
   }
 
+  /**
+   * Sets the titled border of the group box by creating an empty border and a titled border.
+   *
+   * @param title the title of the group box
+   * @return the title of the group box
+   * @see JPanel#setBorder( Border )
+   * @see BorderFactory#createCompoundBorder(Border, Border)
+   * @see BorderFactory#createEmptyBorder()
+   * @see BorderFactory#createTitledBorder(String)
+   */
   public String setTitledBorder(String title) {
     super.setBorder(
         BorderFactory.createCompoundBorder(
