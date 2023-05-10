61a1754d021b9995de826587ce330930604365e6
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-10 18:25:04 +0300

    remove documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/JGroupBox.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/JGroupBox.java
@@ -2,44 +2,17 @@ package io.github.kawaxte.twentyten.launcher.ui.custom;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
 
-/**
- * This class extends the {@code JPanel} class to create a group box, which is simply a panel with a
- * title and a border surrounding it.
- *
- * @author Kawaxte
- */
 public class JGroupBox extends JPanel {
 
   private static final long serialVersionUID = 1L;
 
-  /**
-   * Creates a new {@link JGroupBox} with a title, FlowLayout and the specified buffering strategy.
-   *
-   * <p>It also calls the {@code setTitledBorder(String)} method to set the titled border of the
-   * group box.
-   *
-   * @param title the title of the group box
-   * @param isDoubleBuffered a boolean, true for double-buffering, which uses additional memory
-   */
   public JGroupBox(String title, boolean isDoubleBuffered) {
     super(isDoubleBuffered);
 
     this.setTitledBorder(title);
   }
 
-  /**
-   * Sets the titled border of the group box by creating an empty border outside and a titled border
-   * inside of the compound border.
-   *
-   * @param title the title of the group box
-   * @return the title of the group box
-   * @see JPanel#setBorder( Border )
-   * @see BorderFactory#createCompoundBorder(Border, Border)
-   * @see BorderFactory#createEmptyBorder()
-   * @see BorderFactory#createTitledBorder(String)
-   */
   public String setTitledBorder(String title) {
     super.setBorder(
         BorderFactory.createCompoundBorder(
--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/TransparentJCheckBox.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/TransparentJCheckBox.java
@@ -1,30 +1,13 @@
 package io.github.kawaxte.twentyten.launcher.ui.custom;
 
 import javax.swing.JCheckBox;
-import javax.swing.JComponent;
 
-/**
- * This class extends the {@link JCheckBox} class to make it transparent.
- *
- * @author Kawaxte
- */
 public class TransparentJCheckBox extends JCheckBox {
 
-  /**
-   * Creates a new {@link TransparentJCheckBox} with the given text.
-   *
-   * @param text the text to display on the checkbox
-   */
   public TransparentJCheckBox(String text) {
     super(text);
   }
 
-  /**
-   * Overrides the {@code isOpaque} method to return false, making the checkbox transparent.
-   *
-   * @return false
-   * @see JComponent#isOpaque()
-   */
   @Override
   public boolean isOpaque() {
     return false;
