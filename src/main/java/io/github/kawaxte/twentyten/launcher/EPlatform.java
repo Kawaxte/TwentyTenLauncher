517a6712f859f71ec82515bff2250341a5047bb0
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-11 10:23:38 +0300

    add methods for detecting OS architecture

--- src/main/java/io/github/kawaxte/twentyten/launcher/EPlatform.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/EPlatform.java
@@ -1,47 +1,74 @@
 package io.github.kawaxte.twentyten.launcher;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
 public enum EPlatform {
-  LINUX("aix,", "nix", "nux"),
-  MACOS("darwin", "mac"),
-  WINDOWS("win");
+  LINUX(new String[] {"aix", "nix", "nux"}),
+  MACOS(new String[] {"darwin", "mac"}),
+  WINDOWS(new String[] {"win"}),
+  AARCH64("aarch64"),
+  AMD64("amd64"),
+  X86("x86");
 
   public static final String OS_NAME;
+  public static final String OS_ARCH;
 
   static {
     OS_NAME = System.getProperty("os.name");
+    OS_ARCH = System.getProperty("os.arch");
   }
 
-  private final List<String> osNames;
+  private String[] names;
+  private String arch;
 
-  EPlatform(String... osNames) {
-    this.osNames = Collections.unmodifiableList(Arrays.asList(osNames));
+  EPlatform(String[] names) {
+    this.names = names;
   }
 
-  public static EPlatform getPlatform() {
+  EPlatform(String arch) {
+    this.arch = arch;
+  }
+
+  public static EPlatform getOSName() {
     return Arrays.stream(values())
         .filter(
             platform ->
-                platform.osNames.stream()
-                    .anyMatch(osName -> OS_NAME.toLowerCase(Locale.ROOT).contains(osName)))
+                Arrays.stream(platform.names)
+                    .anyMatch(name -> OS_NAME.toLowerCase(Locale.ROOT).contains(name)))
+        .findFirst()
+        .orElse(null);
+  }
+
+  public static EPlatform getOSArch() {
+    return Arrays.stream(values())
+        .filter(platform -> platform.arch != null && platform.arch.equalsIgnoreCase(OS_ARCH))
         .findFirst()
         .orElse(null);
   }
 
   public static boolean isWindows() {
-    return Objects.equals(WINDOWS, getPlatform());
+    return Objects.equals(WINDOWS, getOSName());
   }
 
   public static boolean isMacOS() {
-    return Objects.equals(MACOS, getPlatform());
+    return Objects.equals(MACOS, getOSName());
   }
 
   public static boolean isLinux() {
-    return Objects.equals(LINUX, getPlatform());
+    return Objects.equals(LINUX, getOSName());
+  }
+
+  public static boolean isAARCH64() {
+    return Objects.equals(AARCH64, getOSArch());
+  }
+
+  public static boolean isAMD64() {
+    return Objects.equals(AMD64, getOSArch());
+  }
+
+  public static boolean isX86() {
+    return Objects.equals(X86, getOSArch());
   }
 }
