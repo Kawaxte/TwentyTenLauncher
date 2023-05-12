fe44a88197db02b0f4e122afa7835465e5cec2e5
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-12 21:03:03 +0300

    add various variables related to GameUpdater, remove graphics for GameUpdater

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/LauncherPanel.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/LauncherPanel.java
@@ -1,7 +1,5 @@
 package io.github.kawaxte.twentyten.launcher.ui;
 
-import io.github.kawaxte.twentyten.launcher.LauncherLanguage;
-import io.github.kawaxte.twentyten.launcher.game.EState;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
@@ -13,28 +11,12 @@ import java.awt.Transparency;
 import java.util.Optional;
 import java.util.stream.IntStream;
 import javax.swing.JPanel;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.val;
 
 public class LauncherPanel extends JPanel {
 
   private static final long serialVersionUID = 1L;
   public static LauncherPanel instance;
-  @Getter @Setter private String taskStateMessage;
-  @Getter @Setter private String taskProgressMessage;
-  @Getter @Setter private int taskState;
-  @Getter @Setter private int taskProgress;
-  @Getter @Setter private boolean updaterTaskStarted;
-  @Getter @Setter private boolean updaterTaskFinished;
-  @Getter @Setter private boolean updaterTaskErrored;
-
-  {
-    this.taskState = EState.INITIALISE.ordinal();
-    this.taskStateMessage = EState.INITIALISE.getMessage();
-    this.taskProgressMessage = "";
-    this.taskProgress = 0;
-  }
 
   public LauncherPanel() {
     super(new GridBagLayout(), true);
@@ -82,67 +64,12 @@ public class LauncherPanel extends JPanel {
               });
       g2dBuffered.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
 
-      if (!this.updaterTaskStarted) {
-        this.drawTitleString("TwentyTen Launcher", panelWidth, panelHeight, g2dBuffered);
-      } else {
-        val title =
-            this.updaterTaskErrored
-                ? LauncherLanguage.bundle.getString("lp.updaterErrored")
-                : LauncherLanguage.bundle.getString("lp.updaterStarted");
-        this.drawTitleString(title, panelWidth, panelHeight, g2dBuffered);
-        this.drawTaskStateString(this.taskStateMessage, panelWidth, panelHeight, g2dBuffered);
-
-        if (!this.updaterTaskErrored) {
-          this.drawTaskProgressString(
-              this.taskProgressMessage, panelWidth, panelHeight, g2dBuffered);
-          this.drawTaskProgressRect(panelWidth, panelHeight, g2dBuffered);
-        }
-      }
+      this.drawTitleString("TwentyTen Launcher", panelWidth, panelHeight, g2dBuffered);
     } finally {
       g2dBuffered.dispose();
     }
 
     g2d.drawImage(bufferedImage, 0, 0, panelWidth, panelHeight, this);
-  }
-
-  private void drawTaskProgressRect(int width, int height, Graphics2D g2d) {
-    int rectX = 64;
-    int rectY = (height >> 1) - rectX;
-    int rectWidth = (width >> 1) - 128;
-    int rectHeight = 5;
-
-    g2d.setColor(Color.BLACK);
-    g2d.fillRect(rectX, rectY, rectWidth + 1, rectHeight);
-
-    g2d.setColor(Color.GREEN.darker().darker());
-    g2d.fillRect(rectX, rectY, (this.taskProgress * (rectWidth)) / 100, rectHeight - 1);
-
-    g2d.setColor(Color.GREEN.darker());
-    g2d.fillRect(rectX, rectY + 1, ((this.taskProgress * rectWidth) / 100) - 2, rectHeight - 4);
-  }
-
-  private void drawTaskProgressString(String s, int width, int height, Graphics2D g2d) {
-    g2d.setFont(this.getFont().deriveFont(Font.PLAIN, 12f));
-    g2d.setColor(Color.LIGHT_GRAY);
-
-    val fm = g2d.getFontMetrics();
-    int titleWidth = fm.stringWidth(s);
-    int titleHeight = fm.getHeight();
-    int titleX = (width >> 1 >> 1) - (titleWidth >> 1);
-    int titleY = (height >> 1 >> 1) + (titleHeight << 1);
-    g2d.drawString(s, titleX, titleY);
-  }
-
-  private void drawTaskStateString(String s, int width, int height, Graphics2D g2d) {
-    g2d.setFont(this.getFont().deriveFont(Font.PLAIN, 12f));
-    g2d.setColor(Color.LIGHT_GRAY);
-
-    val fm = g2d.getFontMetrics();
-    int titleWidth = fm.stringWidth(s);
-    int titleHeight = fm.getHeight();
-    int titleX = (width >> 1 >> 1) - (titleWidth >> 1);
-    int titleY = (height >> 1 >> 1) + (titleHeight);
-    g2d.drawString(s, titleX, titleY);
   }
 
   private void drawTitleString(String s, int width, int height, Graphics2D g2d) {
