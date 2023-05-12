228c3cae9c774d7907fd0395b218f0446c07c15f
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-12 20:58:25 +0300

    add `launchMinecraft(...)`

--- src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
@@ -1,6 +1,8 @@
 package io.github.kawaxte.twentyten.launcher;
 
+import io.github.kawaxte.twentyten.launcher.ui.GameAppletWrapper;
 import io.github.kawaxte.twentyten.launcher.ui.LauncherFrame;
+import io.github.kawaxte.twentyten.launcher.ui.LauncherPanel;
 import io.github.kawaxte.twentyten.launcher.util.MicrosoftAuthUtils;
 import io.github.kawaxte.twentyten.launcher.util.YggdrasilAuthUtils;
 import java.util.Objects;
@@ -29,5 +31,21 @@ public class Launcher {
     if (microsoftAccessTokenExpired) {
       MicrosoftAuthUtils.refreshAccessToken();
     }
+  }
+
+  public static void launchMinecraft(String username, String accessToken, String id) {
+    val sessionId =
+        new StringBuilder().append("token:").append(accessToken).append(":").append(id).toString();
+
+    val gameAppletWrapper = new GameAppletWrapper(username, sessionId);
+    gameAppletWrapper.init();
+
+    LauncherFrame.instance.remove(LauncherPanel.instance);
+    LauncherFrame.instance.setContentPane(gameAppletWrapper);
+    LauncherFrame.instance.revalidate();
+    LauncherFrame.instance.repaint();
+
+    gameAppletWrapper.start();
+    LauncherFrame.instance.setTitle("Minecraft");
   }
 }
