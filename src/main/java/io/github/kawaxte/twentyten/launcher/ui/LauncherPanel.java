32cd5f3add68146ca641b610dccaed1f7c8067f1
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-12 16:14:56 +0300

    add `Updating Minecraft` graphics in `paintComponent(...)`

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/LauncherPanel.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/LauncherPanel.java
@@ -1,5 +1,7 @@
 package io.github.kawaxte.twentyten.launcher.ui;
 
+import io.github.kawaxte.twentyten.launcher.LauncherLanguage;
+import io.github.kawaxte.twentyten.launcher.game.EState;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
@@ -8,14 +10,31 @@ import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Transparency;
+import java.util.Optional;
 import java.util.stream.IntStream;
 import javax.swing.JPanel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.val;
 
 public class LauncherPanel extends JPanel {
 
   private static final long serialVersionUID = 1L;
   public static LauncherPanel instance;
+  @Getter @Setter private String taskStateMessage;
+  @Getter @Setter private String taskProgressMessage;
+  @Getter @Setter private int taskState;
+  @Getter @Setter private int taskProgress;
+  @Getter @Setter private boolean updaterTaskStarted;
+  @Getter @Setter private boolean updaterTaskFinished;
+  @Getter @Setter private boolean updaterTaskErrored;
+
+  {
+    this.taskState = EState.INITIALISE.ordinal();
+    this.taskStateMessage = EState.INITIALISE.getMessage();
+    this.taskProgressMessage = "";
+    this.taskProgress = 0;
+  }
 
   public LauncherPanel() {
     super(new GridBagLayout(), true);
@@ -33,48 +52,108 @@ public class LauncherPanel extends JPanel {
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
 
-    val image = this.getToolkit().getImage(getClass().getResource("/dirt.png"));
-    int imageWidth = image.getWidth(this) << 1;
-    int imageHeight = image.getHeight(this) << 1;
-
+    val bgImageUrl =
+        Optional.ofNullable(this.getClass().getClassLoader().getResource("dirt.png"))
+            .orElseThrow(() -> new NullPointerException("bgImageUrl cannot be null"));
+    val bgImage = this.getToolkit().getImage(bgImageUrl);
+    int bgImageWidth = bgImage.getWidth(this) << 1;
+    int bgImageheight = bgImage.getHeight(this) << 1;
     int panelWidth = this.getWidth();
     int panelHeight = this.getHeight();
 
     val g2d = (Graphics2D) g;
-    val g2dConfiguration = g2d.getDeviceConfiguration();
+    val deviceConfiguration = g2d.getDeviceConfiguration();
 
     val bufferedImage =
-        g2dConfiguration.createCompatibleImage(
+        deviceConfiguration.createCompatibleImage(
             panelWidth >> 1, panelHeight >> 1, Transparency.OPAQUE);
-    val bufferedImageG2d = bufferedImage.createGraphics();
-    bufferedImageG2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25F));
+    val g2dBuffered = bufferedImage.createGraphics();
+    g2dBuffered.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.25f));
     try {
-      int gridWidth = (panelWidth + imageWidth) >> 5;
-      int gridHeight = (panelHeight + imageHeight) >> 5;
+      int gridWidth = (panelWidth + bgImageWidth) >> 5;
+      int gridHeight = (panelHeight + bgImageheight) >> 5;
       IntStream.range(0, (gridWidth * gridHeight))
           .parallel()
           .forEach(
               i -> {
                 int gridX = (i % gridWidth) << 5;
                 int gridY = (i / gridWidth) << 5;
-                bufferedImageG2d.drawImage(image, gridX, gridY, imageWidth, imageHeight, this);
+                g2dBuffered.drawImage(bgImage, gridX, gridY, bgImageWidth, bgImageheight, this);
               });
-      bufferedImageG2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1F));
+      g2dBuffered.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
 
-      String title = "TwentyTen Launcher";
-      bufferedImageG2d.setFont(this.getFont().deriveFont(Font.BOLD, 20F));
-      bufferedImageG2d.setColor(Color.LIGHT_GRAY);
+      if (!this.updaterTaskStarted) {
+        this.drawTitleString("TwentyTen Launcher", panelWidth, panelHeight, g2dBuffered);
+      } else {
+        val title =
+            this.updaterTaskErrored
+                ? LauncherLanguage.bundle.getString("lp.updaterErrored")
+                : LauncherLanguage.bundle.getString("lp.updaterStarted");
+        this.drawTitleString(title, panelWidth, panelHeight, g2dBuffered);
+        this.drawTaskStateString(this.taskStateMessage, panelWidth, panelHeight, g2dBuffered);
 
-      val fm = bufferedImageG2d.getFontMetrics();
-      int titleWidth = fm.stringWidth(title);
-      int titleHeight = fm.getHeight();
-      int titleX = (panelWidth >> 2) - (titleWidth >> 1);
-      int titleY = (panelHeight >> 2) - (titleHeight << 1);
-      bufferedImageG2d.drawString(title, titleX, titleY);
+        if (!this.updaterTaskErrored) {
+          this.drawTaskProgressString(
+              this.taskProgressMessage, panelWidth, panelHeight, g2dBuffered);
+          this.drawTaskProgressRect(panelWidth, panelHeight, g2dBuffered);
+        }
+      }
     } finally {
-      bufferedImageG2d.dispose();
+      g2dBuffered.dispose();
     }
 
     g2d.drawImage(bufferedImage, 0, 0, panelWidth, panelHeight, this);
+  }
+
+  private void drawTaskProgressRect(int width, int height, Graphics2D g2d) {
+    int rectX = 64;
+    int rectY = (height >> 1) - rectX;
+    int rectWidth = (width >> 1) - 128;
+    int rectHeight = 5;
+
+    g2d.setColor(Color.BLACK);
+    g2d.fillRect(rectX, rectY, rectWidth + 1, rectHeight);
+
+    g2d.setColor(Color.GREEN.darker().darker());
+    g2d.fillRect(rectX, rectY, (this.taskProgress * (rectWidth)) / 100, rectHeight - 1);
+
+    g2d.setColor(Color.GREEN.darker());
+    g2d.fillRect(rectX, rectY + 1, ((this.taskProgress * rectWidth) / 100) - 2, rectHeight - 4);
+  }
+
+  private void drawTaskProgressString(String s, int width, int height, Graphics2D g2d) {
+    g2d.setFont(this.getFont().deriveFont(Font.PLAIN, 12f));
+    g2d.setColor(Color.LIGHT_GRAY);
+
+    val fm = g2d.getFontMetrics();
+    int titleWidth = fm.stringWidth(s);
+    int titleHeight = fm.getHeight();
+    int titleX = (width >> 1 >> 1) - (titleWidth >> 1);
+    int titleY = (height >> 1 >> 1) + (titleHeight << 1);
+    g2d.drawString(s, titleX, titleY);
+  }
+
+  private void drawTaskStateString(String s, int width, int height, Graphics2D g2d) {
+    g2d.setFont(this.getFont().deriveFont(Font.PLAIN, 12f));
+    g2d.setColor(Color.LIGHT_GRAY);
+
+    val fm = g2d.getFontMetrics();
+    int titleWidth = fm.stringWidth(s);
+    int titleHeight = fm.getHeight();
+    int titleX = (width >> 1 >> 1) - (titleWidth >> 1);
+    int titleY = (height >> 1 >> 1) + (titleHeight);
+    g2d.drawString(s, titleX, titleY);
+  }
+
+  private void drawTitleString(String s, int width, int height, Graphics2D g2d) {
+    g2d.setFont(this.getFont().deriveFont(Font.BOLD, 20f));
+    g2d.setColor(Color.LIGHT_GRAY);
+
+    val fm = g2d.getFontMetrics();
+    int titleWidth = fm.stringWidth(s);
+    int titleHeight = fm.getHeight();
+    int titleX = (width >> 1 >> 1) - (titleWidth >> 1);
+    int titleY = (height >> 1 >> 1) - (titleHeight << 1);
+    g2d.drawString(s, titleX, titleY);
   }
 }
