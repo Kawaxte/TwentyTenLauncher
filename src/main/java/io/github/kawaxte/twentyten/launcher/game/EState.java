92fa8528a3e0c234ade4b069a975ffb0971636fc
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-13 20:12:31 +0300

    fix issue where text does not update on language change

--- src/main/java/io/github/kawaxte/twentyten/launcher/game/EState.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/game/EState.java
@@ -1,19 +1,22 @@
 package io.github.kawaxte.twentyten.launcher.game;
 
+import io.github.kawaxte.twentyten.launcher.LauncherConfig;
 import io.github.kawaxte.twentyten.launcher.LauncherLanguage;
 import lombok.Getter;
+import lombok.val;
 
 public enum EState {
-  INITIALISE(LauncherLanguage.bundle.getString("es_enum.initialise")),
-  CHECK_CACHE(LauncherLanguage.bundle.getString("es_enum.checkCache")),
-  DOWNLOAD_PACKAGES(LauncherLanguage.bundle.getString("es_enum.downloadPackages")),
-  EXTRACT_PACKAGES(LauncherLanguage.bundle.getString("es_enum.extractPackages")),
-  UPDATE_CLASSPATH(LauncherLanguage.bundle.getString("es_enum.updateClasspath")),
-  DONE(LauncherLanguage.bundle.getString("es_enum.done"));
+  INITIALISE("es_enum.initialise"),
+  CHECK_CACHE("es_enum.checkCache"),
+  DOWNLOAD_PACKAGES("es_enum.downloadPackages"),
+  EXTRACT_PACKAGES("es_enum.extractPackages"),
+  UPDATE_CLASSPATH("es_enum.updateClasspath"),
+  DONE("es_enum.done");
 
   @Getter private final String message;
 
   EState(String message) {
-    this.message = message;
+    val selectedLanguage = (String) LauncherConfig.lookup.get("selectedLanguage");
+    this.message = LauncherLanguage.getUTF8Bundle(selectedLanguage).getString(message);
   }
 }
