b63de8228a54b2cad415137b66cabc1838e63e18
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-19 14:50:49 +0300

    add license notice

--- src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/Launcher.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2023 Kawaxte
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package io.github.kawaxte.twentyten.launcher;
 
 import io.github.kawaxte.twentyten.launcher.ui.GameAppletWrapper;
