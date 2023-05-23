e8ab28692043cec0029413d9f38a254e4305ffdd
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-23 22:05:21 +0300

    logging & field dec adjustment(s), add `getKeys()`, `get(...)` & `set(...)`

--- src/main/java/io/github/kawaxte/twentyten/launcher/LauncherConfig.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/LauncherConfig.java
@@ -12,11 +12,11 @@
  * You should have received a copy of the GNU Lesser General Public License along with this
  * program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package io.github.kawaxte.twentyten.launcher;
 
-import static io.github.kawaxte.twentyten.launcher.util.LauncherUtils.workingDirectoryPath;
-
 import io.github.kawaxte.twentyten.LinkedProperties;
+import io.github.kawaxte.twentyten.launcher.util.LauncherUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -24,7 +24,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -36,51 +35,63 @@ import org.apache.logging.log4j.Logger;
 public final class LauncherConfig {
 
   private static final Logger LOGGER;
-  public static Map<String, Object> lookup;
+  private static final Map<String, Object> PROPERTIES_MAP;
 
   static {
     LOGGER = LogManager.getLogger(LauncherConfig.class);
 
-    lookup = new LinkedHashMap<>();
-    lookup.put("selectedLanguage", "en");
-    lookup.put("showBetaVersionsSelected", true);
-    lookup.put("showAlphaVersionsSelected", false);
-    lookup.put("showInfdevVersionsSelected", false);
-    lookup.put("selectedVersion", "b1.1_02");
-    lookup.put("microsoftProfileId", null);
-    lookup.put("microsoftProfileName", null);
-    lookup.put("microsoftAccessToken", null);
-    lookup.put("microsoftAccessTokenExpiresIn", 0L);
-    lookup.put("microsoftRefreshToken", null);
-    lookup.put("microsoftClientToken", UUID.randomUUID().toString().replace("-", ""));
-    lookup.put("mojangUsername", null);
-    lookup.put("mojangPassword", null);
-    lookup.put("mojangRememberPasswordChecked", false);
-    lookup.put("mojangProfileId", null);
-    lookup.put("mojangProfileName", null);
-    lookup.put("mojangProfileLegacy", false);
-    lookup.put("mojangAccessToken", null);
-    lookup.put("mojangClientToken", UUID.randomUUID().toString().replace("-", ""));
+    PROPERTIES_MAP = new LinkedHashMap<>();
+    PROPERTIES_MAP.put("selectedLanguage", "en"); // 0
+    PROPERTIES_MAP.put("showBetaVersionsSelected", true); // 1
+    PROPERTIES_MAP.put("showAlphaVersionsSelected", false); // 2
+    PROPERTIES_MAP.put("showInfdevVersionsSelected", false); // 3
+    PROPERTIES_MAP.put("selectedVersion", "b1.1_02"); // 4
+    PROPERTIES_MAP.put("microsoftProfileId", null); // 5
+    PROPERTIES_MAP.put("microsoftProfileName", null); // 6
+    PROPERTIES_MAP.put("microsoftAccessToken", null); // 7
+    PROPERTIES_MAP.put("microsoftAccessTokenExpiresIn", 0L); // 8
+    PROPERTIES_MAP.put("microsoftRefreshToken", null); // 9
+    PROPERTIES_MAP.put("microsoftClientToken", UUID.randomUUID().toString().replace("-", "")); // 10
+    PROPERTIES_MAP.put("mojangUsername", null); // 11
+    PROPERTIES_MAP.put("mojangPassword", null); // 12
+    PROPERTIES_MAP.put("mojangRememberPasswordChecked", false); // 13
+    PROPERTIES_MAP.put("mojangProfileId", null); // 14
+    PROPERTIES_MAP.put("mojangProfileName", null); // 15
+    PROPERTIES_MAP.put("mojangProfileLegacy", false); // 16
+    PROPERTIES_MAP.put("mojangAccessToken", null); // 17
+    PROPERTIES_MAP.put("mojangClientToken", UUID.randomUUID().toString().replace("-", "")); // 18
   }
 
   private LauncherConfig() {}
 
-  private static Path getConfigFilePath() {
+  private static Path getFilePath() {
     String userName = System.getProperty("user.name");
     String fileName = String.format("%s_%s.properties", "twentyten", userName);
 
-    Path filePath = Paths.get(String.valueOf(workingDirectoryPath), fileName);
-    File configFile = filePath.toFile();
+    Path filePath = LauncherUtils.WORKING_DIRECTORY_PATH.resolve(fileName);
+    File file = filePath.toFile();
     try {
-      return !configFile.exists() && !configFile.createNewFile() ? null : filePath;
+      return !file.exists() && !file.createNewFile() ? null : filePath;
     } catch (IOException ioe) {
-      LOGGER.error("Cannot create {}", configFile.getAbsolutePath(), ioe);
+      LOGGER.error("Cannot create {}", filePath, ioe);
     }
     return null;
   }
 
+  private static Object[] getKeys() {
+    return PROPERTIES_MAP.keySet().toArray();
+  }
+
+  public static Object get(int index) {
+    return PROPERTIES_MAP.get(getKeys()[index].toString());
+  }
+
+  public static void set(int index, Object value) {
+    PROPERTIES_MAP.put(getKeys()[index].toString(), value);
+  }
+
   public static void loadConfig() {
-    Path filePath = getConfigFilePath();
+    Path filePath = getFilePath(); // this is problematic for whatever reason
     if (Objects.isNull(filePath)) {
       return;
     }
@@ -88,16 +99,17 @@ public final class LauncherConfig {
     URI filePathUri = filePath.toUri();
 
     LinkedProperties properties = new LinkedProperties();
+
     try (FileInputStream fis = new FileInputStream(filePath.toFile())) {
       properties.load(fis);
-      properties.forEach((key, value) -> lookup.put((String) key, value));
+      properties.forEach((key, value) -> PROPERTIES_MAP.put((String) key, value));
     } catch (FileNotFoundException fnfe) {
       LOGGER.error("Cannot find {}", filePathUri, fnfe);
     } catch (IOException ioe) {
       LOGGER.error("Cannot load {}", filePathUri, ioe);
     } finally {
       if (!properties.isEmpty()) {
-        LOGGER.info("Loaded {}", filePathUri);
+        LOGGER.info("Loading {}", filePathUri);
       } else {
         saveConfig();
       }
@@ -105,7 +117,7 @@ public final class LauncherConfig {
   }
 
   public static void saveConfig() {
-    Path filePath = getConfigFilePath();
+    Path filePath = getFilePath();
     if (Objects.isNull(filePath)) {
       return;
     }
@@ -115,11 +127,11 @@ public final class LauncherConfig {
     try (FileOutputStream fos = new FileOutputStream(filePath.toFile())) {
       LinkedProperties properties = new LinkedProperties();
 
-      lookup
+      PROPERTIES_MAP
           .keySet()
           .forEach(
               key -> {
-                Object value = lookup.get(key);
+                Object value = PROPERTIES_MAP.get(key);
                 properties.put(key, Optional.ofNullable(value).map(Object::toString).orElse(""));
               });
       properties.store(fos, "TwentyTen Launcher");
@@ -131,7 +143,7 @@ public final class LauncherConfig {
       LOGGER.error("Cannot save {}", filePathUri, ioe);
     } finally {
       if (filePath.toFile().exists()) {
-        LOGGER.info("Saved {}", filePathUri);
+        LOGGER.info("Saving {}", filePathUri);
       }
     }
   }
