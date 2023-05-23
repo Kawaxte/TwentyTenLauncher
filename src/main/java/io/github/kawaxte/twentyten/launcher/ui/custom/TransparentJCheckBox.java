a95e580cd3d32419f6f058eee8ac75146dd27a0f
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-23 22:05:34 +0300

    new-line adjustment(s)

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/TransparentJCheckBox.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/TransparentJCheckBox.java
@@ -12,6 +12,7 @@
  * You should have received a copy of the GNU Lesser General Public License along with this
  * program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package io.github.kawaxte.twentyten.launcher.ui.custom;
 
 import javax.swing.JCheckBox;
