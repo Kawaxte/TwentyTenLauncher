28b9fe8a52cda1ba9f7be6f86f2ec8c14924caaa
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-26 18:57:53 +0300

    add documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/TransparentJButton.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/custom/TransparentJButton.java
@@ -20,18 +20,41 @@ import java.util.Objects;
 import javax.swing.JButton;
 import javax.swing.UIManager;
 
+/**
+ * This class extends {@link javax.swing.JButton} and overrides the {@link
+ * javax.swing.JComponent#isOpaque()} method to provide a tailored behaviour for button transparency
+ * in certain Windows environments. Specifically, it provides support for Windows NT kernels between
+ * version 5.1 and 6.1 when set to "Windows Classic" theme.
+ *
+ * <p>In these specific environments, a standard opaque setting can cause the button's core drawn
+ * elements to completely disappear. The overridden implementation in this class addresses this
+ * issue, ensuring the button is displayed as expected.
+ *
+ * @see javax.swing.JButton
+ * @author Kawaxte
+ * @since 1.4.0823_01
+ */
 public class TransparentJButton extends JButton {
 
   public TransparentJButton(String text) {
     super(text);
   }
 
+  /**
+   * This method has been overridden to provide custom behaviour in certain Windows environments
+   * where the standard opaque setting causes the button's core drawn elements to disappear. This
+   * particularly affects users who have their theme set to "Windows Classic" on Windows NT kernels
+   * between version 5.0 and 6.1.
+   *
+   * @return {@code true} if the {@link javax.swing.JButton} should be opaque, {@code false}
+   *     otherwise.
+   */
   @Override
   public boolean isOpaque() {
     Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
-    String windowsId = UIManager.getLookAndFeel().getID();
+    String id = UIManager.getLookAndFeel().getID();
 
-    boolean windows = Objects.equals(windowsId, "Windows");
+    boolean windows = Objects.equals(id, "Windows");
     Boolean winXpStyleThemeActive =
         (Boolean) defaultToolkit.getDesktopProperty("win.xpstyle.themeActive");
     boolean windowsClassic = UIManager.getLookAndFeel().getName().equals("Windows Classic");
