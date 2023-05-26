57c956b3f2e73fdec7a2d2bfa01b4c75921c02cb
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-26 14:56:31 +0300

    add documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/game/EState.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/game/EState.java
@@ -21,6 +21,31 @@ import io.github.kawaxte.twentyten.launcher.LauncherLanguage;
 import io.github.kawaxte.twentyten.launcher.util.LauncherLanguageUtils;
 import lombok.Getter;
 
+/**
+ * This enum represents the various states the {@link
+ * io.github.kawaxte.twentyten.launcher.game.GameUpdater} can be in during its execution.
+ *
+ * <p>Each constant of this enum corresponds to a specific operation in the update process, and is
+ * associated with a message that describes this operation. These messages are internationalised
+ * strings stored in resource bundles, and are retrieved based on the current launcher language.
+ *
+ * <p>The state constants and their corresponding operations are as follows:
+ *
+ * <ol>
+ *   <li>INITIALISE: The update process is being initialised.
+ *   <li>CHECK_CACHE: The cache for existing files is being checked.
+ *   <li>DOWNLOAD_PACKAGES: Packages necessary for the update are being downloaded.
+ *   <li>EXTRACT_PACKAGES: The downloaded packages are being extracted.
+ *   <li>UPDATE_CLASSPATH: The classpath is being updated.
+ *   <li>DONE: The update process is complete.
+ * </ol>
+ *
+ * <p>These states are used by the {@link io.github.kawaxte.twentyten.launcher.game.GameUpdater} to
+ * determine and display the current operation being performed.
+ *
+ * @author Kawaxte
+ * @since 1.5.1023_04
+ */
 public enum EState {
   INITIALISE(LauncherLanguageUtils.getESEnumKeys()[0]),
   CHECK_CACHE(LauncherLanguageUtils.getESEnumKeys()[1]),
@@ -31,9 +56,16 @@ public enum EState {
 
   @Getter private final String message;
 
-  EState(String message) {
+  /**
+   * Constructor for each enum constant. It takes a key which is used to retrieve the corresponding
+   * message from the resource bundle based on the current launcher language.
+   *
+   * @param key The key used to retrieve the corresponding message from the resource bundle.
+   * @see LauncherLanguageUtils#getESEnumKeys()
+   */
+  EState(String key) {
     String selectedLanguage = (String) LauncherConfig.get(0);
     UTF8ResourceBundle bundle = LauncherLanguage.getUTF8Bundle(selectedLanguage);
-    this.message = bundle.getString(message);
+    this.message = bundle.getString(key);
   }
 }
