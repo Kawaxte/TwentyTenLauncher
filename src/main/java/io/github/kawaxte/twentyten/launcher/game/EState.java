ffb199b298b9be32b23488a9d5315395b6d94fe8
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-26 18:31:51 +0300

    update documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/game/EState.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/game/EState.java
@@ -22,26 +22,16 @@ import io.github.kawaxte.twentyten.launcher.util.LauncherLanguageUtils;
 import lombok.Getter;
 
 /**
- * This enum represents the various states the {@link
- * io.github.kawaxte.twentyten.launcher.game.GameUpdater} can be in during its execution.
+ * Enum representing the various stages of Minecraft updates in the {@link
+ * io.github.kawaxte.twentyten.launcher.game.GameUpdater} class.
  *
- * <p>Each constant of this enum corresponds to a specific operation in the update process, and is
- * associated with a message that describes this operation. These messages are internationalised
- * strings stored in resource bundles, and are retrieved based on the current launcher language.
+ * <p>The constants in this Enum correspond to different states in the updating process, such as
+ * initialising, checking the cache, downloading and extracting packages, updating the classpath,
+ * and completion.
  *
- * <p>The state constants and their corresponding operations are as follows:
- *
- * <ol>
- *   <li>INITIALISE: The update process is being initialised.
- *   <li>CHECK_CACHE: The cache for existing files is being checked.
- *   <li>DOWNLOAD_PACKAGES: Packages necessary for the update are being downloaded.
- *   <li>EXTRACT_PACKAGES: The downloaded packages are being extracted.
- *   <li>UPDATE_CLASSPATH: The classpath is being updated.
- *   <li>DONE: The update process is complete.
- * </ol>
- *
- * <p>These states are used by the {@link io.github.kawaxte.twentyten.launcher.game.GameUpdater} to
- * determine and display the current operation being performed.
+ * <p>Each Enum constant has a corresponding {@code message} that describes the current state. These
+ * messages are internationalised and loaded from a resource bundle. The key to obtain the correct
+ * message is fetched from {@code LauncherLanguageUtils.getESEnumKeys()}.
  *
  * @author Kawaxte
  * @since 1.5.1023_04
@@ -56,13 +46,6 @@ public enum EState {
 
   @Getter private final String message;
 
-  /**
-   * Constructor for each enum constant. It takes a key which is used to retrieve the corresponding
-   * message from the resource bundle based on the current launcher language.
-   *
-   * @param key The key used to retrieve the corresponding message from the resource bundle.
-   * @see LauncherLanguageUtils#getESEnumKeys()
-   */
   EState(String key) {
     String selectedLanguage = (String) LauncherConfig.get(0);
     UTF8ResourceBundle bundle = LauncherLanguage.getUTF8Bundle(selectedLanguage);
