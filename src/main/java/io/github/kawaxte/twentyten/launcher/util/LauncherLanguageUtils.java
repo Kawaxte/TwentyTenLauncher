0cc2706a358eb7f81021061e0ad69be9a82df747
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-27 13:48:56 +0300

    add documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/util/LauncherLanguageUtils.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/util/LauncherLanguageUtils.java
@@ -1,9 +1,21 @@
 package io.github.kawaxte.twentyten.launcher.util;
 
+/**
+ * Utility class for retrieving keys from the language resource bundles.
+ *
+ * @author Kawaxte
+ * @since 1.5.0623_01
+ */
 public final class LauncherLanguageUtils {
 
   private LauncherLanguageUtils() {}
 
+  /**
+   * Returns an array of keys for {@link io.github.kawaxte.twentyten.launcher.game.EState}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.game.EState
+   */
   public static String[] getESEnumKeys() {
     return new String[] {
       "es_enum.initialise",
@@ -15,10 +27,24 @@ public final class LauncherLanguageUtils {
     };
   }
 
+  /**
+   * Returns an array of keys for {@link
+   * io.github.kawaxte.twentyten.launcher.ui.options.LanguageGroupBox}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.options.LanguageGroupBox
+   */
   public static String[] getLGBKeys() {
     return new String[] {"lgb.title", "lgb.setLanguageLabel"};
   }
 
+  /**
+   * Returns an array of keys for {@link
+   * io.github.kawaxte.twentyten.launcher.ui.LauncherNoNetworkPanel}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.LauncherNoNetworkPanel
+   */
   public static String[] getLNPPKeys() {
     return new String[] {
       "lnnp.errorLabel.signin",
@@ -32,6 +58,12 @@ public final class LauncherLanguageUtils {
     };
   }
 
+  /**
+   * Returns an array of keys for {@link io.github.kawaxte.twentyten.launcher.ui.GameAppletWrapper}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.GameAppletWrapper
+   */
   public static String[] getGAWKeys() {
     return new String[] {
       "gaw.updaterStarted",
@@ -41,24 +73,57 @@ public final class LauncherLanguageUtils {
     };
   }
 
+  /**
+   * Returns an array of keys for {@link io.github.kawaxte.twentyten.launcher.ui.MicrosoftAuthPanel}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.MicrosoftAuthPanel
+   */
   public static String[] getMAPKeys() {
     return new String[] {"map.copyCodeLabel", "map.openBrowserButton", "map.cancelButton"};
   }
 
+  /**
+   * Returns an array of keys for {@link
+   * io.github.kawaxte.twentyten.launcher.ui.options.OptionsDialog}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.options.OptionsDialog
+   */
   public static String[] getODKeys() {
     return new String[] {"od.title"};
   }
 
+  /**
+   * Returns an array of keys for {@link
+   * io.github.kawaxte.twentyten.launcher.ui.options.OptionsPanel}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.options.OptionsPanel
+   */
   public static String[] getOPKeys() {
     return new String[] {
       "op.versionGroupBox", "op.languageGroupBox", "op.openFolderButton", "op.saveOptionsButton"
     };
   }
 
+  /**
+   * Returns an array of keys for {@link
+   * io.github.kawaxte.twentyten.launcher.ui.options.VersionGroupBox}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.options.VersionGroupBox
+   */
   public static String[] getVGBKeys() {
     return new String[] {"vgb.title", "vgb.showVersionsCheckBox", "vgb.useVersionLabel"};
   }
 
+  /**
+   * Returns an array of keys for {@link io.github.kawaxte.twentyten.launcher.ui.YggdrasilAuthPanel}
+   *
+   * @return an array of keys from the language resource bundle
+   * @see io.github.kawaxte.twentyten.launcher.ui.YggdrasilAuthPanel
+   */
   public static String[] getYAPKeys() {
     return new String[] {
       "yap.microsoftSigninButton",
