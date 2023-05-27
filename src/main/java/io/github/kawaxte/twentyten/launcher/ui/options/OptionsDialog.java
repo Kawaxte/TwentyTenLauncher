5b1cc2961a01fd2af082bac1e89fe93845c1c973
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-27 14:05:42 +0300

    add documentation

--- src/main/java/io/github/kawaxte/twentyten/launcher/ui/options/OptionsDialog.java
+++ src/main/java/io/github/kawaxte/twentyten/launcher/ui/options/OptionsDialog.java
@@ -24,11 +24,31 @@ import java.awt.Window;
 import java.util.Objects;
 import javax.swing.JDialog;
 
+/**
+ * Class representing a non-modal dialog window for presenting various options to the user.
+ *
+ * <p>Note that the language used in this dialog can be dynamically changed according to the
+ * launcher's configuration.
+ *
+ * @see javax.swing.JDialog
+ * @see io.github.kawaxte.twentyten.launcher.ui.options.OptionsPanel
+ * @author Kawaxte
+ * @since 1.4.0923_02
+ */
 public class OptionsDialog extends JDialog {
 
   public static final long serialVersionUID = 1L;
   private static OptionsDialog instance;
 
+  /**
+   * Constructor for OptionsDialog.
+   *
+   * <p>Sets up the dialog's properties, including its content pane, default close operation, size,
+   * location, and resizable state. Also, sets the dialog's titles according to the currently
+   * selected language.
+   *
+   * @param owner the {@link java.awt.Window} from which the dialog is displayed
+   */
   public OptionsDialog(Window owner) {
     super(owner, ModalityType.MODELESS);
 
@@ -48,18 +68,45 @@ public class OptionsDialog extends JDialog {
         Objects.nonNull(selectedLanguage) ? bundle : LauncherLanguage.getBundle());
   }
 
+  /**
+   * Returns the current instance of the OptionsDialog.
+   *
+   * @return the instance of OptionsDialog
+   */
   public static OptionsDialog getInstance() {
     return instance;
   }
 
+  /**
+   * Sets the current instance of the OptionsDialog.
+   *
+   * @param od the instance of OptionsDialog to be set
+   */
   private static void setInstance(OptionsDialog od) {
     instance = od;
   }
 
+  /**
+   * Updates the title of the dialog.
+   *
+   * <p>The title is set according to the provided {@link
+   * io.github.kawaxte.twentyten.UTF8ResourceBundle}.
+   *
+   * @param bundle a {@link io.github.kawaxte.twentyten.UTF8ResourceBundle} object containing the
+   *     text resources to be used for the title
+   */
   public void updateContainerTitles(UTF8ResourceBundle bundle) {
     LauncherUtils.setContainerTitle(bundle, this, LauncherLanguageUtils.getODKeys()[0]);
   }
 
+  /**
+   * Returns the title of this dialog window.
+   *
+   * <p>The title is fetched from the {@link
+   * io.github.kawaxte.twentyten.launcher.util.LauncherLanguageUtils} based on a predefined key.
+   *
+   * @return a string representing the title of this dialog window
+   */
   @Override
   public String getTitle() {
     return LauncherLanguageUtils.getODKeys()[0];
