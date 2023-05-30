223955a69905caddeb7ae828b0b5fc73bc320226
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-05-30 19:13:21 +0300

    remove commit

--- src/main/java/ch/kawaxte/launcher/impl/exception/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package ch.kawaxte.launcher.impl.exception;
-
-public class AuthenticationException extends RuntimeException {
-
-  public AuthenticationException(String message) {
-    super(message);
-  }
-}
